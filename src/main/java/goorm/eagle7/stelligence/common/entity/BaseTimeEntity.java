a68c68cac25c98f5e1de0981b33ef6fcb2e5e582
sbslc2000 <sbslc2000@gmail.com>
2024-01-12 15:34:48 +0900

    [#7] feat : BaseTimeEntity에 Getter 추가

--- src/main/java/goorm/eagle7/stelligence/common/entity/BaseTimeEntity.java
+++ src/main/java/goorm/eagle7/stelligence/common/entity/BaseTimeEntity.java
@@ -8,12 +8,14 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 
 /**
  * BaseTimeEntity
  * 생성 시간과 변경 시간을 추적하기 위한 추상 클래스
  * Entity 에 상속하여 사용
  */
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
