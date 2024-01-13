69595db12ce2b5b96ae6d067b603d419d3d377b4
youngmin <dudals9901@naver.com>
2024-01-14 00:27:16 +0900

    [#5] chore: 불필요한 import 제거

--- src/main/java/goorm/eagle7/stelligence/domain/graph/model/DocumentNode.java
+++ src/main/java/goorm/eagle7/stelligence/domain/graph/model/DocumentNode.java
@@ -1,11 +1,8 @@
 package goorm.eagle7.stelligence.domain.graph.model;
 
-import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
-import org.springframework.data.neo4j.core.schema.Property;
 import org.springframework.data.neo4j.core.schema.Relationship;
-import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
 
 import lombok.AccessLevel;
 import lombok.Getter;
