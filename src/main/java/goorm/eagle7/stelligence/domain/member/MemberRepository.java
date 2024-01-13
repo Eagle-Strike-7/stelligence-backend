79b32caf212f2bce7c5355c196929e3318e1ead5
Sooamazing <honang515@gmail.com>
2024-01-13 16:12:12 +0900

    [#9] chore: MemberRepository method 주석 달기

--- src/main/java/goorm/eagle7/stelligence/domain/member/MemberRepository.java
+++ src/main/java/goorm/eagle7/stelligence/domain/member/MemberRepository.java
@@ -10,6 +10,12 @@ import goorm.eagle7.stelligence.domain.member.model.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+	/*
+	 * member id로 member 정보를 가져온다.
+	 * member가 있다면, Badge List와 Bookmark List를 함께 가져 옴.
+	 * @Param("id") Member id
+	 * @return Optional<Member>
+	 */
 	@Query("SELECT m FROM Member m LEFT JOIN FETCH m.badges LEFT JOIN FETCH m.bookmarks WHERE m.id = :id")
 	Optional<Member> findByIdWithBadgesAndBookmarks(@Param("id") Long id);
 
