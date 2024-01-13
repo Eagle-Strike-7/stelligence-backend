ffb637a935764ec3221df65b2f2ca1af973fe160
sbslc2000 <sbslc2000@gmail.com>
2024-01-14 00:40:26 +0900

    [#main] bug: MemberRepository 쿼리 주석 처리

--- src/main/java/goorm/eagle7/stelligence/domain/member/MemberRepository.java
+++ src/main/java/goorm/eagle7/stelligence/domain/member/MemberRepository.java
@@ -1,10 +1,6 @@
 package goorm.eagle7.stelligence.domain.member;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import goorm.eagle7.stelligence.domain.member.model.Member;
 
@@ -16,7 +12,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	 * @Param("id") Member id
 	 * @return Optional<Member>
 	 */
-	@Query("SELECT m FROM Member m LEFT JOIN FETCH m.badges LEFT JOIN FETCH m.bookmarks WHERE m.id = :id")
-	Optional<Member> findByIdWithBadgesAndBookmarks(@Param("id") Long id);
+	// @Query("SELECT m FROM Member m LEFT JOIN FETCH m.badges LEFT JOIN FETCH m.bookmarks WHERE m.id = :id")
+	// Optional<Member> findByIdWithBadgesAndBookmarks(@Param("id") Long id);
 
 }
