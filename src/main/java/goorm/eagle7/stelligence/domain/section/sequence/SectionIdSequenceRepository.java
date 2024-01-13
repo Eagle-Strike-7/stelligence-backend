50a92f776eb0724cdb02b2344b1c7455e72218a3
sbslc2000 <sbslc2000@gmail.com>
2024-01-13 23:39:10 +0900

    [#4] feat : SectionIdSequence 관련 코드 삭제

--- src/main/java/goorm/eagle7/stelligence/domain/section/sequence/SectionIdSequenceRepository.java
+++
@@ -1,8 +0,0 @@
-package goorm.eagle7.stelligence.domain.section.sequence;
-
-public interface SectionIdSequenceRepository {
-
-	Long getAndIncrementSectionId(Long documentId);
-
-	void createSequence(Long documentId);
-}
