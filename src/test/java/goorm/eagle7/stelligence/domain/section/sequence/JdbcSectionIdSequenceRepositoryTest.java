d919c66fa4af3fed000bef214648ceec6c5acaa2
sbslc2000 <sbslc2000@gmail.com>
2024-01-13 10:35:45 +0900

    [#4] test : JdbcSectionIdSequenceRepositoryTest 테스트코드 변경 -> 실행가능하게 작성

--- src/test/java/goorm/eagle7/stelligence/domain/section/sequence/JdbcSectionIdSequenceRepositoryTest.java
+++ src/test/java/goorm/eagle7/stelligence/domain/section/sequence/JdbcSectionIdSequenceRepositoryTest.java
@@ -5,14 +5,14 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-@Disabled
+@Transactional
 class JdbcSectionIdSequenceRepositoryTest {
 
 	@Autowired
@@ -21,9 +21,9 @@ class JdbcSectionIdSequenceRepositoryTest {
 	@Test
 	@DisplayName("기본 동작 테스트")
 	void basic() {
-		Long documentId = 1L;
+		Long documentId = 201L;
 
-		repository.createSequence(1L);
+		repository.createSequence(documentId);
 
 		Long sectionId = repository.getAndIncrementSectionId(documentId);
 
@@ -33,23 +33,31 @@ class JdbcSectionIdSequenceRepositoryTest {
 		assertThat(sectionId2).isEqualTo(2L);
 	}
 
+	public static Long sum = 0L;
+
+	/**
+	 * SectionIdSequence가 서로다른 스레드의 요청에서도 잘 동작하는지 확인합니다.
+	 * @throws InterruptedException
+	 */
 	@Test
 	@DisplayName("동시성 테스트")
 	void concurrency() throws InterruptedException {
-		Long documentId = 1L;
+		Long documentId = 200L;
 
-		repository.createSequence(1L);
+		repository.createSequence(documentId);
 
 		//repository.getAndIncrementSectionId(documentId); 를 1번 호출하는 스레드를 100개 생성
 
 		List<Thread> threads = new ArrayList<>();
 
+		//각각의 쓰레드는 repository.getAndIncrementSectionId(documentId); 를 1번 호출합니다.
+		//이후 나온 값의 합이 5050이 나오는지 확인합니다.
 		Runnable query = () -> {
 			Long sectionId = repository.getAndIncrementSectionId(documentId);
-			System.out.println("sectionId = " + sectionId);
+			sum += sectionId;
 		};
 
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 100; i++) {
 			threads.add(new Thread(query));
 		}
 
@@ -60,6 +68,8 @@ class JdbcSectionIdSequenceRepositoryTest {
 		}
 
 		Long sectionId = repository.getAndIncrementSectionId(documentId);
+		assertThat(sectionId).isEqualTo(101L); //101번째 요청의 결과는 101이 나와야 합니다.
+		assertThat(sum).isEqualTo(5050L); // 1~100까지의 합은 5050이 나와야 합니다.
 
 	}
 
