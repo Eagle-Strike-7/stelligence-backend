81fca53812da1d7e8df60b2a1e64159a998e95b6
sbslc2000 <sbslc2000@gmail.com>
2024-01-15 17:23:22 +0900

    [#21] docs: dummy.sql에 설명 추가

--- src/test/resources/dummy.sql
+++ src/test/resources/dummy.sql
@@ -1,3 +1,4 @@
+-- 4명의 멤버가 존재합니다.
 insert into member (member_id, email, name, nickname, role, contributes, image_url, refresh_token, social_type,
                     social_id, created_at, updated_at)
 values (1, 'email1', 'name1', 'nickname1', 'USER', 0, 'image_url1', 'refresh_token1', 0, 'social_id1', NOW(), NOW()),
@@ -5,12 +6,26 @@ values (1, 'email1', 'name1', 'nickname1', 'USER', 0, 'image_url1', 'refresh_tok
        (3, 'email3', 'name3', 'nickname3', 'USER', 0, 'image_url3', 'refresh_token3', 0, 'social_id3', NOW(), NOW()),
        (4, 'email4', 'name4', 'nickname4', 'USER', 0, 'image_url4', 'refresh_token4', 0, 'social_id4', NOW(), NOW());
 
+-- 4개의 문서가 존재합니다.
 insert into document (document_id, title, current_revision, created_at, updated_at)
 values (1, 'title1', 3, NOW(), NOW()),
        (2, 'title2', 2, NOW(), NOW()),
        (3, 'title3', 1, NOW(), NOW()),
        (4, 'title4', 1, NOW(), NOW());
 
+-- 14개의 섹션이 존재합니다.
+------ 1번 문서에는 6개의 섹션이 존재합니다.
+---------- (1,1) (2,1) (3,1) 은 최초의 1번 문서로 생성된 섹션입니다.
+---------- (2,2) (13,2) 는 Contribute 1번에 의하여 반영된 섹션입니다. (업데이트와 생성)
+------ (1,3) 은 Contribute 2번에 의하여 반영된 섹션입니다. (삭제)
+---------- 2번 문서에는 4개의 섹션이 존재합니다.
+------ (4,1) (5,1) (6,1) 은 최초의 2번 문서로 생성된 섹션입니다.
+---------- (14,2) 는 Contribute 3번에 의하여 반영된 섹션입니다. (생성)
+------ 3번 문서에는 3개의 섹션이 존재합니다.
+---------- (7,1) (8,1) (9,1) 은 최초의 3번 문서로 생성된 섹션입니다.
+------ 4번 문서에는 3개의 섹션이 존재합니다.
+----------- (10,1) (11,1) (12,1) 은 최초의 4번 문서로 생성된 섹션입니다.
+-- 관련 링크 : https://excited-cycle-902.notion.site/762ff798869f4320aec03bb4fbff27c9?pvs=4
 insert into section (section_id, revision, document_id, heading, title, content, orders, created_at, updated_at)
 values (1, 1, 1, 'H1', 'document1_title1', 'document1_content1', 1, NOW(), NOW()),
        (1, 3, 1, null, null, null, 1, NOW(), NOW()),
@@ -29,11 +44,14 @@ values (1, 1, 1, 'H1', 'document1_title1', 'document1_content1', 1, NOW(), NOW()
        (11, 1, 4, 'H2', 'document4_title2', 'document4_content2', 2, NOW(), NOW()),
        (12, 1, 4, 'H3', 'document4_title3', 'document4_content3', 3, NOW(), NOW());
 
+-- SectionId의 sequence_Id는 15부터
 INSERT sequence_table (sequence_name, sequence_value)
 values ('section', 15);
 
 
-
+-- 5개의 contribute가 존재합니다.
+------ 4번 Contribute는 5번 admentment를 갖고있는 수정요청으로, 거절되었습니다.
+------ 5번은 6, 7번 admentment를 갖고 있는 수정요청으로, 투표중입니다.
 insert into contribute (id, status, created_at, updated_at)
 values (1, 'MERGED', NOW(), NOW()),
        (2, 'MERGED', NOW(), NOW()),
@@ -42,7 +60,14 @@ values (1, 'MERGED', NOW(), NOW()),
        (5, 'VOTING', NOW(), NOW());
 
 
-
+-- 7개의 admentment가 존재합니다.
+------ 1번 amendment는 1번 contribute에 포함되어있으며, 섹션 2번을 업데이트하고자하는 수정안입니다.
+------ 2번 amendment는 1번 contribute에 포함되어있으며, 섹션 3번 뒤에 새로운 섹션을 생성하고자하는 수정안입니다.
+------ 3번 amendment는 2번 contribute에 포함되어있으며, 섹션 1번을 삭제하고자하는 수정안입니다.
+------ 4번 amendment는 수정요청에 등록되지 않았으며,, 섹션 13번을 업데이트하고자하는 수정안입니다.
+------ 5번 amendment는 4번 contribute에 포함되어있으며, 섹션 3번을 업데이트하고자하는 수정안입니다.
+------ 6번 amendment는 5번 contribute에 포함되어있으며, 섹션 4번 뒤에 새로운 섹션을 생성하고자하는 수정안입니다.
+------ 7번 amendment는 5번 contribute에 포함되어있으며, 섹션 6번을 업데이트하고자하는 수정안입니다.
 insert into amendment (commit_id, contribute_id, member_id, target_section_id, target_revision,
                        amendment_title, amendment_description, new_section_heading, new_section_title,
                        new_section_content, status, type, created_at, updated_at)
