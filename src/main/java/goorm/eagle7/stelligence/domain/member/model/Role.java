f7c50fc01358f902de0355d942cadb9bfc1d2be1
Sooamazing <honang515@gmail.com>
2024-01-16 14:13:19 +0900

    [#(no branch, rebasing 19] [#(no branch, rebasing 19] [#19] feat: String에서 Enum Role로 변환 위해 메서드 추가

--- src/main/java/goorm/eagle7/stelligence/domain/member/model/Role.java
+++ src/main/java/goorm/eagle7/stelligence/domain/member/model/Role.java
@@ -1,5 +1,13 @@
 package goorm.eagle7.stelligence.domain.member.model;
 
 public enum Role {
-	ADMIN, USER
+	ADMIN, USER;
+
+	public static Role getRoleFromString(String roleStr) {
+		try {
+			return Role.valueOf(roleStr.toUpperCase());
+		} catch (IllegalArgumentException e) {
+			return Role.USER;
+		}
+	}
 }
