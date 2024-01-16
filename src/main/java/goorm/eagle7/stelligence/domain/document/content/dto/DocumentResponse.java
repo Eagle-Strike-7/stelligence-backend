f319d44344edbaa12c17fa12be06f289c480caf8
sbslc2000 <sbslc2000@gmail.com>
2024-01-16 15:44:25 +0900

    [#24] docs: DocumentResponse 관련 주석 변경

--- src/main/java/goorm/eagle7/stelligence/domain/document/content/dto/DocumentResponse.java
+++ src/main/java/goorm/eagle7/stelligence/domain/document/content/dto/DocumentResponse.java
@@ -22,6 +22,15 @@ public class DocumentResponse {
 		this.sections = sections;
 	}
 
+	/**
+	 * DocumentResponse를 생성합니다.
+	 *
+	 * DocumentResponse의 sections는 해당 Document와 연결된 모든 Section을 담지 않습니다.
+	 * DocumentResponse에 담길 sections는 특정 버전에 해당하는 섹션만 담기므로, DTO 외부에서 이를 결정하고 삽입해주도록 했습니다. (documentContentService.getDocument 참조)
+	 * @param document : 조회한 Document
+	 * @param sections : 특정 버전에 해당하는 섹션들
+	 * @return
+	 */
 	public static DocumentResponse of(Document document, List<SectionResponse> sections) {
 		return new DocumentResponse(document.getId(), document.getTitle(), sections);
 	}
--- src/main/java/goorm/eagle7/stelligence/domain/document/dto/DocumentCreateRequest.java
+++ src/main/java/goorm/eagle7/stelligence/domain/document/dto/DocumentCreateRequest.java
@@ -1,11 +1,16 @@
 package goorm.eagle7.stelligence.domain.document.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
+@Schema(description = "생성할 문서의 정보")
 public class DocumentCreateRequest {
 
+	@Schema(description = "문서의 제목", example = "마리모")
 	private String title;
+	@Schema(description = "문서의 부모 문서 ID", example = "1")
 	private Long parentDocumentId;
+	@Schema(description = "문서의 내용", example = "# 마리모\n마리모는 조류의 일종이다.\n## 마리모의 생애\n마리모의 수명은 평균 1년이다.\n")
 	private String content;
 }
