6218478f06bc7824c5e5b546d0437e670fb19588
Sooamazing <honang515@gmail.com>
2024-01-16 13:48:19 +0900

    [#(no branch, rebasing 19] [#(no branch, rebasing 19] [#19] bug: Key 타입 호환 불가로 SecretKey로 수정, 주석 추가 및 문자열 상수 선언

--- src/main/java/goorm/eagle7/stelligence/config/JwtConfig.java
+++ src/main/java/goorm/eagle7/stelligence/config/JwtConfig.java
@@ -9,12 +9,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * JWT 설정
+ * 해시 AlGORITHM은 HmacSHA256 사용
+ * jwtSecret을 Base64로 디코딩하여 바이트 배열로 변환한 후 SecretKey를 생성한다.
+ *
+ */
 @Configuration
 public class JwtConfig {
 
 	@Value("${jwt.secretKey}")
 	private String jwtSecret;
 
+	private static final String ALGORITHM_NAME = "HmacSHA256";
+
+	/**
+	 * jwtSecret을 Base64로 디코딩하여 바이트 배열로 변환한 후 SecretKey를 생성한다.
+	 * @return SecretKey jwt 유효성 검증 시에 사용할 키
+	 */
 	@Bean
 	public SecretKey jwtKey() {
 
@@ -22,7 +34,7 @@ public class JwtConfig {
 		byte[] decodedKey = Base64.getDecoder().decode(jwtSecret);
 
 		// 생성된 바이트 배열로부터 SecretKey 생성
-		return new SecretKeySpec(decodedKey, 0, decodedKey.length, "HmacSHA256");
+		return new SecretKeySpec(decodedKey, 0, decodedKey.length, ALGORITHM_NAME);
 
 	}
 
