db25ed308f22f3d80a5b091b9f86781547cbc736
Sooamazing <honang515@gmail.com>
2024-01-16 13:52:24 +0900

    [#(no branch, rebasing 19] [#(no branch, rebasing 19] [#19] feat: filter에서 걸러줄 uri repository 생성, matcher로 기능 수행

---
+++ src/main/java/goorm/eagle7/stelligence/common/auth/filter/Resource.java
@@ -0,0 +1,19 @@
+package goorm.eagle7.stelligence.common.auth.filter;
+
+import static lombok.AccessLevel.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Filter에서 사용, 인증이 필요한 리소스 정보를 담는 객체
+ * CustomAntPathMatcher에서 사용
+ */
+@Getter
+@NoArgsConstructor(access = PRIVATE)
+@AllArgsConstructor(staticName = "of") // 정적 팩토리 메서드
+public class Resource {
+	private String httpMethod;
+	private String uri;
+}
