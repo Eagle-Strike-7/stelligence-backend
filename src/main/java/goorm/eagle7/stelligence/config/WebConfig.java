6cd293038e645023b2bbd1fcf2ed9567c1af9ab3
Sooamazing <honang515@gmail.com>
2024-01-16 14:00:17 +0900

    [#(no branch, rebasing 19] [#(no branch, rebasing 19] [#19] feat: memberInfoArgumentResolver를 Config에 등록해 사용하기

---
+++ src/main/java/goorm/eagle7/stelligence/config/WebConfig.java
@@ -0,0 +1,25 @@
+package goorm.eagle7.stelligence.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import goorm.eagle7.stelligence.common.auth.memberinfo.MemberInfoArgumentResolver;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * memberInfoArgumentResolver를 WebMvcConfigurer에 등록해 HandlerMethodArgumentResolver로 사용하게 한다.
+ *
+ */
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+	private final MemberInfoArgumentResolver memberInfoArgumentResolver;
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(memberInfoArgumentResolver);
+	}
+}
