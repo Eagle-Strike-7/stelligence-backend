20d24fb96d1fec2bc19ca08758afd21aa8cc00b6
Sooamazing <honang515@gmail.com>
2024-01-17 12:10:06 +0900

    [#(no branch, rebasing 19] [#19] chore: static import를 AccessLevel이 드러나도록 변경 - PR 리뷰 시 의견 받은 그 외 잘못된 주석 수

--- src/main/java/goorm/eagle7/stelligence/common/auth/dto/LoginRequest.java
+++ src/main/java/goorm/eagle7/stelligence/common/auth/dto/LoginRequest.java
@@ -1,13 +1,12 @@
 package goorm.eagle7.stelligence.common.auth.dto;
 
-import static lombok.AccessLevel.*;
-
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(staticName = "of") // 정적 팩토리 메서드
 public class LoginRequest {
 
--- src/main/java/goorm/eagle7/stelligence/common/login/dto/LoginRequest.java
+++ src/main/java/goorm/eagle7/stelligence/common/login/dto/LoginRequest.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = PRIVATE)
 @AllArgsConstructor(staticName = "of") // 정적 팩토리 메서드
 public class LoginRequest {
-
+	// TODO 닉네임 중복 체크 이후에 대한 요청인 게 드러나도록 이름 수정하기
 	private String nickname;
 
 }
