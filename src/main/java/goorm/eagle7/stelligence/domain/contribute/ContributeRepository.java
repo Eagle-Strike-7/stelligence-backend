3f70ea5d4c10a1d0ec98bc275f86f9d2a4ff75c3
youngmin <dudals9901@naver.com>
2024-01-17 11:33:56 +0900

    [#40] feat: ContributeRepository 생성

---
+++ src/main/java/goorm/eagle7/stelligence/domain/contribute/ContributeRepository.java
@@ -0,0 +1,9 @@
+package goorm.eagle7.stelligence.domain.contribute;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import goorm.eagle7.stelligence.domain.contribute.model.Contribute;
+
+public interface ContributeRepository extends JpaRepository<Contribute, Long> {
+
+}
