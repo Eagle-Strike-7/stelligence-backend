0a302f06493a74ad7f3fb899e56b4f1322233e2d
youngmin <dudals9901@naver.com>
2024-01-17 18:19:50 +0900

    [#40] feat: ContributeService 구현해야하는 메서드 틀 작성

--- src/main/java/goorm/eagle7/stelligence/domain/contribute/ContributeService.java
+++ src/main/java/goorm/eagle7/stelligence/domain/contribute/ContributeService.java
@@ -3,6 +3,7 @@ package goorm.eagle7.stelligence.domain.contribute;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import goorm.eagle7.stelligence.domain.contribute.dto.ContributeCreateRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,4 +15,28 @@ public class ContributeService {
 
 	private final ContributeRepository contributeRepository;
 
+	// TODO: 수정 요청 엔티티를 생성하고, 리포지토리에 save
+	public void createContribute(ContributeCreateRequest contributeCreateRequest) {
+
+	}
+
+	// TODO: 멤버에 따라 수정 요청 엔티티 리스트를 찾아서, 적절히 반환
+	public void getContributesByMember() {
+
+	}
+
+	// TODO: 문서에 따라 수정 요청 엔티티 리스트를 찾아서, 적절히 반환
+	public void getContributesByDocument() {
+
+	}
+
+	// TODO: 수정 요청 id에 따라 수정 요청 엔티티를 찾아서, 적절히 반환
+	public void getContributeById() {
+
+	}
+
+	// TODO: 수정 요청 id에 따라 수정 요청 엔티티를 찾아서 수정 요청을 철회 - 이때 기존 수정 요청은 상태를 pending으로 수정
+	public void deleteContributesById() {
+
+	}
 }
