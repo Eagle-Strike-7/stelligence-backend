ecffebf509ca96cb448461208e37b2e54e8e5eb1
eenzzi <purelej99@gmail.com>
2024-01-18 18:14:18 +0900

    [#17] feat: QueryDSL CustomRepository 적용

--- src/main/java/goorm/eagle7/stelligence/domain/amendment/AmendmentRepository.java
+++ src/main/java/goorm/eagle7/stelligence/domain/amendment/AmendmentRepository.java
@@ -1,10 +1,10 @@
 package goorm.eagle7.stelligence.domain.amendment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
+import goorm.eagle7.stelligence.domain.amendment.custom.CustomAmendmentRepository;
 import goorm.eagle7.stelligence.domain.amendment.model.Amendment;
 
-public interface AmendmentRepository extends JpaRepository<Amendment, Long>, QuerydslPredicateExecutor<Amendment> {
+public interface AmendmentRepository extends JpaRepository<Amendment, Long>, CustomAmendmentRepository {
 
 }
