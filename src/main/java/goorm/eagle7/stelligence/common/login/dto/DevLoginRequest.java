71c4703e3768ba937ba0c04300b7cae7b57178d3
Sooamazing <honang515@gmail.com>
2024-01-18 15:09:32 +0900

    [#19] refactor: LoginRequest -> Dev로 변경, response 변경 - DevLoginRequest는 개발 단계의 편의성을 위함. - 쿠키 사용으로 결정해 socialtype만 response body에 반환

--- src/main/java/goorm/eagle7/stelligence/common/login/dto/DevLoginRequest.java
+++ src/main/java/goorm/eagle7/stelligence/common/login/dto/DevLoginRequest.java
@@ -6,11 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
+// test를 위한 닉네임 RequestDTO
 @Getter
 @NoArgsConstructor(access = PRIVATE)
 @AllArgsConstructor(staticName = "of") // 정적 팩토리 메서드
-public class LoginRequest {
-	// TODO 닉네임 중복 체크 이후에 대한 요청인 게 드러나도록 이름 수정하기
+public class DevLoginRequest {
 	private String nickname;
-
 }
