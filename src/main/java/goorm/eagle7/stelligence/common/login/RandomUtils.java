40e92a9024884ce56ac9d2f5d4704cff2ee01793
Sooamazing <honang515@gmail.com>
2024-01-18 15:03:50 +0900

    [#19] refactor: DevFilter에서도 사용해 public으로 변경 및 주석 추가

--- src/main/java/goorm/eagle7/stelligence/common/login/RandomUtils.java
+++ src/main/java/goorm/eagle7/stelligence/common/login/RandomUtils.java
@@ -2,7 +2,7 @@ package goorm.eagle7.stelligence.common.login;
 
 import java.util.Random;
 
-class RandomUtils {
+public class RandomUtils {
 
 	private static final Random random = new Random();
 
@@ -10,12 +10,15 @@ class RandomUtils {
 		throw new IllegalStateException("Utility class");
 	}
 
-	// 5자리 랜덤 숫자를 추가하는 메서드
+	/**
+	 * 닉네임 중복이면 랜덤 닉네임 생성
+	 * @param nickname 닉네임
+	 * @return 닉네임+랜덤숫자 5자리
+	 */
 	public static String createNicknameWithRandomNumber(String nickname) {
 
 		// nickname 중복 확인 후 중복이면 랜덤 생성
 		int randomNumber = 10000 + random.nextInt(90000); // 10000 ~ 99999 범위의 숫자 생성
-
 		return nickname + randomNumber;
 
 	}
