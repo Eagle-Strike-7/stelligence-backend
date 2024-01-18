c4e54cc788cb4d5cd4def875347fe3a95c508cb2
Sooamazing <honang515@gmail.com>
2024-01-18 15:20:56 +0900

    [#19] feat: test 시 /api/login은 실제 로그인되도록 경로 설정

--- src/main/java/goorm/eagle7/stelligence/common/auth/filter/ResourceMemoryRepository.java
+++ src/main/java/goorm/eagle7/stelligence/common/auth/filter/ResourceMemoryRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 
 /**
- * 인증이 필요한 리소스 정보를 arrayList로 저장해 놓은 객체
+ * 인증이 필요한 리소스 정보를 arrayList로 저장해 놓은 객체이나 테스트 용도로 인증 필요없는 리소스만 저장해 놓음.
  * CustomAntPathMatcher에서 사용, 전체를 불러와 매칭할 때 사용함.
  */
 @Repository
@@ -16,8 +16,8 @@ public class ResourceMemoryRepository {
 
 	public ResourceMemoryRepository() {
 
-		resources.add(Resource.of("GET", "/api/"));
-		resources.add(Resource.of("GET", "/api/members/me"));
+		resources.add(Resource.of("POST", "/api/login"));
+
 	}
 
 	public List<Resource> findAll() {
