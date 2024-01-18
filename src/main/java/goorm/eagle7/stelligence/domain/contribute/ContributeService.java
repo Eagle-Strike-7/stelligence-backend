afd946f59885f3a39c71ec84a7b58abcc3f4ff7b
youngmin <dudals9901@naver.com>
2024-01-19 01:38:43 +0900

    [#40] chore: main pull 전 커밋

--- src/main/java/goorm/eagle7/stelligence/domain/contribute/ContributeService.java
+++ src/main/java/goorm/eagle7/stelligence/domain/contribute/ContributeService.java
@@ -1,8 +1,12 @@
 package goorm.eagle7.stelligence.domain.contribute;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import goorm.eagle7.stelligence.domain.amendment.AmendmentRepository;
+import goorm.eagle7.stelligence.domain.amendment.model.Amendment;
 import goorm.eagle7.stelligence.domain.contribute.dto.ContributeCreateRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,9 +18,13 @@ import lombok.extern.slf4j.Slf4j;
 public class ContributeService {
 
 	private final ContributeRepository contributeRepository;
+	private final AmendmentRepository amendmentRepository;
 
 	// TODO: 수정 요청 엔티티를 생성하고, 리포지토리에 save
-	public void createContribute(ContributeCreateRequest contributeCreateRequest) {
+	public void createContribute(ContributeCreateRequest contributeCreateRequest, Long loginMemberId) {
+
+		List<Amendment> amendments = amendmentRepository.findAllById(contributeCreateRequest.getAmendmentIdList());
+		//TODO: amendments에 대한 권한 있는 유저인지 판단 필요
 
 	}
 
