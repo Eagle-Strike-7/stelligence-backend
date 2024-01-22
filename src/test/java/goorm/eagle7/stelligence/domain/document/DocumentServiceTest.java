ab913d9e89bc4585058eacd8dee2a377ec58acae
sbslc2000 <sbslc2000@gmail.com>
2024-01-22 22:17:39 +0900

    [#69] test: DocumentService 단위테스트로 변경

--- src/test/java/goorm/eagle7/stelligence/domain/document/DocumentServiceTest.java
+++ src/test/java/goorm/eagle7/stelligence/domain/document/DocumentServiceTest.java
@@ -13,14 +13,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import goorm.eagle7.stelligence.domain.document.content.DocumentContentService;
-import goorm.eagle7.stelligence.domain.document.content.dto.DocumentResponse;
 import goorm.eagle7.stelligence.domain.document.content.model.Document;
 import goorm.eagle7.stelligence.domain.document.dto.DocumentCreateRequest;
 import goorm.eagle7.stelligence.domain.document.graph.DocumentGraphService;
 import goorm.eagle7.stelligence.domain.member.MemberRepository;
 import goorm.eagle7.stelligence.domain.member.model.Member;
-import goorm.eagle7.stelligence.domain.section.model.Heading;
-import goorm.eagle7.stelligence.domain.section.model.Section;
 
 @ExtendWith(MockitoExtension.class)
 class DocumentServiceTest {
@@ -40,21 +37,9 @@ class DocumentServiceTest {
 	@Test
 	void createDocument() {
 		//given
-		Long loginMemberId = 1L;
+		Member author = member(1L, "testNickname");
 
-		Member author = member("testNickname");
-
-		Document document = document("testTitle", author);
-
-		Section.createSection(
-			document,
-			1L,
-			1L,
-			Heading.H1,
-			"testTitle",
-			"testSectionContent",
-			1
-		);
+		Document document = document(1L, author, "title", 1L);
 
 		DocumentCreateRequest documentCreateRequest = DocumentCreateRequest.of(
 			"testTitle",
@@ -62,29 +47,23 @@ class DocumentServiceTest {
 			"testSectionContent"
 		);
 
-		when(memberRepository.findById(loginMemberId)).thenReturn(Optional.of(author));
+		when(memberRepository.findById(author.getId())).thenReturn(Optional.of(author));
 		when(documentContentService.createDocument("testTitle", "testSectionContent", author))
 			.thenReturn(document);
 
 		//when
-		DocumentResponse documentResponse = documentService.createDocument(
+		documentService.createDocument(
 			documentCreateRequest,
-			loginMemberId
+			author.getId()
 		);
 
 		//then
 		//memberRepository, documentContentService, documentGraphService가 각각 한 번씩 호출되어야 합니다.
-		verify(memberRepository, times(1)).findById(loginMemberId);
+		verify(memberRepository, times(1)).findById(author.getId());
 		verify(documentContentService, times(1)).createDocument("testTitle", "testSectionContent", author);
 		verify(documentGraphService, times(1)).createDocumentNode(document);
 
 		//기여한 글 개수가 올라야 합니다.
 		assertThat(author.getContributes()).isEqualTo(1);
-
-		//DocumentResponse가 정상적으로 생성되어야 합니다.
-		assertThat(documentResponse.getTitle()).isEqualTo("testTitle");
-		assertThat(documentResponse.getSections()).hasSize(1);
-		assertThat(documentResponse.getSections().get(0).getTitle()).isEqualTo("testTitle");
-		assertThat(documentResponse.getSections().get(0).getContent()).isEqualTo("testSectionContent");
 	}
 }
