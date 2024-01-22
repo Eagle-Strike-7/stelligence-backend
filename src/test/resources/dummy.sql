9df739019fa36b0ed31a41b9d09a240854c5f86a
eenzzi <purelej99@gmail.com>
2024-01-22 14:43:32 +0900

    [#53] test: 바뀐 요구사항에 맞게 amendment dummy 변경

--- src/test/resources/dummy.sql
+++ src/test/resources/dummy.sql
@@ -1,10 +1,14 @@
 -- 4명의 멤버가 존재합니다.
 insert into member (member_id, email, name, nickname, role, contributes, image_url, refresh_token, social_type,
                     social_id, created_at, updated_at)
-values (1, 'email1', 'name1', 'nickname1', 'USER', 0, 'image_url1', 'refresh_token1', 'KAKAO', 'social_id1', NOW(), NOW()),
-       (2, 'email2', 'name2', 'nickname2', 'USER', 0, 'image_url2', 'refresh_token2', 'GOOGLE', 'social_id2', NOW(), NOW()),
-       (3, 'email3', 'name3', 'nickname3', 'USER', 0, 'image_url3', 'refresh_token3', 'NAVER', 'social_id3', NOW(), NOW()),
-       (4, 'email4', 'name4', 'nickname4', 'USER', 0, 'image_url4', 'refresh_token4', 'GOOGLE', 'social_id4', NOW(), NOW());
+values (1, 'email1', 'name1', 'nickname1', 'USER', 0, 'image_url1', 'refresh_token1', 'KAKAO', 'social_id1', NOW(),
+        NOW()),
+       (2, 'email2', 'name2', 'nickname2', 'USER', 0, 'image_url2', 'refresh_token2', 'GOOGLE', 'social_id2', NOW(),
+        NOW()),
+       (3, 'email3', 'name3', 'nickname3', 'USER', 0, 'image_url3', 'refresh_token3', 'NAVER', 'social_id3', NOW(),
+        NOW()),
+       (4, 'email4', 'name4', 'nickname4', 'USER', 0, 'image_url4', 'refresh_token4', 'GOOGLE', 'social_id4', NOW(),
+        NOW());
 
 -- 4개의 문서가 존재합니다.
 insert into document (document_id, title, current_revision, created_at, updated_at)
@@ -68,24 +72,17 @@ values (1, 'MERGED', NOW(), NOW()),
 ------ 5번 amendment는 4번 contribute에 포함되어있으며, 섹션 3번을 업데이트하고자하는 수정안입니다.
 ------ 6번 amendment는 5번 contribute에 포함되어있으며, 섹션 4번 뒤에 새로운 섹션을 생성하고자하는 수정안입니다.
 ------ 7번 amendment는 5번 contribute에 포함되어있으며, 섹션 6번을 업데이트하고자하는 수정안입니다.
-insert into amendment (commit_id, contribute_id, member_id, target_section_id, target_revision,
-                       amendment_title, amendment_description, new_section_heading, new_section_title,
-                       new_section_content, status, type, created_at, updated_at)
-values (1, 1, 1, 2, 1, 'amendment_title1', 'amendment_description1', 'H2', 'document1_title2_update',
-        'document1_content2_update', 'REQUESTED', 'UPDATE', NOW(), NOW()),
-       (2, 1, 1, 3, 1, 'amendment_title2', 'amendment_description2', 'H1', 'document1_title4_insert',
-        'document1_content4_insert', 'REQUESTED', 'CREATE', NOW(), NOW()),
-       (3, 2, 1, 1, 1, 'amendment_title3', 'amendment_description3', NULL, NULL,
-        NULL, 'REQUESTED', 'DELETE', NOW(), NOW()),
-       (4, null, 1, 13, 2, 'amendment_title4', 'amendment_description4', 'H1', 'document1_title4_insert_update',
-        'document1_content4_insert_update', 'PENDING', 'UPDATE', NOW(), NOW()),
-       (5, 4, 1, 3, 1, 'amendment_title5', 'amendment_description5', 'H2', 'document1_title3', 'document1_content3',
-        'REQUESTED', 'UPDATE', NOW(), NOW()),
-       (6, 5, 2, 4, 1, 'amendment_title6', 'amendment_description6', 'H1', 'document2_title4_insert',
-        'document2_content4_insert', 'REQUESTED', 'CREATE', NOW(), NOW()),
-       (7, 5, 2, 6, 1, 'amendment_title6', 'amendment_description6', 'H1', 'document2_title3_update',
-        'document2_content3_update',
-        'REQUESTED', 'UPDATE', NOW(), NOW());
+insert into amendment (amendment_id, contribute_id, target_section_id, target_revision, creating_order,
+                       new_section_heading, new_section_title,
+                       new_section_content, type, created_at, updated_at)
+values (1, 1, 2, 1, null, 'H2', 'document1_title2_update', 'document1_content2_update', 'UPDATE', NOW(), NOW()),
+       (2, 1, 3, 1, 1, 'H1', 'document1_title4_insert', 'document1_content4_insert', 'CREATE', NOW(), NOW()),
+       (3, 2, 1, 1, null, NULL, NULL, NULL, 'DELETE', NOW(), NOW()),
+       (4, null, 13, 2, null, 'H1', 'document1_title4_insert_update',
+        'document1_content4_insert_update', 'UPDATE', NOW(), NOW()),
+       (5, 4, 3, 1, null, 'H2', 'document1_title3', 'document1_content3', 'UPDATE', NOW(), NOW()),
+       (6, 5, 4, 1, 2, 'H1', 'document2_title4_insert', 'document2_content4_insert', 'CREATE', NOW(), NOW()),
+       (7, 5, 6, 1, null, 'H1', 'document2_title3_update', 'document2_content3_update', 'UPDATE', NOW(), NOW());
 
 
 
