d046aaa99bbfbd3a8d58a8421c72be4c133cc3ef
sbslc2000 <sbslc2000@gmail.com>
2024-01-23 21:29:23 +0900

    [#83] feat: MemberInfo가 Swagger에 RequestBody로 나타나는 문제 해결

--- src/main/java/goorm/eagle7/stelligence/common/auth/memberinfo/MemberInfo.java
+++ src/main/java/goorm/eagle7/stelligence/common/auth/memberinfo/MemberInfo.java
@@ -1,6 +1,7 @@
 package goorm.eagle7.stelligence.common.auth.memberinfo;
 
 import goorm.eagle7.stelligence.domain.member.model.Role;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 /**
  * MemberInfoArgumentResolver를 이용해 @Auth 애노테이션 사용 시 반환하는 객체
  */
+@Hidden // Swagger 문서에 노출되지 않도록 설정
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(staticName = "of")
