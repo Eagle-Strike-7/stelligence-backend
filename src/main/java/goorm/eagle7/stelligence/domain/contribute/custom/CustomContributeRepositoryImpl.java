e6915f563bf6cbd76a375a8481c66f78acf0955a
eenzzi <purelej99@gmail.com>
2024-01-23 22:58:08 +0900

    [#71] feat: Contribute repository, Query dsl을 위한 custom repository 구현

---
+++ src/main/java/goorm/eagle7/stelligence/domain/contribute/custom/CustomContributeRepositoryImpl.java
@@ -0,0 +1,46 @@
+package goorm.eagle7.stelligence.domain.contribute.custom;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import goorm.eagle7.stelligence.domain.contribute.model.Contribute;
+import goorm.eagle7.stelligence.domain.contribute.model.ContributeStatus;
+import goorm.eagle7.stelligence.domain.contribute.model.QContribute;
+import jakarta.persistence.EntityManager;
+
+public class CustomContributeRepositoryImpl implements CustomContributeRepository {
+
+	private final JPAQueryFactory queryFactory;
+
+	public CustomContributeRepositoryImpl(EntityManager em) {
+		this.queryFactory = new JPAQueryFactory(em);
+	}
+
+	@Override
+	public List<Contribute> findContributesByDocumentCreatedAtDesc(Long documentId, Pageable pageable) {
+
+		QContribute contribute = QContribute.contribute;
+
+		return queryFactory.selectFrom(contribute)
+			.where(contribute.document.id.eq(documentId))
+			.orderBy(contribute.createdAt.desc())
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetch();
+	}
+
+	@Override
+	public List<Contribute> findVotingContributesCreatedAtDesc(Pageable pageable) {
+		QContribute contribute = QContribute.contribute;
+
+		return queryFactory.selectFrom(contribute)
+			.where(contribute.status.eq(ContributeStatus.VOTING))
+			.orderBy(contribute.createdAt.desc())
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetch();
+	}
+}
