8899d04fad0a6f3e59d830a27eb6ac153f3be9f5
eenzzi <purelej99@gmail.com>
2024-01-24 11:13:18 +0900

    [#71] feat: join fetch 적용, batch fetch size 조정

--- src/main/java/goorm/eagle7/stelligence/domain/contribute/ContributeRepository.java
+++ src/main/java/goorm/eagle7/stelligence/domain/contribute/ContributeRepository.java
@@ -1,9 +1,15 @@
 package goorm.eagle7.stelligence.domain.contribute;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import goorm.eagle7.stelligence.domain.contribute.custom.CustomContributeRepository;
 import goorm.eagle7.stelligence.domain.contribute.model.Contribute;
 
 public interface ContributeRepository extends JpaRepository<Contribute, Long>, CustomContributeRepository {
+	@Query("SELECT c FROM Contribute c JOIN FETCH c.amendments JOIN FETCH c.member WHERE c.id = :id")
+	Optional<Contribute> findByIdWithAmendmentsAndMember(@Param("id") Long id);
 }
