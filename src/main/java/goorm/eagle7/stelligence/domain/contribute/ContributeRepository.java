cfd2809d2b54d8d699bc114bf8ab2dda4b416fca
sbslc2000 <sbslc2000@gmail.com>
2024-01-24 17:53:49 +0900

    [#77] feat: SchedulingTarget을 가져오는 모듈 개발

--- src/main/java/goorm/eagle7/stelligence/domain/contribute/ContributeRepository.java
+++ src/main/java/goorm/eagle7/stelligence/domain/contribute/ContributeRepository.java
@@ -1,5 +1,7 @@
 package goorm.eagle7.stelligence.domain.contribute;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +14,8 @@ import goorm.eagle7.stelligence.domain.contribute.model.Contribute;
 public interface ContributeRepository extends JpaRepository<Contribute, Long>, CustomContributeRepository {
 	@Query("SELECT c FROM Contribute c LEFT JOIN FETCH c.amendments LEFT JOIN FETCH c.member WHERE c.id = :id")
 	Optional<Contribute> findByIdWithAmendmentsAndMember(@Param("id") Long id);
+
+	// 생성시간이 from과 to 사이인 Contribute를 가져온다.
+	@Query("SELECT c FROM Contribute c LEFT JOIN FETCH Amendment a LEFT JOIN FETCH Member m WHERE c.createdAt BETWEEN :from AND :to")
+	List<Contribute> findByCreatedAtBetween(@Param("from") LocalDateTime from, @Param("to") LocalDateTime to);
 }
