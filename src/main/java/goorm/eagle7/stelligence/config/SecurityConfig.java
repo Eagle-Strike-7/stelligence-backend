373bbfdcff67cd5750773c2089a2dcd4177f2d6a
Sooamazing <honang515@gmail.com>
2024-01-24 19:52:56 +0900

    [#51] feat: SecurityConfig 설정 완료 및 주석 - csrf 비활성화(for token) - basicfilter 사용 제한 - 세션 stateless 설정 - 인증 필요한 uri 설정   - /api/login으로 test용 로그인 임시 uri 허용 - filter 순서 설정(jwt 인증, cors 설정) - exception handler로 403, 401 에러 응답 처리 - oauth2Login service, success, failure 설정 - logout (/api/logout)   - cookie 삭제, Authentication 초기화, db에서 refreshToken 삭제   - logout 경로는 누구든 가능하게 설정

--- src/main/java/goorm/eagle7/stelligence/config/SecurityConfig.java
+++ src/main/java/goorm/eagle7/stelligence/config/SecurityConfig.java
@@ -10,10 +10,12 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.filter.CorsFilter;
 
 import goorm.eagle7.stelligence.common.auth.filter.AuthFilter;
-import goorm.eagle7.stelligence.common.auth.filter.ResourceAntPathMatcher;
+import goorm.eagle7.stelligence.common.auth.jwt.JwtAccessDeniedHandler;
+import goorm.eagle7.stelligence.common.auth.jwt.JwtAuthenticationEntryPoint;
 import goorm.eagle7.stelligence.common.auth.oauth.handler.OAuth2LogoutCustomHandler;
 import goorm.eagle7.stelligence.common.auth.oauth.handler.OAuth2LogoutSuccessHandler;
 import goorm.eagle7.stelligence.common.auth.oauth.service.CustomOAuth2UserService;
@@ -35,68 +37,115 @@ public class SecurityConfig {
 	private String refreshTokenCookieName;
 
 	private final AuthFilter authFilter;
+	private final CorsFilter corsFilter;
 	private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 	private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
 	private final OAuth2LogoutCustomHandler oAuth2LogoutCustomHandler;
 	private final OAuth2LogoutSuccessHandler oAuth2LogoutSuccessHandler;
+	private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 	private final CustomOAuth2UserService customOAuth2UserService;
-	private final CorsFilter corsFilter;
-	private final ResourceAntPathMatcher resourceAntPathMatcher;
 
+	/**
+	 * .ignoring():
+	 * 		- 인증 및 권한 검사 없이 접근 가능해 Spring Security의 보안 처리를 완전히 우회함.
+	 * 		- 주로 정적 리소스에 사용됨.
+	 * .permitAll():
+	 * 		- 모든 사용자의 접근을 허용
+	 * 		- CSRF 보호, XSS 방지 등 Spring Security의 다른 보안 메커니즘은 여전히 적용됨.
+	 * 		- 주로 특정 API 엔드포인트나 페이지에 사용됨.
+	 * 		- 보안 검사는 필요하지만 인증은 필요하지 않은 경우에 적합.
+	 * @return WebSecurityCustomizer : WebSecurityCustomizer
+	 */
 	@Bean
 	public WebSecurityCustomizer webSecurityCustomizer() {
-		return web -> web.ignoring().requestMatchers("/static/**");
+		return web -> web.ignoring()
+			.requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico", "/fonts/**"," /assets/**");
 	}
 
-	// TODO 권한 없으면 EXCEPTION handler, Exception 설정.
+	/**
+	 * 1. csrf -> token 기반으로 비활성화
+	 * 2. httpBasic -> form 기반 로그인 비활성화
+	 * 3. sessionManagement -> 세션 비활성화 for token
+	 * 4. authorizeHttpRequests -> 인가된 사용자만 접근 가능하도록 설정
+	 * 		- permitAll -> 인증되지 않은 사용자도 접근 가능
+	 * 			- /api/login -> test nickname 가입 용도
+	 * 			- /oauth2/** -> oauth2 로그인 요청
+	 * 			- /login/oauth2/code/** -> oauth2 로그인 성공 후 redirect uri
+	 * 		- anyRequest().authenticated() -> 인증된 사용자만 접근 가능
+	 * 			- /api/** -> 인가된 사용자만 접근 가능
+	 * 5. addFilterBefore -> corsFilter, authFilter 추가
+	 * 		- corsFilter -> cors 설정
+	 * 		- authFilter -> 토큰 검증, Authentication 저장, 인증되지 않으면 throw Error			- LogoutFilter 전에 해야 logout 시에도 Authentication 사용 가능
+	 * 6. exceptionHandling ->
+	 * 		- AuthenticationEntryPoint 인증되지 않은 사용자가 접근하면 401
+	 * 			- InvalidCookieException, JwtException, AuthenticationException
+	 * 		- AccessDeniedException 인가된 사용자가 접근하면 403
+	 * 7. oauth2Login ->
+	 * 		- userInfoEndpoint -> oauth2 로그인 성공 후 서비스 로직,
+	 * 			- OAuth2UserService: 각 OAuth2 제공자에 맞춰 oauth2User 정보를 가져오는 서비스
+	 * 			- OAuth2User는 Authentication 객체의 일부로 SecurityContextHolder에 저장
+	 * 		- successHandler -> oauth2 로그인 성공 후 서비스 로직
+	 * 			- 로그인 성공 시, 회원 가입 혹은 로그인 후 토큰 쿠키 발행해 response에 추가
+	 *	 	 	- 사용자 인증 과정이 완료 이후 임시 인증 관련 데이터를 세션에서 제거
+	 * 		- failureHandler -> oauth2 로그인 실패 후 서비스 로직
+	 * 			- 로그인 실패 시, error message 발행해 response에 추가
+	 * 8. logout -> /api/logout 요청 시 로그아웃
+	 * 		- LogoutHandler에서 DB의 refreshToken 삭제
+	 * 		- cookie에서 accessToken, refreshToken 삭제
+	 * 		- Security의 Authentication 삭제
+	 * - OAuth2 로그인
+	 * 		- 클라이언트에서 요청하는 링크: http://{localhost:8080}/oauth2/authorization/{kakao}
+	 * 		- 해당 링크로 로그인 요청을 보내면 security가 인가 코드를 받아서 accessToken 발급 후 userInfoEndpoint 이후로 진행
+	 * @param http : HttpSecurity
+	 * @return SecurityFilterChain
+	 * @throws Exception : Exception
+	 */
+	//
+	//
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 		http
-			.csrf(AbstractHttpConfigurer::disable)
-			.httpBasic(AbstractHttpConfigurer::disable) // BasicAuthenticationFilter 사용 X (일회성으로 페이지 불러오는 필터) // formLoginFilter 사용 X
+			.csrf(AbstractHttpConfigurer::disable) // csrf -> token 기반으로 비활성화
+			.httpBasic(
+				AbstractHttpConfigurer::disable) // BasicAuthenticationFilter 사용 X (대신 JWT 토큰, OAuth 방식 사용), formLoginFilter 사용 X(사용자 정의 로그인 페이지 이용)
 			.sessionManagement(sessionManagement -> sessionManagement
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			) // for token
 			.authorizeHttpRequests(request -> request
-				.requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico", "/index.html",
-					"/login.html", "logout.html", "/failure.html", "/api/login", "/login", "/oauth2/**", "/login/oauth2/code/**") // "/static/**" 이건 작동 X
-				.permitAll() // login, logout, static 파일은 누구나 접근 가능
+				.requestMatchers("/api/login", "/oauth2/**",
+					"/login/oauth2/code/**")
+				.permitAll() // /api/login은 test nickname 가입 용도
 				.requestMatchers("/api/**").hasRole("USER")
 				.anyRequest().authenticated())
 
 			.addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
 			.addFilterBefore(authFilter,
-				UsernamePasswordAuthenticationFilter.class) // 토큰 검증하고, Member 정보를 holder에 담아 넘겨 줌.
-			// .exceptionHandling(exceptionHandling -> exceptionHandling
-			// 	.accessDeniedHandler(jwtAccessDeniedHandler)
-			// 	.authenticationEntryPoint(jwtAuthenticationEntryPoint)
-			// )
+				LogoutFilter.class) // 토큰 검증, Authentication 저장, 인증되지 않으면 throw Error // LogoutFilter 전에 해야 logout 시에도 Authentication 사용 가능
+			.exceptionHandling(exceptionHandling -> exceptionHandling
+				.accessDeniedHandler(jwtAccessDeniedHandler)// 인가된 사용자가 접근하면 403
+				.authenticationEntryPoint(jwtAuthenticationEntryPoint) // 인증되지 않은 사용자가 접근하면 401
+			)
 			.oauth2Login(oauth2 -> oauth2
-				// .loginPage("/login.html") // 설정 안 하면 무조건 /login으로 이동, 개발에서만 사용. production에서는 설정 X
-				.userInfoEndpoint(userInfo -> userInfo // userInfo == oauth2User // oauth2 로그인 성공 후 가져올 때의 설정들
+				.userInfoEndpoint(userInfo -> userInfo // userInfo == oauth2User // oauth2 로그인 성공 후 서비스 로직
 					.userService(customOAuth2UserService)
 				)
 				.successHandler(oAuth2LoginSuccessHandler)
 				.failureHandler(oAuth2LoginFailureHandler)
 			)
-			// security가 인가 코드를 아예 받아서 accesstoken까지 주기 때문에 프론트는  oauth/authorize로 로그인 요청을 보내기만 함.
-			// http://localhost:8080/oauth2/authorization/kakao -> 해당 링크로 이동
-			.logout( // 서비스 내 로그아웃만 관리, 소셜 버튼 누르면 로그인 창 뜨지 않고 해당 서비스에 바로 재로그인 가능
+			// 서비스 내 로그아웃만 관리, 소셜 버튼 누르면 로그인 창 뜨지 않고 해당 서비스에 바로 재로그인 가능
+			// session 무효화 X - 다른 데서 이용할 수도 있음. 불필요한 일 X.
+			.logout(
 				logout -> logout
 					.logoutUrl("/api/logout")
-				.addLogoutHandler(oAuth2LogoutCustomHandler) // TODO refreshToken 만료 처리
-				// .invalidateHttpSession(true) // session 무효화 - 다른 데서 이용할 수도 있으니 설정 X
-				.deleteCookies(accessTokenCookieName, refreshTokenCookieName) // cookie 삭제, 톰캣 - JSESSIONID
-				.logoutSuccessHandler(oAuth2LogoutSuccessHandler)
-				.clearAuthentication(true) // SecurityContextHolder.clearContext();
-				.logoutSuccessUrl("/logout.html") // 여기보다 handler 설정이 더 우선, handler 설정 시 이 url은 무시됨. 개발에서만 사용
-				.permitAll()
-			)
-		;
+					.addLogoutHandler(oAuth2LogoutCustomHandler) // 로그아웃 시 refreshToken 삭제 // handler 순서 중요함.
+					.deleteCookies(accessTokenCookieName, refreshTokenCookieName) // cookie 삭제, (JSESSIONID은 톰캣 사용)
+					.clearAuthentication(true) // 순서 상관 X
+					.logoutSuccessHandler(oAuth2LogoutSuccessHandler)
+					.permitAll() // 세션이 만료된 상태 등에서도 로그아웃 가능. (사용자 경험 위해)
+			);
 
 		return http.build();
-
 	}
-
 }
