36a550f63c65362ce32c782417911dfc4fbc6b41
Sooamazing <honang515@gmail.com>
2024-01-25 13:13:44 +0900

    [#51] feat: (PR 반영) Role 생성자로 수정 - Enum 타입은 AllArgsConstructor 적절하지 않아 생성자 선언 - value 필드는 final 선언으로 변경

--- src/main/java/goorm/eagle7/stelligence/domain/member/model/Role.java
+++ src/main/java/goorm/eagle7/stelligence/domain/member/model/Role.java
@@ -1,14 +1,16 @@
 package goorm.eagle7.stelligence.domain.member.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public enum Role {
 	ADMIN("ROLE_ADMIN"), USER("ROLE_USER");
 
-	private String value;
+	private final String value;
+
+	Role(String value) {
+		this.value = value;
+	}
 
 	public static Role getRoleFromString(String roleStr) {
 		try {
