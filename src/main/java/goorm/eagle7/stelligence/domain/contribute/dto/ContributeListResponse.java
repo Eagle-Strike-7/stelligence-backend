2629bd9ed03681f053605be165a1a5b94526a850
eenzzi <purelej99@gmail.com>
2024-01-25 14:24:19 +0900

    [#107] feat: 목록 조회용 ContributeListResponse 생성

---
+++ src/main/java/goorm/eagle7/stelligence/domain/contribute/dto/ContributeListResponse.java
@@ -0,0 +1,35 @@
+package goorm.eagle7.stelligence.domain.contribute.dto;
+
+import goorm.eagle7.stelligence.domain.contribute.model.Contribute;
+import goorm.eagle7.stelligence.domain.contribute.model.ContributeStatus;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+public class ContributeListResponse {
+	private Long contributeId;
+	private ContributeStatus contributeStatus;
+	private String title;
+	private Long documentId;
+	private String documentTitle;
+	private Long contributorId;
+	private String contributorName;
+
+	private ContributeListResponse(Contribute contribute) {
+		this.contributeId = contribute.getId();
+		this.contributeStatus = contribute.getStatus();
+		this.title = contribute.getTitle();
+		this.documentId = contribute.getDocument().getId();
+		this.documentTitle = contribute.getDocument().getTitle();
+		this.contributorId = contribute.getMember().getId();
+		this.contributorName = contribute.getMember().getName();
+	}
+
+	public static ContributeListResponse of(Contribute contribute) {
+		return new ContributeListResponse(contribute);
+	}
+}
