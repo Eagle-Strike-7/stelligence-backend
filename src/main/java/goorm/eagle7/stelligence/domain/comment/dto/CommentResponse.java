95a98894af91928d31732316dafad1e98c9cd346
youngmin <dudals9901@naver.com>
2024-01-28 00:16:32 +0900

    [#113] refactor: CommentResponse DTO 수정

--- src/main/java/goorm/eagle7/stelligence/domain/comment/dto/CommentResponse.java
+++ src/main/java/goorm/eagle7/stelligence/domain/comment/dto/CommentResponse.java
@@ -2,7 +2,6 @@ package goorm.eagle7.stelligence.domain.comment.dto;
 
 import java.time.LocalDateTime;
 
-import goorm.eagle7.stelligence.domain.member.dto.MemberSimpleResponse;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +12,9 @@ public class CommentResponse {
 
 	private Long commentId;
 	private LocalDateTime createdAt;
-	private MemberSimpleResponse createdBy;
+
+	private Long commenterId;
+	private String commenterNickname;
+	private String commenterImageUrl;
 
 }
