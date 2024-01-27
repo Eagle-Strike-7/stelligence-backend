2d875f2a0867a479d3ce64ae6e34a230cb26db58
youngmin <dudals9901@naver.com>
2024-01-27 21:56:15 +0900

    [#113] feat: 토론 닫기 API 개발

--- src/main/java/goorm/eagle7/stelligence/domain/debate/DebateController.java
+++ src/main/java/goorm/eagle7/stelligence/domain/debate/DebateController.java
@@ -2,6 +2,7 @@ package goorm.eagle7.stelligence.domain.debate;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import goorm.eagle7.stelligence.api.ResponseTemplate;
+import goorm.eagle7.stelligence.common.auth.memberinfo.Auth;
+import goorm.eagle7.stelligence.common.auth.memberinfo.MemberInfo;
 import goorm.eagle7.stelligence.domain.debate.dto.DebateResponse;
 import goorm.eagle7.stelligence.domain.debate.dto.DebateSimpleResponse;
 import goorm.eagle7.stelligence.domain.debate.model.DebateStatus;
@@ -33,6 +36,14 @@ public class DebateController {
 	@GetMapping("/{debateId}")
 	public ResponseTemplate<DebateResponse> getDebateDetail(
 		@PathVariable("debateId") Long debateId
+	) {
+		return ResponseTemplate.ok(null);
+	}
+
+	@DeleteMapping("/{debateId}")
+	public ResponseTemplate<Void> closeDebate(
+		@PathVariable("debateId") Long debateId,
+		@Auth MemberInfo memberInfo
 	) {
 		return ResponseTemplate.ok(null);
 	}
