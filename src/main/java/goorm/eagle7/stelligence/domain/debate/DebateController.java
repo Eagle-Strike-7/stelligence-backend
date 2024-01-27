9427c83a92bb7a66d79814e81d31a8856b2c16c9
youngmin <dudals9901@naver.com>
2024-01-27 16:45:29 +0900

    [#113] feat: DebateController 생성

---
+++ src/main/java/goorm/eagle7/stelligence/domain/debate/DebateController.java
@@ -0,0 +1,16 @@
+package goorm.eagle7.stelligence.domain.debate;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequestMapping("/api/debates")
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class DebateController {
+
+
+}
