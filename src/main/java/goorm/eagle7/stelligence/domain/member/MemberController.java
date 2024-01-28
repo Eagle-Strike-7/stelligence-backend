927f1eba4f0863d35e56efc15460e4ef7028ab97
Sooamazing <honang515@gmail.com>
2024-01-29 01:23:39 +0900

    [#120] feat: Swagger 사용 위해 어노테이션 추가.

--- src/main/java/goorm/eagle7/stelligence/domain/member/MemberController.java
+++ src/main/java/goorm/eagle7/stelligence/domain/member/MemberController.java
@@ -16,11 +16,16 @@ import goorm.eagle7.stelligence.domain.member.dto.MemberBadgesResponse;
 import goorm.eagle7.stelligence.domain.member.dto.MemberMiniProfileResponse;
 import goorm.eagle7.stelligence.domain.member.dto.MemberMyPageResponse;
 import goorm.eagle7.stelligence.domain.member.dto.MemberUpdateNicknameRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Tag(name = "Member API", description = "회원 정보를 조회하고, 수정하고, 삭제하는 API를 제공합니다.")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -34,18 +39,37 @@ public class MemberController {
 
 	private final MemberService memberService;
 
+
+	@Operation(summary = "간단한 회원 정보 조회", description = "페이지마다 우측 상단에서 보이는 닉네임과 프로필url를 조회합니다")
+	@ApiResponse(
+		responseCode = "200",
+		description = "간단한 회원 정보 조회 성공",
+		useReturnTypeSchema = true
+	)
 	@GetMapping("/members/me/mini-profile")
 	public ResponseTemplate<MemberMiniProfileResponse> findMiniProfileFromMember(@Auth MemberInfo memberInfo) {
 		MemberMiniProfileResponse memberMiniProfileResponse = memberService.getMiniProfileById(memberInfo.getId());
 		return ResponseTemplate.ok(memberMiniProfileResponse);
 	}
 
+	@Operation(summary = "회원 정보 조회", description = "회원 정보를 조회합니다")
+	@ApiResponse(
+		responseCode = "200",
+		description = "회원 정보 조회 성공",
+		useReturnTypeSchema = true
+	)
 	@GetMapping("/members/me")
 	public ResponseTemplate<MemberMyPageResponse> findMember(@Auth MemberInfo memberInfo) {
 		MemberMyPageResponse memberMyPageResponse = memberService.getMyPageById(memberInfo.getId());
 		return ResponseTemplate.ok(memberMyPageResponse);
 	}
 
+	@Operation(summary = "회원 탈퇴", description = "회원을 삭제합니다.")
+	@ApiResponse(
+		responseCode = "200",
+		description = "회원 탈퇴 성공",
+		useReturnTypeSchema = true
+	)
 	@DeleteMapping("/members/me")
 	public ResponseTemplate<Void> deleteMember(@Auth MemberInfo memberInfo,
 		HttpServletRequest request, HttpServletResponse response) {
@@ -62,8 +86,17 @@ public class MemberController {
 		return ResponseTemplate.ok();
 	}
 
+	@Operation(summary = "회원 가입 이후 닉네임 수정", description = "회원 가입 이후 닉네임을 수정합니다.")
+	@ApiResponse(
+		responseCode = "200",
+		description = "닉네임 수정 성공",
+		useReturnTypeSchema = true
+	)
 	@PutMapping("/members/me/nickname")
-	public ResponseTemplate<Void> updateNickname(@Auth MemberInfo memberInfo, @RequestBody MemberUpdateNicknameRequest memberUpdateNicknameRequest) {
+	public ResponseTemplate<Void> updateNickname(
+		@Auth MemberInfo memberInfo,
+		@Parameter(description = "변경할 닉네임", example = "은하수")
+		@RequestBody MemberUpdateNicknameRequest memberUpdateNicknameRequest) {
 		memberService.updateNickname(memberInfo.getId(), memberUpdateNicknameRequest);
 		return ResponseTemplate.ok();
 	}
--- src/main/java/goorm/eagle7/stelligence/domain/member/dto/MemberUpdateNicknameRequest.java
+++ src/main/java/goorm/eagle7/stelligence/domain/member/dto/MemberUpdateNicknameRequest.java
@@ -1,13 +1,18 @@
 package goorm.eagle7.stelligence.domain.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Schema(description = "수정할 닉네임 정보")
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(staticName = "of")
 public class MemberUpdateNicknameRequest {
+
+	@Schema(description = "수정할 닉네임", example = "은하세계")
 	private String nickname;
+
 }
