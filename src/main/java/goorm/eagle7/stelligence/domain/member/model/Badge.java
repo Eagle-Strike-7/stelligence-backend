7ed9010cb9f3c8b501b3ada459c4ece00925cc80
Sooamazing <honang515@gmail.com>
2024-01-29 02:32:01 +0900

    [#123] feat: badge enum 추가 - title: 사용자에게 보이는 이름 - TODO 메시지 국제화 기능 고려할지 고민

--- src/main/java/goorm/eagle7/stelligence/domain/member/model/Badge.java
+++ src/main/java/goorm/eagle7/stelligence/domain/member/model/Badge.java
@@ -1,5 +1,31 @@
 package goorm.eagle7.stelligence.domain.member.model;
 
+import lombok.Getter;
+
+@Getter
 public enum Badge {
-	WELCOME, SUN, MOON, STAR, CLOUD, RAIN, SNOW, WIND, THUNDER, LIGHTNING, RAINBOW, FOG, HAIL, SLEET, TORNADO, EARTHQUAKE, VOLCANO, TSUNAMI, AVALANCHE, FLOOD, DROUGHT, FIRE, HURRICANE, TYPHOON, BLIZZARD, METEOR, COMET, ASTEROID, GALAXY, UNIVERSE, COSMOS, BLACKHOLE, BIGBANG
+
+	ASTRONAUT("우주 여행자"),
+	MOON("달 탐사 완료"),
+	MERCURY("수성 탐사 완료"),
+	VENUS( "금성 탐사 완료"),
+	MARS("화성 탐사 완료"),
+	JUPITER("목성 탐사 완료"),
+	SATURN("토성 탐사 완료"),
+	URANUS("천왕성 탐사 완료"),
+	NEPTUNE("해왕성 탐사 완료"),
+	PLUTO("명왕성 탐사 완료"),
+	SUN("태양 탐사 완료"),
+	GALAXY("태양계"),
+	ANDROMEDA("안드로메다 정복"),
+	BLACKHOLE("블랙홀"),
+	SPROUT("우주 새싹"),
+	GUARD("우주 방범대");
+
+	private final String title; // 홈페이지에서 사용자가 보는 뱃지 이름
+
+	Badge(String title) {
+		this.title = title;
+	}
+
 }
