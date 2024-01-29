cae5deab6c322d22a8e07751684df2ebb016e5bc
youngmin <dudals9901@naver.com>
2024-01-30 01:02:44 +0900

    [#113] refactor: Debate에서 Comment를 참조하지 않는 단방향 매핑으로 변경

--- src/main/java/goorm/eagle7/stelligence/domain/debate/model/Debate.java
+++ src/main/java/goorm/eagle7/stelligence/domain/debate/model/Debate.java
@@ -1,8 +1,6 @@
 package goorm.eagle7.stelligence.domain.debate.model;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import goorm.eagle7.stelligence.common.entity.BaseTimeEntity;
 import goorm.eagle7.stelligence.domain.contribute.model.Contribute;
@@ -15,7 +13,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -37,9 +34,6 @@ public class Debate extends BaseTimeEntity {
 
 	@Enumerated(EnumType.STRING)
 	private DebateStatus status;
-
-	@OneToMany(mappedBy = "debate")
-	private List<Comment> comments = new ArrayList<>();
 
 	// 종료 예정 시각 (종료된 토론이라면 실제 종료된 시각)
 	private LocalDateTime endAt;
