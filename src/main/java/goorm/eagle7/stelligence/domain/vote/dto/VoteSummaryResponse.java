e3197237e33d95a99cceb0f6242eef2a81d442f2
eenzzi <purelej99@gmail.com>
2024-01-29 17:04:47 +0900

    [#112] refactor: response에 사용자의 투표 상태 추가

--- src/main/java/goorm/eagle7/stelligence/domain/vote/dto/VoteSummaryResponse.java
+++ src/main/java/goorm/eagle7/stelligence/domain/vote/dto/VoteSummaryResponse.java
@@ -1,5 +1,6 @@
 package goorm.eagle7.stelligence.domain.vote.dto;
 
+import goorm.eagle7.stelligence.domain.vote.model.VoteAction;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,4 +11,5 @@ public class VoteSummaryResponse {
 
 	private Long agreeCount;    //찬성 개수
 	private Long disagreeCount;    //반대 개수
+	private VoteAction myVoteAction;    //내 투표 상태
 }
