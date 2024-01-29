d998a8c21e9d7b96aa5d1a80a55f12195c4c6b31
Sooamazing <honang515@gmail.com>
2024-01-30 05:21:16 +0900

    [#120] chore: JavaDocs 문법 이용해 주석 추가 및 수정

--- src/main/java/goorm/eagle7/stelligence/domain/member/MemberService.java
+++ src/main/java/goorm/eagle7/stelligence/domain/member/MemberService.java
@@ -18,13 +18,16 @@ import lombok.extern.slf4j.Slf4j;
 public class MemberService {
 
 	private final MemberRepository memberRepository;
-	private static final String NOT_FOUND_MEMBER_EXCEPTION_MESSAGE =  "해당 멤버를 찾을 수 없습니다. MemberId= %s";
-	// TODO 401 error - 프론트와 어떤 uri가 로그인 필요한 건지 다시 한번 협의
+	private static final String NOT_FOUND_MEMBER_EXCEPTION_MESSAGE =  "해당 멤버를 찾을 수 없습니다. MemberId= %s"; // 서식 문자 사용
 
+	// TODO 401 error - 프론트와 어떤 uri가 로그인 필요한 건지 다시 한번 협의
 	/**
-	 * 회원의 정보를 조회합니다.
+	 * <h2>회원 프로필 조회</h2>
+	 * <p>프로필: 닉네임, 이메일, 프로필 사진, 가입한 소셜 타입</p>
 	 * @param memberId 회원 id
-	 * @return MemberMyPageResponse (닉네임, 이메일, 프로필 사진, 가입한 소셜 타입)
+	 * @return MemberMyPageResponse 닉네임, 이메일, 프로필 사진, 가입한 소셜 타입
+	 * @throws BaseException 회원을 찾을 수 없는 경우 400
+	 * @see MemberService#findMemberById(Long)
 	 */
 	public MemberProfileResponse getProfileById(Long memberId) {
 		Member member = findMemberById(memberId);
@@ -32,7 +35,9 @@ public class MemberService {
 	}
 
 	/**
-	 * 회원 탈퇴 요청시 회원을 삭제합니다.
+	 * <h2>회원 탈퇴 요청 시 회원 삭제</h2>
+	 * <p>- 회원 삭제 시, 글 제외한 해당 회원만 삭제.</p> TODO 모든 정보인지, null 처리 등 확인 필요.
+	 * <p>- 존재하지 않는 회원 id여도 Exception은 발생하지 않음.</p>
 	 * @param memberId 회원 id
 	 */
 	@Transactional
@@ -41,11 +46,13 @@ public class MemberService {
 	}
 
 	/**
-	 * 회원의 닉네임을 수정합니다.
-	 * 이때, 닉네임이 존재하는지를 먼저 조회해 이미 사용 중인 닉네임이면 예외를 발생시킵니다.
-	 * 이후, 사용중이지 않다면 Id로 멤버를 조회해 해당 닉네임으로 닉네임을 변경합니다.
+	 * <h2>회원 닉네임 수정</h2>
+	 * <p>- 닉네임 중복 시 예외 발생</p>
+	 * <p>- 중복이 아니라면 해당 닉네임으로 닉네임 변경</p>
 	 * @param memberId 회원 id
 	 * @param memberUpdateNicknameRequest 수정할 닉네임
+	 * @throws BaseException 회원을 찾을 수 없는 경우 400, TODO 이미 사용 중인 닉네임인 경우 409
+	 * @see MemberService#findMemberById(Long)
 	 */
 	@Transactional
 	public void updateNickname(Long memberId, MemberUpdateNicknameRequest memberUpdateNicknameRequest) {
@@ -61,10 +68,12 @@ public class MemberService {
 	}
 
 	/**
-	 * 회원의 mini profile을 조회합니다.
-	 * mini profile이란, 현재는 오른쪽 상단에 보이는 닉네임과 프로필 사진을 의미합니다.
+	 * <h2>회원의 mini profile 조회</h2>
+	 * <p>- mini profile: 닉네임, 프로필 사진 Url</p>
 	 * @param memberId 회원 id
 	 * @return MemberMiniProfileResponse (닉네임, 프로필 사진)
+	 * @throws BaseException 회원을 찾을 수 없는 경우 400
+	 * @see MemberService#findMemberById(Long)
 	 */
 	public MemberMiniProfileResponse getMiniProfileById(Long memberId) {
 		Member member = findMemberById(memberId);
@@ -75,11 +84,11 @@ public class MemberService {
 	}
 
 	/**
-	 * 회원 id로 회원을 조회합니다.
-	 * 해당 Service에서 findById를 호출하는 경우가 많고, 해당 요구 사항에 대한 Exception을 일관적으로 유지하기 위해 메서드로 분리.
+	 * <h2>회원 id로 회원 조회</h2>
+	 * <p>해당 요구 사항에 대한 일관적인 Exception 유지 및 코드 중복 최소화를 위해 메서드 분리.</p>
 	 * @param memberId 회원 id
 	 * @return Member 회원
-	 * @throws BaseException 회원을 찾을 수 없는 경우
+	 * @throws BaseException 회원을 찾을 수 없는 경우 400
 	 */
 	private Member findMemberById(Long memberId) {
 		return memberRepository.findById(memberId).orElseThrow(
