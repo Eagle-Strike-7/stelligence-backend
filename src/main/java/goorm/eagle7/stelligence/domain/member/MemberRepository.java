8979cc4aae1af370a1a3e51bdb0acd3a5c04f79c
Sooamazing <honang515@gmail.com>
2024-01-30 05:07:40 +0900

    [#120] chore: JavaDocs 문법 이용해 주석 추가 및 수정

--- src/main/java/goorm/eagle7/stelligence/domain/member/MemberRepository.java
+++ src/main/java/goorm/eagle7/stelligence/domain/member/MemberRepository.java
@@ -8,24 +8,24 @@ import goorm.eagle7.stelligence.domain.member.model.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-	/*
-	 * member id로 member 정보를 가져온다.
-	 * member가 있다면, Badge List와 Bookmark List를 함께 가져 옴.
-	 * @Param("id") Member id
-	 * @return Optional<Member>
+	/**
+	 * <h2>Member id로 member를 찾는다.</h2>
+	 * <p>Member와 Badge List, Bookmark List를 Fetch Join으로 함께 가져 옴.</p>
+	 * @param  id Member id
+	 * @return Optional<Member> Member 정보
 	 */
 	// @Query("SELECT m FROM Member m LEFT JOIN FETCH m.badges LEFT JOIN FETCH m.bookmarks WHERE m.id = :id")
 	// Optional<Member> findByIdWithBadgesAndBookmarks(@Param("id") Long id);
 
-	/*
-	 * member nickname으로 member 정보를 가져온다.
-	 * @Param("nickname") Member nickname
-	 * @return Optional<Member>
+	/**
+	 * <h2>Member nickname으로 Member를 찾는다..</h2>
+	 * @param  nickname  Member nickname
+	 * @return Optional<Member> Member 정보
 	 */
 	Optional<Member> findByNickname(String nickname);
 
 	/**
-	 * member nickname으로 member 정보가 존재하는지 확인한다.
+	 * <h2>Member nickname으로 member의 존재를 확인한다.</h2>
 	 * @param nickname	member nickname
 	 * @return  존재하면 true, 존재하지 않으면 false
 	 */
