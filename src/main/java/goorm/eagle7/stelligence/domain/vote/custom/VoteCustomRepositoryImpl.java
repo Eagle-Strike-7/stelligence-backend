1a77073f1c7b3370b66956174a48ab2c924c2e70
eenzzi <purelej99@gmail.com>
2024-01-31 12:02:21 +0900

    [#110] refactor: vote 개수만 가져오게 query 변경

--- src/main/java/goorm/eagle7/stelligence/domain/vote/custom/VoteCustomRepositoryImpl.java
+++ src/main/java/goorm/eagle7/stelligence/domain/vote/custom/VoteCustomRepositoryImpl.java
@@ -1,12 +1,11 @@
 package goorm.eagle7.stelligence.domain.vote.custom;
 
-import java.util.List;
+import java.util.Objects;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import goorm.eagle7.stelligence.domain.vote.VoteSummary;
 import goorm.eagle7.stelligence.domain.vote.model.QVote;
-import goorm.eagle7.stelligence.domain.vote.model.Vote;
 import jakarta.persistence.EntityManager;
 
 public class VoteCustomRepositoryImpl implements VoteCustomRepository {
@@ -22,23 +21,21 @@ public class VoteCustomRepositoryImpl implements VoteCustomRepository {
 
 		QVote vote = QVote.vote;
 
-		// 투표 결과를 가져오는 쿼리
-		List<Vote> totalVotesList = queryFactory
-			.selectFrom(vote)
-			.where(vote.contribute.id.eq(contributeId))
-			.fetch();
-
-		int totalVotes = totalVotesList.size();
+		// 총 투표 수를 가져오는 쿼리
+		Long totalVotes = queryFactory
+			.select(vote.count())
+			.from(vote)
+			.where(vote.contribute.id.eq(contributeId), vote.agree.isNotNull())
+			.fetchOne();
 
 		// 찬성 투표 수를 가져오는 쿼리
-		List<Vote> agreementsList = queryFactory
-			.selectFrom(vote)
-			.where(vote.contribute.id.eq(contributeId), vote.agree.eq((short)1))
-			.fetch();
+		Long agreements = queryFactory
+			.select(vote.count())
+			.from(vote)
+			.where(vote.contribute.id.eq(contributeId), vote.agree.isTrue())
+			.fetchOne();
 
-		int agreements = agreementsList.size();
-
-		return new VoteSummary(contributeId, totalVotes, agreements);
-
+		return new VoteSummary(contributeId, Objects.requireNonNull(totalVotes).intValue(),
+			Objects.requireNonNull(agreements).intValue());
 	}
 }
