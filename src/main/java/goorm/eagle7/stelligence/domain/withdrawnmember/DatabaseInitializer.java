8094ebb3787e2a6cd3f2908291e1b20d014ef2b5
Sooamazing <honang515@gmail.com>
2024-02-01 21:58:51 +0900

    [#142] feat: server 시작 시 테이블 생성 - 테이블이 존재하지 않으면 생성(update) - PK(id)는 auto increment - 기존 member_id는 따로 column 생성 - created_at -> joined_at - withdreawn_at -> now() - 전부 not null

---
+++ src/main/java/goorm/eagle7/stelligence/domain/withdrawnmember/DatabaseInitializer.java
@@ -0,0 +1,36 @@
+package goorm.eagle7.stelligence.domain.withdrawnmember;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class DatabaseInitializer implements CommandLineRunner {
+
+	private final JdbcTemplate jdbcTemplate;
+
+	@Override
+	public void run(String... args) {
+		String sql = """
+			CREATE TABLE IF NOT EXISTS withdrawn_member (
+				withdrawn_member_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+				original_member_id BIGINT NOT NULL,
+				joined_at DATETIME(6) NOT NULL,
+				withdrawn_at DATETIME(6) NOT NULL,
+			    name VARCHAR(255) NOT NULL,
+			    nickname VARCHAR(255) NOT NULL,
+			    email VARCHAR(255) NOT NULL,
+			    social_id VARCHAR(255) NOT NULL,
+			    social_type ENUM('GOOGLE', 'NAVER', 'KAKAO') NOT NULL,
+			    role ENUM('ADMIN', 'USER') NOT NULL,
+			    image_url VARCHAR(255) NOT NULL,
+			    contributes BIGINT NOT NULL
+			)
+			""";
+
+		jdbcTemplate.execute(sql);
+	}
+}
