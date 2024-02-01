24b0574cb1b9b5b02a9d6f9fc61b6c06c656d7da
youngmin <dudals9901@naver.com>
2024-02-01 16:20:18 +0900

    [#145] refactor: 댓글 테스트를 위한 CommentRequest 정적팩토리 메서드 추가

--- src/main/java/goorm/eagle7/stelligence/domain/debate/dto/CommentRequest.java
+++ src/main/java/goorm/eagle7/stelligence/domain/debate/dto/CommentRequest.java
@@ -2,12 +2,14 @@ package goorm.eagle7.stelligence.domain.debate.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Schema(description = "작성/수정할 토론 댓글의 정보")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(staticName = "of")
 public class CommentRequest {
 
 	@Schema(description = "댓글 내용", example = "저도 동의합니다.")
