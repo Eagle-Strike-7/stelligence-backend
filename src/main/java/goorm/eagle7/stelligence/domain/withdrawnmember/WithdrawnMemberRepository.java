c3cd25952d1727d9e2d14cc5302703e19a0a028c
Sooamazing <honang515@gmail.com>
2024-02-01 17:07:10 +0900

    [#142] feat: 탈퇴 회원 저장소 생성

---
+++ src/main/java/goorm/eagle7/stelligence/domain/withdrawnmember/WithdrawnMemberRepository.java
@@ -0,0 +1,8 @@
+package goorm.eagle7.stelligence.domain.withdrawnmember;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import goorm.eagle7.stelligence.domain.withdrawnmember.model.WithdrawnMember;
+
+public interface WithdrawnMemberRepository extends JpaRepository<WithdrawnMember, Long>{
+}
