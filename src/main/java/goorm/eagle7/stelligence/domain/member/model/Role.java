a4da79014b878b89cb4cf569c23ed33be1912275
Sooamazing <honang515@gmail.com>
2024-02-01 16:57:04 +0900

    [#142] feat: role에 WITHDRAWN 추가 - WITHDRAWN: 탈퇴한 사용자

--- src/main/java/goorm/eagle7/stelligence/domain/member/model/Role.java
+++ src/main/java/goorm/eagle7/stelligence/domain/member/model/Role.java
@@ -1,7 +1,13 @@
 package goorm.eagle7.stelligence.domain.member.model;
 
+/**
+ * <h2>사용자 권한</h2>
+ * <p>ADMIN: 관리자</p>
+ * <p>USER: 일반 사용자</p>
+ * <p>WITHDRAWN: 탈퇴한 사용자</p>
+ */
 public enum Role {
-	ADMIN, USER;
+	ADMIN, USER, WITHDRAWN;
 
 	public static Role getRoleFromString(String roleStr) {
 		try {
