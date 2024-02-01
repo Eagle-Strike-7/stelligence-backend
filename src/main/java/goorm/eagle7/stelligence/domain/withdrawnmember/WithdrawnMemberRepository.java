582d1af309eb16998ac96fb03287a32be48dfe23
Sooamazing <honang515@gmail.com>
2024-02-01 22:04:30 +0900

    [#142] feat: WithdrawnMember Entity 대신 run 진행 및 repository에서 직접 진행 - jdbcTemplate으로 query 직접 전달.

--- src/main/java/goorm/eagle7/stelligence/domain/withdrawnmember/WithdrawnMemberRepository.java
+++ src/main/java/goorm/eagle7/stelligence/domain/withdrawnmember/WithdrawnMemberRepository.java
@@ -1,8 +1,35 @@
 package goorm.eagle7.stelligence.domain.withdrawnmember;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
-import goorm.eagle7.stelligence.domain.withdrawnmember.model.WithdrawnMember;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
-public interface WithdrawnMemberRepository extends JpaRepository<WithdrawnMember, Long>{
+import goorm.eagle7.stelligence.domain.member.model.Member;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class WithdrawnMemberRepository {
+
+	private final JdbcTemplate jdbcTemplate;
+
+	public void insertWithdrawnMember(Member member) {
+		String sql = "INSERT INTO withdrawn_member (original_member_id, joined_at, withdrawn_at, name, nickname, email, social_id, social_type, role, image_url, contributes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+		jdbcTemplate.update(sql,
+			member.getId(),
+			Timestamp.valueOf(member.getCreatedAt()),
+			LocalDateTime.now(),
+			member.getName(),
+			member.getNickname(),
+			member.getEmail(),
+			member.getSocialId(),
+			member.getSocialType().name(),
+			member.getRole().name(),
+			member.getImageUrl(),
+			member.getContributes());
+
+	}
 }
