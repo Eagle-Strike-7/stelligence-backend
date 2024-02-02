ec27c4648b44d5fbbece48e1c574aaa630db5b51
Sooamazing <honang515@gmail.com>
2024-02-02 18:57:02 +0900

    [#99] bug: cors 설정 - allowedOrigins 전부 설정

--- src/main/java/goorm/eagle7/stelligence/config/CorsConfig.java
+++ src/main/java/goorm/eagle7/stelligence/config/CorsConfig.java
@@ -15,13 +15,13 @@ public class CorsConfig {
 
 		// 특정 IP, 도메인에 허용
 		// 특정 출처만 허용. 보안을 위해 구체적인 도메인 명시. - 개발 시 localhost로 설정
-		config.setAllowedOrigins(java.util.List.of("http://localhost:8080/"));
+		config.setAllowedOrigins(java.util.List.of("**", "*"));
 
 		// 특정 헤더만 허용. 클라이언트가 서버로 전송할 수 있는 HTTP 헤더를 제한함. "Authorization" 등
 		config.setAllowedHeaders(java.util.List.of("Cache-Control", "Content-Type", "Cookie", "Set-Cookie"));
 
 		// 쿠키와 같은 인증 정보를 포함할 지 여부. 대부분의 경우 false로 설정해야 함.
-		config.setAllowCredentials(false);
+		config.setAllowCredentials(true);
 
 		// 특정 HTTP 메서드만 허용. 필요한 메서드만 명시적으로 허용하여 보안을 강화함.
 		config.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE"));
