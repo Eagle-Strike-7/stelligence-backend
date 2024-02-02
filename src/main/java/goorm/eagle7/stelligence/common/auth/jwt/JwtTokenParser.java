0e98f31e054a5587cb3e930f02827027c4e86f39
Sooamazing <honang515@gmail.com>
2024-02-02 15:55:48 +0900

    [#100] refactor: validator 사용 및 코드 정

--- src/main/java/goorm/eagle7/stelligence/common/auth/jwt/JwtTokenParser.java
+++ src/main/java/goorm/eagle7/stelligence/common/auth/jwt/JwtTokenParser.java
@@ -1,16 +1,9 @@
 package goorm.eagle7.stelligence.common.auth.jwt;
 
-import javax.crypto.SecretKey;
-
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.oauth2.jwt.BadJwtException;
 import org.springframework.stereotype.Service;
 
-import goorm.eagle7.stelligence.common.auth.memberinfo.MemberInfo;
-import goorm.eagle7.stelligence.domain.member.model.Role;
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,46 +15,30 @@ public class JwtTokenParser {
 
 	private final JwtTokenValidator jwtTokenValidator;
 
-	private final SecretKey key;
-
-	@Value("${http.header.field}")
-	private String authorization;
 	@Value("${jwt.claim.role}")
 	private String claimRole;
-	private static final String ERROR_MESSAGE = "유효하지 않은 사용자입니다.";
 
 	/**
-	 * token에서 sub(memberId) 추출
+	 * <h2>token에서 sub(memberId) 추출</h2>
 	 * @param token token
-	 * @return memberId Long
-	 * @throws BadJwtException 유효하지 않은 사용자입니다.
+	 * @return memberId 현재 로그인한 사용자의 memberId
 	 */
-	public Long getSubject(String token) {
-		Claims claims = jwtTokenValidator.validateAndExtractClaims(token);
-		return Long.parseLong(claims.getSubject());
-	}
-
-	public MemberInfo extractMemberInfo(String token) {
-		try {
-			Claims claims = jwtTokenValidator.validateAndExtractClaims(token);
-			return MemberInfo.of(
-				Long.parseLong(claims.getSubject()),
-				Role.getRoleFromString(claims.get(claimRole, String.class)));
-		} catch (NumberFormatException e) {
-			throw new BadJwtException("유효하지 않은 사용자입니다.");
-		}
+	public String getSubject(String token) {
+		return jwtTokenValidator
+			.validateAndExtractClaims(token)
+			.getSubject();
 	}
 
 	/**
-	 * 만료된 토큰에서 subject(memberId) 추출
-	 *  - 만료된 토큰은 claims null이기 때문에 Token으로만 받아야 함.
+	 * <h2>만료된 토큰에서 subject(memberId) 추출</h2>
 	 * @param token 만료된 토큰
 	 * @return String subject(memberId)
 	 */
 	public String extractSubFromExpiredToken(String token) {
 
 		try {
-			return jwtTokenValidator.validateAndExtractClaims(token)
+			return jwtTokenValidator
+				.validateAndExtractClaims(token)
 				.getSubject();
 		} catch (ExpiredJwtException e) {
 			log.debug("만료된 JWT에서 sbj 추출: {}", e.getMessage());
