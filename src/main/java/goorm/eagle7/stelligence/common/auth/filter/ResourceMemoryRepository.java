f6cf1640b4c9d1d94a244f0a64eba6e44e195ae4
Sooamazing <honang515@gmail.com>
2024-02-03 12:31:46 +0900

    [#99] bug: /** 경로 인식하지 못하는 문제 해결 및 주석 정정 - set는 /**까지 set로 인식해 endsWith 사용해 basePath 인식하도록 수 - swagger 경로 인식하지 못해 경로 추가

--- src/main/java/goorm/eagle7/stelligence/common/auth/filter/ResourceMemoryRepository.java
+++ src/main/java/goorm/eagle7/stelligence/common/auth/filter/ResourceMemoryRepository.java
@@ -8,29 +8,68 @@ import org.springframework.stereotype.Repository;
 import lombok.NoArgsConstructor;
 
 /**
- * 인증이 필요한 리소스 정보를 arrayList로 저장해 놓은 객체이나 테스트 용도로 인증 필요없는 리소스만 저장해 놓음.
- * CustomAntPathMatcher에서 사용, 전체를 불러와 매칭할 때 사용함.
+ * <h2>허용 리소스 정보 메모리 저장소</h2>
+ * <p>인증이 필요한 리소스(httpMethod, uri) 정보를 Set으로 저장</p>
+ * <p>인증이 필요한 리소스인지 확인하는 메소드(exist) 제공</p>
  */
 @Repository
 @NoArgsConstructor
 class ResourceMemoryRepository {
 	private static final Set<RequestResource> REQUEST_RESOURCES =
 		Set.of(
-		RequestResource.of(HttpMethod.GET.name(), "/api/documents"),
-		RequestResource.of(HttpMethod.GET.name(), "/api/contributes"),
-		RequestResource.of(HttpMethod.GET.name(), "/api/comments"),
-		RequestResource.of(HttpMethod.GET.name(), "/api/debates"),
 
-		RequestResource.of(HttpMethod.GET.name(), "/oauth2/**"),
-		RequestResource.of(HttpMethod.GET.name(), "/login/oauth2/code/**"),
+			// application 권한
+			RequestResource.of(HttpMethod.GET.name(), "/api/documents"),
+			RequestResource.of(HttpMethod.GET.name(), "/api/contributes"),
+			RequestResource.of(HttpMethod.GET.name(), "/api/comments"),
+			RequestResource.of(HttpMethod.GET.name(), "/api/debates"),
 
-		RequestResource.of(HttpMethod.GET.name(), "/swagger-ui/**"),
-		RequestResource.of(HttpMethod.POST.name(), "/error/**"),
+			// oauth2
+			RequestResource.of(HttpMethod.GET.name(), "/oauth2/authorization/google"),
+			RequestResource.of(HttpMethod.GET.name(), "/login/oauth2/code/**"),
 
-		RequestResource.of(HttpMethod.POST.name(), "/api/login")
-	);
+			// swagger
+			RequestResource.of(HttpMethod.GET.name(), "/swagger-ui/**"),
+			RequestResource.of(HttpMethod.GET.name(), "/swagger-resources/**"),
+			RequestResource.of(HttpMethod.GET.name(), "/v3/api-docs/**"),
 
+			// error
+			RequestResource.of(HttpMethod.POST.name(), "/error/**"),
+
+			// login - dev
+			RequestResource.of(HttpMethod.POST.name(), "/api/login"),
+			RequestResource.of(HttpMethod.GET.name(), "/api/oauth2/**")
+		);
+
+	/**
+	 * <h2>허용하는 uri 확인</h2>
+	 * <p>- 요청의 httpMethod, uri가 리소스 리스트 중 어느 하나라도 일치하는 게 있는지 확인</p>
+	 * <p>- 허용하는 uri가 /**로 끝나면, basePath 비교</p>
+	 * @param requestResource 요청의 httpMethod, uri
+	 * @return boolean 리소스 리스트에 있으면 true, 없으면 false
+	 */
 	public boolean exist(RequestResource requestResource) {
-		return REQUEST_RESOURCES.contains(requestResource);
+
+		for (RequestResource resource : REQUEST_RESOURCES) {
+			// httpMethod가 다르면 다음 리소스로
+			if (!resource.getHttpMethod().equals(requestResource.getHttpMethod())) {
+				continue;
+			}
+
+			// uri가 다르면 다음 리소스로
+			// 허용하는 uri가 /**로 끝나면 basePath만 비교
+			if (resource.getUri().endsWith("/**")) {
+				String basePath = resource.getUri().substring(0, resource.getUri().length() - 3);
+				if (requestResource.getUri().startsWith(basePath)) {
+					return true;
+				}
+			} else {
+				// 그 외의 경우는 uri 전체 비교
+				if (resource.getUri().equals(requestResource.getUri())) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
