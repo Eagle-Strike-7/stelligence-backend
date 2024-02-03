8e14fd873558e6785fb7eb0690ca5a9d4e47176d
Sooamazing <honang515@gmail.com>
2024-02-03 23:57:16 +0900

    [#159] refactor: 빈으로 관리, 쿠키 타입 enum에 따라 내부 로직 수행하도록 변경 - 정적 메서드 사용 대신 Value 사용해 일원화, @Component 사용 - ServletServerHttpRequest는 webflux에서 사용한다고 해 httpservlet으로 변경 - sameSite 정책 고려해 responseCookie는 유지 - cookie 설정도 모두 yml으로 관리 - request, response 파라미터 삭제, cookieType enum 생성, 변경 시 용이하도록 cookie 종류에 따라 내부에서 자체 동작하도록 설정. - 주석 수정

--- src/main/java/goorm/eagle7/stelligence/common/login/CookieUtils.java
+++ src/main/java/goorm/eagle7/stelligence/common/login/CookieUtils.java
@@ -2,33 +2,71 @@ package goorm.eagle7.stelligence.common.login;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
-import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.stereotype.Component;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 /**
- * 쿠키 Util
- * 	- filter, loginController에서 사용
- * 테스트 용이성을 위해 ServletServerHttpResponse 사용
- *
+ * <h2>쿠키 Util</h2>
+ * <p>- 쿠키 생성, 삭제, 조회</p>
+ * <p>- sameSite 정책 고려해 responseCookie 이용</p>
  */
-public final class CookieUtils {
+@Component
+@RequiredArgsConstructor // final 필드가 없지만, 우선 선언. TODO 적절한지 확인
+public class CookieUtils {
 
-	// private static final String COOKIE_DOMAIN = "*"; // TODO 도메인 설정
-	private static final String COOKIE_PATH = "/";
-	private static final String COOKIE_SAME_SITE = "Lax"; // Naver는 Lax여야 로컬에서 동작
-	private static final boolean COOKIE_SECURE = true;
-	private static final boolean COOKIE_HTTP_ONLY = true;
-	private static final String HEADER_NAME = "Set-Cookie"; // 2주
+	@Value("${jwt.accessToken.name}")
+	private String accessTokenCookieName;
+	@Value("${jwt.refreshToken.name}")
+	private String refreshTokenCookieName;
+	@Value("${http.cookie.accessToken.maxAge}")
+	private Long accessCookieMaxAge;
+	@Value("${http.cookie.refreshToken.maxAge}")
+	private Long refreshCookieMaxAge;
+	@Value("${http.cookie.domain}")
+	private String cookieDomain;
+	@Value("${http.cookie.path}")
+	private String cookiePath;
+	@Value("${http.cookie.sameSite}")
+	private String cookieSameSite; // Naver는 Lax여야 로컬에서 동작
+	@Value("${http.cookie.secure}")
+	private boolean cookieSecure;
+	@Value("${http.cookie.httpOnly}")
+	private boolean cookieHttpOnly;
+	@Value("${http.cookie.headerName}")
+	private String headerName;
 
-	private CookieUtils() {
-		throw new IllegalStateException("Utility class");
+	/**
+	 * <h2>쿠키 조회</h2>
+	 * <p>- cookieType으로 구분해 내부에서 처리</p>
+	 * <p>- 쿠키가 없는 경우, 상황에 따라 exception 다를 수 있어 optional로 진행</p>
+	 * @param cookieType 쿠키 타입, accessToken, refreshToken
+	 * @return 쿠키, 없으면 Optional.empty()
+	 * @throws IllegalStateException request가 없는 경우
+	 */
+	public Optional<Cookie> getCookieFromRequest(CookieType cookieType) {
+		return switch (cookieType) {
+			case ACCESS_TOKEN -> getCookie(accessTokenCookieName);
+			case REFRESH_TOKEN -> getCookie(refreshTokenCookieName);
+		};
 	}
 
-	public static Optional<Cookie> getCookieFromCookies(HttpServletRequest request, String cookieName) {
+	/**
+	 * <h2>쿠키 조회</h2>
+	 * <p>쿠키 이름으로 조회</p>
+	 * @param cookieName 쿠키 이름
+	 * @return 쿠키, 없으면 Optional.empty()
+	 * @throws IllegalStateException request가 없는 경우
+	 */
+	private Optional<Cookie> getCookie(String cookieName) {
+
+		HttpServletRequest request = RequestScopeUtil.getHttpServletRequest();
+
 		Cookie[] cookies = request.getCookies();
 		if (cookies != null) {
 			for (Cookie cookie : cookies) {
@@ -39,48 +77,84 @@ public final class CookieUtils {
 		}
 		return Optional.empty();
 	}
+
 	/**
-	 * ResponseCookie 생성 후 response에 쿠키 추가
-	 * ServletServerHttpResponse 사용
-	 * @param response HttpServletResponse
+	 * <h2>쿠키 추가</h2>
+	 * <p>- cookieType으로 구분해 내부에서 처리</p>
+	 * @param cookieType 쿠키 타입, accessToken, refreshToken
+	 * @param value 쿠키 값 - 현재는 토큰 값
+	 * @throws IllegalStateException response가 없는 경우
+	 */
+	public void addCookieBy(CookieType cookieType, String value) {
+
+		switch (cookieType) {
+			case ACCESS_TOKEN -> addCookie(accessTokenCookieName, value, accessCookieMaxAge);
+			case REFRESH_TOKEN -> addCookie(refreshTokenCookieName, value, refreshCookieMaxAge);
+		}
+
+	}
+
+	/**
+	 * <h2>쿠키 추가</h2>
+	 * <p>- 쿠키 이름, 값, 만료 시간으로 추가</p>
 	 * @param name 쿠키 이름
 	 * @param value 쿠키 값
 	 * @param maxAge 쿠키 만료 시간
+	 * @throws IllegalStateException response가 없는 경우
 	 */
-	public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+	private void addCookie(String name, String value, long maxAge) {
 
-		// 테스트 용이성을 위해 ServletServerHttpResponse 사용
-		try (ServletServerHttpResponse servletServerHttpResponse = new ServletServerHttpResponse(response)) {
+		HttpServletResponse response = RequestScopeUtil.getHttpServletResponse();
 
-			ResponseCookie responseCookie = ResponseCookie.from(name, value)
-				.httpOnly(COOKIE_HTTP_ONLY) // XSS 방지
-				.maxAge(maxAge) //
-				// .domain(COOKIE_DOMAIN)
-				.path(COOKIE_PATH)
-				.sameSite(COOKIE_SAME_SITE) // CSRF 방지
-				// .secure(COOKIE_SECURE) // HTTPS
-				.build();
-			servletServerHttpResponse.getServletResponse().addHeader(HEADER_NAME, responseCookie.toString());
-		}
+		ResponseCookie responseCookie = ResponseCookie.from(name, value)
+			.httpOnly(cookieHttpOnly) // XSS 방지
+			.maxAge(maxAge) //
+			.domain(cookieDomain)
+			.path(cookiePath)
+			.sameSite(cookieSameSite) // CSRF 방지
+			.secure(cookieSecure) // HTTPS
+			.build();
+
+		response.addHeader(headerName, responseCookie.toString());
+
 	}
 
-	public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String cookieName) {
+	/**
+	 * <h2>쿠키 삭제</h2>
+	 * <p>- cookieType으로 구분해 내부에서 처리</p>
+	 * @param cookieType 쿠키 타입, accessToken, refreshToken
+	 * @throws IllegalStateException response가 없는 경우
+	 */
+	public void deleteCookieBy(CookieType cookieType) {
+		getCookieFromRequest(cookieType).ifPresent(cookie -> {
+			switch (cookieType) {
+				case ACCESS_TOKEN -> deleteCookie(accessTokenCookieName);
+				case REFRESH_TOKEN -> deleteCookie(refreshTokenCookieName);
+			}
+		});
+	}
 
-		try (ServletServerHttpResponse servletServerHttpResponse = new ServletServerHttpResponse(response)) {
+	/**
+	 * <h2>쿠키 삭제</h2>
+	 * <p>- 쿠키 이름으로 삭제</p>
+	 * @param cookieName 쿠키 이름
+	 * @throws IllegalStateException response가 없는 경우
+	 */
+	private void deleteCookie(String cookieName) {
 
-			getCookieFromCookies(request, cookieName).ifPresent(cookie -> {
-					ResponseCookie responseCookie = ResponseCookie.from(cookieName, "")
-						.httpOnly(COOKIE_HTTP_ONLY) // XSS 방지
-						.maxAge(0)
-						// .domain(COOKIE_DOMAIN)
-						.path(COOKIE_PATH)
-						.sameSite(COOKIE_SAME_SITE) // CSRF 방지
-						// .secure(COOKIE_SECURE) // HTTPS
-						.build();
-					servletServerHttpResponse.getServletResponse().addHeader(HEADER_NAME, responseCookie.toString());
-				}
-			);
-		}
+		HttpServletResponse response = RequestScopeUtil.getHttpServletResponse();
+
+		ResponseCookie responseCookie = ResponseCookie.from(cookieName, "")
+			.httpOnly(cookieHttpOnly) // XSS 방지
+			.maxAge(0)
+			.domain(cookieDomain)
+			.path(cookiePath)
+			.sameSite(cookieSameSite) // CSRF 방지
+			.secure(cookieSecure) // HTTPS
+			.build();
+
+		response.addHeader(headerName, responseCookie.toString());
+
 	}
 
 }
