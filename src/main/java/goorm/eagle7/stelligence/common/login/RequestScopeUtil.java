ec58191ef7be5f96406fd6b0b0cc9abcb0eb3464
Sooamazing <honang515@gmail.com>
2024-02-03 23:07:55 +0900

    [#159] refactor: 현재 스레드의 request, response를 얻는 util 클래스 생성 - TODO filter에서 사용 시 throw에 대한 처리 필요

---
+++ src/main/java/goorm/eagle7/stelligence/common/login/RequestScopeUtil.java
@@ -0,0 +1,37 @@
+package goorm.eagle7.stelligence.common.login;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * <h2>RequestScope Util - 현재 request, response.</h2>
+ * <p>RequestContextHolder를 이용해 request, response를 가져온다.</p>
+ * <p> request가 없으면 IllegalStateException 반환</p>
+ */
+public final class RequestScopeUtil {
+
+	private RequestScopeUtil() {
+		throw new IllegalStateException("Utility class");
+	}
+
+	/**
+	 * <h2>현재 스레드의 HttpServletRequest 가져오기</h2>
+	 * @return HttpServletRequest
+	 * @throws IllegalStateException request가 없으면 IllegalStateException 반환 // TODO Filter에서 request가 없을 때 처리
+	 */
+	public static HttpServletRequest getHttpServletRequest() {
+		return ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
+	}
+
+	/**
+	 * <h2>현재 스레드의 HttpServletResponse 가져오기</h2>
+	 * @return HttpServletResponse
+	 * @throws IllegalStateException request가 없으면 IllegalStateException 반환
+	 */
+	public static HttpServletResponse getHttpServletResponse() {
+		return ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getResponse();
+	}
+}
