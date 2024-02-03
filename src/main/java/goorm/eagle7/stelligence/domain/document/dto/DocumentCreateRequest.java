032d5e35db2fe5882c0a29a1479815defad5a6a0
sbslc2000 <sbslc2000@gmail.com>
2024-02-03 23:19:03 +0900

    [#160] feat: Document 생성시 dto 검증

--- src/main/java/goorm/eagle7/stelligence/domain/document/dto/DocumentCreateRequest.java
+++ src/main/java/goorm/eagle7/stelligence/domain/document/dto/DocumentCreateRequest.java
@@ -1,6 +1,7 @@
 package goorm.eagle7.stelligence.domain.document.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,14 +14,18 @@ import lombok.NoArgsConstructor;
 public class DocumentCreateRequest {
 
 	@Schema(description = "문서의 제목", example = "마리모")
+	@NotBlank(message = "문서의 제목을 입력해주세요.")
 	private String title;
+
 	@Schema(description = "문서의 부모 문서 ID", example = "1")
 	private Long parentDocumentId;
+
 	@Schema(description = "문서의 내용",
 		example = "<h1>마리모</h1>"
 			+ "<p>마리모는 조류의 일종이다.</p>"
 			+ "<h2>마리모의 생애</h2>"
 			+ "<p>마리모의 수명은 평균 1년이다.</p>")
+	@NotBlank(message = "문서의 내용을 입력해주세요.")
 	private String content;
 
 	@Override
