8d2fcf9f37b734550b660772bb937d8e2319d974
Sooamazing <honang515@gmail.com>
2024-02-03 20:30:49 +0900

    [#143] chore: 로그인하지 않았을 경우 null 주석 추가 - 주석 불충분 의견으로 test 후 추가

--- src/main/java/goorm/eagle7/stelligence/common/auth/memberinfo/MemberInfo.java
+++ src/main/java/goorm/eagle7/stelligence/common/auth/memberinfo/MemberInfo.java
@@ -8,7 +8,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
- * MemberInfoArgumentResolver를 이용해 @Auth 애노테이션 사용 시 반환하는 객체
+ * <h2>로그인한 회원 정보</h2>
+ * <p>MemberInfoArgumentResolver를 이용해 @Auth 애노테이션 사용 시 반환하는 객체</p>
+ * <p>MemberInfoContextHolder에 저장해 놓음.</p>
+ * <p>로그인하지 않은 경우(쿠키가 없는 경우), null 반환.</p>
  */
 @Hidden // Swagger 문서에 노출되지 않도록 설정
 @Getter
