f9b7c603253f96f86a0da5a35317d3c8f51cf1f1
eenzzi <purelej99@gmail.com>
2024-02-05 12:33:47 +0900

    [#137] feat: 투표 현황 조회 할 때 필요한 VoteSummary 생성

--- src/main/java/goorm/eagle7/stelligence/domain/vote/dto/VoteSummaryResponse.java
+++ src/main/java/goorm/eagle7/stelligence/domain/vote/dto/VoteSummaryResponse.java
@@ -8,7 +8,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class VoteSummaryResponse {
 
-	private Long agreeCount;    //찬성 개수
-	private Long disagreeCount;    //반대 개수
+	private Integer agreeCount;    //찬성 개수
+	private Integer disagreeCount;    //반대 개수
 	private Boolean myVote; //내 투표 상태
+
+	private VoteSummaryResponse(Integer agreeCount, Integer disagreeCount, Boolean myVote) {
+		this.agreeCount = agreeCount;
+		this.disagreeCount = disagreeCount;
+		this.myVote = myVote;
+	}
+
+	public static VoteSummaryResponse of(Integer agreeCount, Integer disagreeCount, Boolean myVote) {
+		return new VoteSummaryResponse(agreeCount, disagreeCount, myVote);
+	}
 }
