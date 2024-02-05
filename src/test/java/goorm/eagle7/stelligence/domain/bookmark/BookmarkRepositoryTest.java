0b694b159745004c8d11fc4cc90e49a5dbca9266
Sooamazing <honang515@gmail.com>
2024-02-06 02:28:26 +0900

    [#146] test: test 코드 수정 - 생성, 삭제 기능 확인 - WithMockData 사용 - BeforeEach로 member, document, bookmark 미리 생성 - 존재 확인, 조회(순서), 조회, 삭제 시 정상, 예외 확인 - 더 명확하게 실제 검사하는 내용을 표현하기 위해 assertj 사용

--- src/test/java/goorm/eagle7/stelligence/domain/bookmark/BookmarkRepositoryTest.java
+++ src/test/java/goorm/eagle7/stelligence/domain/bookmark/BookmarkRepositoryTest.java
@@ -2,7 +2,8 @@ package goorm.eagle7.stelligence.domain.bookmark;
 
 import static goorm.eagle7.stelligence.config.mockdata.TestFixtureGenerator.*;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,10 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 
-import goorm.eagle7.stelligence.config.mockdata.TestFixtureGenerator;
 import goorm.eagle7.stelligence.config.mockdata.WithMockData;
 import goorm.eagle7.stelligence.domain.bookmark.model.Bookmark;
+import goorm.eagle7.stelligence.domain.document.content.model.Document;
 import goorm.eagle7.stelligence.domain.member.model.Member;
 
 @DataJpaTest
@@ -29,24 +31,78 @@ class BookmarkRepositoryTest {
 	private Bookmark bookmark3;
 	private Bookmark bookmark4;
 
+	private Member stdMember;
+	private Member exMember;
+	private Document document;
+
 	@BeforeEach
 	void setUp() {
 
-		Member member1 = member(1L, "nickname");
+		stdMember = member(1L, "nickname");
+		exMember = member(4L, "nickname2");
+		document = document(1L, stdMember, "title1", 1L);
 
 		// given - memberId가 1L인 북마크 총 4개
-		bookmark1 = bookmarkRepository.findById(1L).get();
-		bookmark2 = bookmarkRepository.findById(5L).get();
-		bookmark3 = bookmarkRepository.findById(8L).get();
-		bookmark4 = bookmarkRepository.findById(10L).get();
+		bookmark1 = bookmarkRepository.findById(1L).get(); // memberId: 1L, documentId: 1L
+		bookmark2 = bookmarkRepository.findById(5L).get(); // memberId: 1L, documentId: 2L
+		bookmark3 = bookmarkRepository.findById(8L).get(); // memberId: 1L, documentId: 3L
+		bookmark4 = bookmarkRepository.findById(10L).get(); // memberId: 1L, documentId: 4L
 
 	}
 
-	// @Test
-	// void existsByMemberIdAndDocumentId() {
-	//
-	// }
+	/**
+	 * <h2>[정상] 북마크 존재 여부 조회</h2>
+	 * <p> - memberId, documentId를 이용해 북마크 존재 여부 조회</p>
+	 * <p> - memberId: 1L, documentId: 1L인 북마크는 존재함</p>
+	 * <p>결과: memberId, documentId가 일치하면 true 반환</p>
+	 * <p>검증 방식: 반환값 확인</p>
+	 */
+	@Test
+	@DisplayName("[정상] 북마크 존재 여부 조회 - existsByMemberIdAndDocumentId")
+	void existsByMemberIdAndDocumentId() {
 
+		// given
+		Long memberId = stdMember.getId();
+		Long documentId = document.getId();
+
+		// when
+		boolean exists = bookmarkRepository.existsByMemberIdAndDocumentId(memberId, documentId);
+
+		// then
+		assertThat(exists).isTrue();
+
+	}
+
+	/**
+	 * <h2>[예외] 북마크 존재 여부 조회</h2>
+	 * <p> - memberId, documentId를 이용해 북마크 존재 여부 조회</p>
+	 * <p> - memberId: 4L, documentId: 1L인 북마크는 존재하지 않음</p>
+	 * <p>결과: memberId, documentId가 일치하지 않으면 false 반환</p>
+	 * <p>검증 방식: 반환값 확인</p>
+	 */
+	@Test
+	@DisplayName("[예외] 북마크 존재 여부 조회 - existsByMemberIdAndDocumentId")
+	void existsByMemberIdAndDocumentIdEx() {
+
+		// given
+		Long memberId = exMember.getId();
+		Long documentId = document.getId();
+
+		// when - {memberId, documentId} == {4L, 1L}인 북마크는 존재하지 않음
+		boolean exists = bookmarkRepository.existsByMemberIdAndDocumentId(memberId, documentId);
+
+		// then
+		assertThat(exists).isFalse();
+
+	}
+
+	/**
+	 * <h2>[정상] 특정 사용자의 북마크 조회, 순서 확인</h2>
+	 * <p> - memberId를 이용해 사용자의 북마크 목록을 조회, Slice로 반환</p>
+	 * <p> - page, size를 받아 조회</p>
+	 * <p>결과: memberId가 1L인 북마크 총 4개 조회, bookmarkId 기준 asc 순서 확인</p>
+	 * <p>검증 방식: 반환값 크기, 순서 확인</p>
+	 */
 	@Test
 	@DisplayName("[정상] 특정 사용자의 북마크 조회, 순서 확인 - findSliceByMemberIdWithPageable")
 	void findSliceByMemberIdWithPageable() {
@@ -58,19 +114,136 @@ class BookmarkRepositoryTest {
 			PageRequest.of(0, 10));
 
 		// then
-		assertNotNull(bookmarkSlice);
-		assertEquals(4, bookmarkSlice.getContent().size());
+		assertThat(bookmarkSlice).isNotNull();
+		assertThat(bookmarkSlice.getContent()).hasSize(4);
 		// bookmarkId 기준 순서
 		assertThat(bookmarkSlice.getContent()).containsExactly(
 			bookmark1, bookmark2, bookmark3, bookmark4
 		);
+
 	}
-	//
-	// @Test
-	// void findByMemberIdAndDocumentId() {
-	// }
-	//
-	// @Test
-	// void deleteByMemberIdAndDocumentId() {
-	// }
+
+	/**
+	 * <h2>[정상] 특정 사용자의 북마크 조회, sort 무시, 순서 확인</h2>
+	 * <p> - memberId를 이용해 사용자의 북마크 목록을 조회, Slice로 반환</p>
+	 * <p> - page, size, sort를 받아 조회</p>
+	 * <p> - sort(document, desc) 무시하고 bookmarkId 기준으로 정렬하는지 확인</p>
+	 * <p>결과: memberId가 1L인 북마크 총 4개 조회, bookmarkId 기준 asc 순서 확인</p>
+	 * <p>검증 방식: 반환값 크기, 순서 확인</p>
+	 */
+	@Test
+	@DisplayName("[정상] 특정 사용자의 북마크 조회, sort 무시, 순서 확인 - findSliceByMemberIdWithPageable")
+	void findSliceByMemberIdWithPageableSort() {
+
+		// given
+
+		// when
+		Slice<Bookmark> bookmarkSlice = bookmarkRepository.findSliceByMemberIdWithPageable(1L,
+			PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "document")));
+
+		// then
+		assertThat(bookmarkSlice).isNotNull();
+		assertThat(bookmarkSlice.getContent()).hasSize(4);
+		// bookmarkId 기준 순서
+		assertThat(bookmarkSlice.getContent()).containsExactly(
+			bookmark1, bookmark2, bookmark3, bookmark4
+		);
+
+	}
+
+	/**
+	 * <h2>[정상] 북마크 단건 조회 - findByMemberIdAndDocumentId</h2>
+	 * <p> - memberId, documentId를 이용해 북마크 단건 조회</p>
+	 * <p>결과: memberId: 1L, documentId: 1L인 북마크 반환</p>
+	 * <p>검증 방식: 기존 저장된 북마크와 같은지 id 비교</p>
+	 */
+	@Test
+	@DisplayName("[정상] 북마크 단건 조회 - findByMemberIdAndDocumentId")
+	void findByMemberIdAndDocumentId() {
+
+		// given
+		Long memberId = stdMember.getId();
+		Long documentId = document.getId();
+
+		// when
+		Bookmark bookmark = bookmarkRepository.findByMemberIdAndDocumentId(memberId, documentId).get();
+
+		// then
+		assertThat(bookmark).isNotNull();
+		assertThat(bookmark.getId()).isEqualTo(1L);
+
+	}
+
+	/**
+	 * <h2>[예외] 없는 북마크 단건 조회 - findByMemberIdAndDocumentId</h2>
+	 * <p> - memberId, documentId를 이용해 북마크 단건 조회</p>
+	 * <p> - memberId: 4L, documentId: 1L인 북마크는 존재하지 않음</p>
+	 * <p>결과: Optional.empty() 반환</p>
+	 * <p>검증 방식: 반환값 확인</p>
+	 */
+	@Test
+	@DisplayName("[예외] 없는 북마크 단건 조회 - findByMemberIdAndDocumentId")
+	void findByMemberIdAndDocumentIdEx() {
+
+		// given
+		Long memberId = exMember.getId();
+		Long documentId = document.getId();
+
+		// when
+		Optional<Bookmark> byMemberIdAndDocumentId = bookmarkRepository.findByMemberIdAndDocumentId(memberId,
+			documentId);
+
+		// then
+		// null X, Optional.empty O, TODO 객체가 비어있다는 것의 의미 확인하기
+		assertThat(byMemberIdAndDocumentId).isEmpty();
+
+	}
+
+	/**
+	 * <h2>[정상] 북마크 삭제 - deleteByMemberIdAndDocumentId</h2>
+	 * <p> - memberId, documentId를 이용해 북마크 삭제</p>
+	 * <p>결과: memberId: 1L, documentId: 1L인 북마크 삭제</p>
+	 * <p>검증 방식: 삭제 후 조회해서 없는지 확인</p>
+	 */
+	@Test
+	@DisplayName("[정상] 북마크 삭제 - deleteByMemberIdAndDocumentId")
+	void deleteByMemberIdAndDocumentId() {
+
+		// given
+		Long memberId = stdMember.getId();
+		Long documentId = document.getId();
+
+		// when
+		bookmarkRepository.deleteByMemberIdAndDocumentId(memberId, documentId);
+
+		// then
+		Optional<Bookmark> none = bookmarkRepository.findByMemberIdAndDocumentId(memberId, documentId);
+		assertThat(none).isEmpty();
+
+	}
+
+	/**
+	 * <h2>[예외] 없는 북마크 삭제 - deleteByMemberIdAndDocumentId</h2>
+	 * <p> - memberId, documentId를 이용해 북마크 삭제</p>
+	 * <p> - memberId: 4L, documentId: 1L인 북마크는 존재하지 않음</p>
+	 * <p> - delete는 void를 반환해 정상 로직과 존재하지 않는 걸 삭제하는 게 같음.</p>
+	 * <p>결과: 삭제 후 조회해서 없는지 확인</p>
+	 */
+	@Test
+	@DisplayName("[예외] 없는 북마크 삭제 - deleteByMemberIdAndDocumentId")
+	void deleteByMemberIdAndDocumentIdEx() {
+
+		// given
+		Long memberId = exMember.getId();
+		Long documentId = document.getId();
+
+		// when
+		bookmarkRepository.deleteByMemberIdAndDocumentId(memberId, documentId);
+
+		// then
+		Optional<Bookmark> none = bookmarkRepository.findByMemberIdAndDocumentId(memberId, documentId);
+		assertThat(none).isEmpty();
+
+	}
+
 }
