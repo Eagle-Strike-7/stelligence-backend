bc50f662b746418b07045621fb5a3fc79a4b5102
eenzzi <purelej99@gmail.com>
2024-02-05 17:38:01 +0900

    [#137] feat: 투표 현황 조회 메서드 추가

--- src/main/java/goorm/eagle7/stelligence/domain/vote/VoteService.java
+++ src/main/java/goorm/eagle7/stelligence/domain/vote/VoteService.java
@@ -8,10 +8,13 @@ import org.springframework.transaction.annotation.Transactional;
 import goorm.eagle7.stelligence.api.exception.BaseException;
 import goorm.eagle7.stelligence.domain.contribute.ContributeRepository;
 import goorm.eagle7.stelligence.domain.contribute.model.Contribute;
+import goorm.eagle7.stelligence.domain.contribute.model.ContributeStatus;
 import goorm.eagle7.stelligence.domain.member.MemberRepository;
 import goorm.eagle7.stelligence.domain.member.model.Member;
 import goorm.eagle7.stelligence.domain.vote.dto.VoteRequest;
+import goorm.eagle7.stelligence.domain.vote.dto.VoteSummaryResponse;
 import goorm.eagle7.stelligence.domain.vote.model.Vote;
+import goorm.eagle7.stelligence.domain.vote.model.VoteSummary;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -32,12 +35,17 @@ public class VoteService {
 		if (voteRequest.getAgree() == null) {
 			throw new BaseException("투표 요청은 찬성(true), 반대(false) 중 하나여야 합니다.");
 		}
-		
+
 		Member member = memberRepository.findById(loginMemberId).orElseThrow(
 			() -> new BaseException("존재하지 않는 회원의 요청입니다. 사용자 ID: " + loginMemberId));
 
 		Contribute contribute = contributeRepository.findById(voteRequest.getContributeId()).orElseThrow(
 			() -> new BaseException("존재하지 않는 Contribute의 요청입니다. Contribute ID: " + voteRequest.getContributeId()));
+
+		// Contribute의 상태가 VOTING인지 확인
+		if (!contribute.getStatus().equals(ContributeStatus.VOTING)) {
+			throw new BaseException("투표가 종료되었거나 진행 중이지 않습니다.");
+		}
 
 		Optional<Vote> existingVote = voteRepository.findByMemberAndContribute(member, contribute);
 
@@ -48,5 +56,36 @@ public class VoteService {
 			Vote vote = Vote.createVote(member, contribute, voteRequest.getAgree());
 			voteRepository.save(vote);
 		}
+	}
+
+	/**
+	 * 투표 현황 조회
+	 * @param contributeId
+	 * @return 투표 현황 (찬성 개수, 반대 개수)
+	 */
+	public VoteSummaryResponse getVoteSummary(Long contributeId, Long loginMemberId) {
+
+		Contribute contribute = contributeRepository.findById(contributeId).orElseThrow(
+			() -> new BaseException("존재하지 않는 Contribute의 요청입니다. Contribute ID: " + contributeId));
+
+		Boolean userVoteStatus = null; //사용자의 투표 상태(기본값: null)
+		if (loginMemberId != null) { //로그인한 경우
+			Member member = memberRepository.findById(loginMemberId).orElseThrow(
+				() -> new BaseException("존재하지 않는 회원의 요청입니다. 사용자 ID: " + loginMemberId));
+
+			Optional<Vote> vote = voteRepository.findByMemberAndContribute(member, contribute);
+
+			if (vote.isPresent()) {
+				userVoteStatus = vote.get().getAgree();
+			}
+		}
+
+		VoteSummary voteSummary = voteRepository.getVoteSummary(contribute.getId());
+
+		return VoteSummaryResponse.of(
+			voteSummary.getAgreeCount(),
+			voteSummary.getDisagreeCount(),
+			userVoteStatus
+		);
 	}
 }
