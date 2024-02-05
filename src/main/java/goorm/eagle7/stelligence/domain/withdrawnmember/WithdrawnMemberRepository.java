ff1bd271f37c66a92471efcc2d0f759bb5c6473e
Sooamazing <honang515@gmail.com>
2024-02-06 03:38:58 +0900

    [#142] refactor: (PR 반영) 많은 파라미터로 NamedParameterJdbcTemplate 사용

--- src/main/java/goorm/eagle7/stelligence/domain/withdrawnmember/WithdrawnMemberRepository.java
+++ src/main/java/goorm/eagle7/stelligence/domain/withdrawnmember/WithdrawnMemberRepository.java
@@ -3,7 +3,8 @@ package goorm.eagle7.stelligence.domain.withdrawnmember;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import goorm.eagle7.stelligence.domain.member.model.Member;
@@ -13,23 +14,51 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WithdrawnMemberRepository {
 
-	private final JdbcTemplate jdbcTemplate;
+	private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	private static final String SQL = """
+		INSERT INTO withdrawn_member (
+		    original_member_id,
+		    joined_at,
+		    withdrawn_at,
+		    name,
+		    nickname,
+		    email,
+		    social_id,
+		    social_type,
+		    role,
+		    image_url,
+		    contributes
+		) VALUES (
+		    :originalMemberId, 
+		    :joinedAt, 
+		    :withdrawnAt, 
+		    :name, 
+		    :nickname, 
+		    :email, 
+		    :socialId, 
+		    :socialType, 
+		    :role, 
+		    :imageUrl, 
+		    :contributes
+		)
+		""";
 
 	public void insertWithdrawnMember(Member member) {
-		String sql = "INSERT INTO withdrawn_member (original_member_id, joined_at, withdrawn_at, name, nickname, email, social_id, social_type, role, image_url, contributes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-		jdbcTemplate.update(sql,
-			member.getId(),
-			Timestamp.valueOf(member.getCreatedAt()),
-			LocalDateTime.now(),
-			member.getName(),
-			member.getNickname(),
-			member.getEmail(),
-			member.getSocialId(),
-			member.getSocialType().name(),
-			member.getRole().name(),
-			member.getImageUrl(),
-			member.getContributes());
+		MapSqlParameterSource parameters = new MapSqlParameterSource()
+			.addValue("originalMemberId", member.getId())
+			.addValue("joinedAt", Timestamp.valueOf(member.getCreatedAt()))
+			.addValue("withdrawnAt", Timestamp.valueOf(LocalDateTime.now()))
+			.addValue("name", member.getName())
+			.addValue("nickname", member.getNickname())
+			.addValue("email", member.getEmail())
+			.addValue("socialId", member.getSocialId())
+			.addValue("socialType", member.getSocialType().name())
+			.addValue("role", member.getRole().name())
+			.addValue("imageUrl", member.getImageUrl())
+			.addValue("contributes", member.getContributes());
+
+		namedParameterJdbcTemplate.update(SQL, parameters);
 
 	}
 }
