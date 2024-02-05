5d93d4cf2b686083282f11c63b305e5513e77612
eenzzi <purelej99@gmail.com>
2024-02-05 12:32:49 +0900

    [#137] feat: getVoteSummary 메서드 추가

--- src/main/java/goorm/eagle7/stelligence/domain/vote/custom/DummyVoteRepository.java
+++ src/main/java/goorm/eagle7/stelligence/domain/vote/custom/DummyVoteRepository.java
@@ -2,7 +2,8 @@ package goorm.eagle7.stelligence.domain.vote.custom;
 
 import org.springframework.stereotype.Component;
 
-import goorm.eagle7.stelligence.domain.vote.VoteResultSummary;
+import goorm.eagle7.stelligence.domain.vote.model.VoteResultSummary;
+import goorm.eagle7.stelligence.domain.vote.model.VoteSummary;
 
 /**
  * 빈을 찾지 못해 에러가 발생하는 것을 방지하기 위해 임시 빈을 등록합니다.
@@ -10,7 +11,12 @@ import goorm.eagle7.stelligence.domain.vote.VoteResultSummary;
 @Component
 public class DummyVoteRepository implements VoteCustomRepository {
 	@Override
-	public VoteResultSummary getVoteSummary(Long contributeId) {
+	public VoteResultSummary getVoteResultSummary(Long contributeId) {
 		return new VoteResultSummary(100L, 100, 80);
+	}
+
+	@Override
+	public VoteSummary getVoteSummary(Long contributeId) {
+		return null;
 	}
 }
--- src/main/java/goorm/eagle7/stelligence/domain/vote/custom/VoteCustomRepositoryImpl.java
+++ src/main/java/goorm/eagle7/stelligence/domain/vote/custom/VoteCustomRepositoryImpl.java
@@ -4,8 +4,9 @@ import java.util.Objects;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
-import goorm.eagle7.stelligence.domain.vote.VoteResultSummary;
 import goorm.eagle7.stelligence.domain.vote.model.QVote;
+import goorm.eagle7.stelligence.domain.vote.model.VoteResultSummary;
+import goorm.eagle7.stelligence.domain.vote.model.VoteSummary;
 import jakarta.persistence.EntityManager;
 
 public class VoteCustomRepositoryImpl implements VoteCustomRepository {
@@ -17,7 +18,7 @@ public class VoteCustomRepositoryImpl implements VoteCustomRepository {
 	}
 
 	@Override
-	public VoteResultSummary getVoteSummary(Long contributeId) {
+	public VoteResultSummary getVoteResultSummary(Long contributeId) {
 
 		QVote vote = QVote.vote;
 
@@ -37,5 +38,28 @@ public class VoteCustomRepositoryImpl implements VoteCustomRepository {
 
 		return new VoteResultSummary(contributeId, Objects.requireNonNull(totalVotes).intValue(),
 			Objects.requireNonNull(agreements).intValue());
+	}
+
+	@Override
+	public VoteSummary getVoteSummary(Long contributeId) {
+
+		QVote vote = QVote.vote;
+
+		// 찬성 투표 수를 가져오는 쿼리
+		Long agreements = queryFactory
+			.select(vote.count())
+			.from(vote)
+			.where(vote.contribute.id.eq(contributeId), vote.agree.isTrue())
+			.fetchOne();
+
+		// 반대 투표 수를 가져오는 쿼리
+		Long disagreements = queryFactory
+			.select(vote.count())
+			.from(vote)
+			.where(vote.contribute.id.eq(contributeId), vote.agree.isFalse())
+			.fetchOne();
+
+		return new VoteSummary(Objects.requireNonNull(agreements).intValue(),
+			Objects.requireNonNull(disagreements).intValue());
 	}
 }
