c5418d531fd45daff376c6f5a901339c565cc0ab
sbslc2000 <sbslc2000@gmail.com>
2024-02-06 23:19:19 +0900

    [#152] feat: Notification의 Table ID를 notification_id로 변경

--- src/main/java/goorm/eagle7/stelligence/domain/notification/model/Notification.java
+++ src/main/java/goorm/eagle7/stelligence/domain/notification/model/Notification.java
@@ -2,6 +2,7 @@ package goorm.eagle7.stelligence.domain.notification.model;
 
 import goorm.eagle7.stelligence.common.entity.BaseTimeEntity;
 import goorm.eagle7.stelligence.domain.member.model.Member;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +20,7 @@ public class Notification extends BaseTimeEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "notification_id")
 	private Long id;
 
 	private String message;
