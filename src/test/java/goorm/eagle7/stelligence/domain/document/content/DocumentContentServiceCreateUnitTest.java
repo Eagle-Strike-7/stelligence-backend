8b2304e782ab3a0c666a1216f5d7f4fdd21b79a4
sbslc2000 <sbslc2000@gmail.com>
2024-02-06 14:54:34 +0900

    [#191] test: parent 가 있는 경우와 없는 경우 문서 생성 테스트

--- src/test/java/goorm/eagle7/stelligence/domain/document/content/DocumentContentServiceCreateUnitTest.java
+++ src/test/java/goorm/eagle7/stelligence/domain/document/content/DocumentContentServiceCreateUnitTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -45,8 +46,8 @@ class DocumentContentServiceCreateUnitTest {
 	DocumentContentService documentContentService;
 
 	@Test
-	@DisplayName("문서 생성 - 성공")
-	void createDocumentSuccess() {
+	@DisplayName("문서 생성 - 부모 문서 없는 경우 - 성공")
+	void createDocumentNoParentSuccess() {
 		//given
 		String title = "title";
 		String rawContent = "# title1\ntestRawContent";
@@ -77,12 +78,41 @@ class DocumentContentServiceCreateUnitTest {
 		assertThat(document.getTitle()).isEqualTo(title);
 		assertThat(document.getAuthor().getNickname()).isEqualTo(nickname);
 		assertThat(document.getSections()).hasSize(2);
+		//부모 문서가 없으므로 null
+		assertThat(document.getParentDocument()).isNull();
 
 		//section의 값이 정상적으로 들어갔는지 확인
 		assertThat(document.getSections().get(0).getHeading()).isEqualTo(Heading.H1);
 		assertThat(document.getSections().get(1).getTitle()).isEqualTo("title2");
 		assertThat(document.getSections().get(0).getOrder()).isEqualTo(1);
 		assertThat(document.getSections().get(1).getOrder()).isEqualTo(2);
+	}
+
+	@Test
+	@DisplayName("문서 생성 - 부모 문서 있는 경우 - 성공")
+	void createDocumentWithParentSuccess() {
+		//given
+		String title = "title";
+		String rawContent = "# title1\ntestRawContent";
+		String nickname = "nickname";
+
+		Member author = member(1L, nickname);
+		Document parent = document(2L, author, "title", 1L);
+
+		//문서 파싱 결과
+		List<SectionRequest> sectionRequests = new ArrayList<>();
+		sectionRequests.add(new SectionRequest(Heading.H1, "title1", "testRawContent\n"));
+		sectionRequests.add(new SectionRequest(Heading.H2, "title2", "content2 line 1\ncontent2 line 2\n"));
+
+		//when
+		when(documentParser.parse(rawContent)).thenReturn(sectionRequests);
+		when(documentContentRepository.findById(2L)).thenReturn(
+			Optional.of(parent));
+		Document document = documentContentService.createDocument(title, rawContent, 2L, author);
+
+		//then
+		//부모 문서가 없으므로 null
+		assertThat(document.getParentDocument()).isEqualTo(parent);
 	}
 
 }
