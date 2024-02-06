152afe7ee1fda237340dac5496456fd55667259b
Sooamazing <honang515@gmail.com>
2024-02-06 15:09:19 +0900

    [#143] test: 커스텀 메서드 test  추가, display name 설정, 케이스 쪼개기 진행 - 각 정상 상황 세분화

--- src/test/java/goorm/eagle7/stelligence/domain/member/MemberRepositoryTest.java
+++ src/test/java/goorm/eagle7/stelligence/domain/member/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -18,13 +19,11 @@ class MemberRepositoryTest {
 
 	@Autowired
 	private MemberRepository memberRepository;
+
 	private Optional<Member> member1;
 	private Optional<Member> member2;
-	private Optional<Member> member3;
-	private Optional<Member> member4;
 	private Optional<Member> withdrawnMember1;
 	private Optional<Member> withdrawnMember2;
-	private Optional<Member> withdrawnMember3;
 
 	@BeforeEach
 	void setUp() {
@@ -32,38 +31,89 @@ class MemberRepositoryTest {
 		// 1~4 line: 회원
 		member1 = memberRepository.findById(1L);
 		member2 = memberRepository.findById(2L);
-		member3 = memberRepository.findById(3L);
-		member4 = memberRepository.findById(4L);
 
 		// 5~7 line: 탈퇴한 회원
 		withdrawnMember1 = memberRepository.findById(5L);
 		withdrawnMember2 = memberRepository.findById(6L);
-		withdrawnMember3 = memberRepository.findById(7L);
 
 	}
 
 	@Test
+	@DisplayName("[정상] Member nickname으로 Member 찾기, 탈퇴 회원 포함 -findByNickname")
 	void findByNickname() {
+
+		// given
+		String memberNickname = member1.get().getNickname();
+		String withdrawnMemberNickname = withdrawnMember1.get().getNickname();
+
+		// when
+		Optional<Member> member = memberRepository.findByNickname(memberNickname);
+		Optional<Member> withdrawnMember = memberRepository.findByNickname(withdrawnMemberNickname);
+
+		// then
+		assertThat(member).isEqualTo(member1);
+		assertThat(withdrawnMember).isEqualTo(withdrawnMember1);
+
 	}
 
 	@Test
+	@DisplayName("[정상] 존재하지 않는 Member nickname으로 Member 찾기, 탈퇴 회원 포함 -findByNickname")
+	void findByNicknameEx() {
+
+		// when
+		Optional<Member> member = memberRepository.findByNickname("nonExistNickname");
+
+		// then
+		assertThat(member).isEmpty();
+
+	}
+
+	@Test
+	@DisplayName("[정상] Member nickname으로 member 존재 확인, 탈퇴 회원 포함 - existsByNickname")
 	void existsByNickname() {
+
+		// given
+		String memberNickname = member1.get().getNickname();
+		String withdrawnMemberNickname = withdrawnMember1.get().getNickname();
+
+		// when
+		boolean memberExists = memberRepository.existsByNickname(memberNickname);
+		boolean withdrawnMemberExists = memberRepository.existsByNickname(withdrawnMemberNickname);
+
+		// then
+		assertThat(memberExists).isTrue();
+		assertThat(withdrawnMemberExists).isTrue();
+
 	}
 
 	@Test
-	void findByIdAndActiveTrue() {
+	@DisplayName("[정상] 활성 Member id로 member 찾기 - findByIdAndActiveTrue")
+	void findByIdAndActive() {
 
 		// given
 
 		// when
 		Optional<Member> activeM = memberRepository.findByIdAndActiveTrue(1L);
-		Optional<Member> inActiveM = memberRepository.findByIdAndActiveTrue(4L);
 
 		// then
 		assertThat(activeM).isNotNull();
 		assertThat(activeM.get().isActive()).isTrue();
 		assertThat(activeM).hasSameHashCodeAs(member1);
+
+	}
+
+	@Test
+	@DisplayName("[정상] 탈퇴한 Member id로 member 찾기 - findByIdAndActiveTrue")
+	void findByIdAndActiveEmpty() {
+
+		// given
+
+		// when
+		Optional<Member> inActiveM = memberRepository.findByIdAndActiveTrue(4L);
+
+		// then
 		assertThat(inActiveM).isEmpty();
 
 	}
+
 }
