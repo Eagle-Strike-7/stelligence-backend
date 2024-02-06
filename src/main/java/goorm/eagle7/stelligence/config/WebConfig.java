4b4bdd5ba4711fb1b4facf3ff30e249e2d937f81
Sooamazing <honang515@gmail.com>
2024-02-06 18:57:43 +0900

    [#195] bug: cors origin 수정

--- src/main/java/goorm/eagle7/stelligence/config/WebConfig.java
+++ src/main/java/goorm/eagle7/stelligence/config/WebConfig.java
@@ -34,7 +34,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOriginPatterns("*")
+			.allowedOriginPatterns("http://localhost:3000", "http://http://3.39.192.156")
 			.allowedMethods("GET", "POST", "PUT", "DELETE")
 			.allowedHeaders("Content-Type", "Set-Cookie")
 			.allowCredentials(true)
