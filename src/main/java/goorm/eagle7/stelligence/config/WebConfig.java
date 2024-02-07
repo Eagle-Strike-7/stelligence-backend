1b6e3b14edce9b8bf3c033a05a2bc0f92d1d2756
Sooamazing <honang515@gmail.com>
2024-02-07 16:38:39 +0900

    [#208] feat: / 잘못 넣은 부분 수정, 줄바꿈.

--- src/main/java/goorm/eagle7/stelligence/config/WebConfig.java
+++ src/main/java/goorm/eagle7/stelligence/config/WebConfig.java
@@ -34,7 +34,10 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000", "http://3.39.192.156:80", "http://www.stelligence.site/")
+			.allowedOrigins(
+				"http://localhost:3000",
+				"http://3.39.192.156:80",
+				"http://www.stelligence.site")
 			.allowedMethods("GET", "POST", "PUT", "DELETE")
 			.allowedHeaders("Content-Type", "Set-Cookie")
 			// .exposedHeaders("Set-Cookie")
