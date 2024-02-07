b37c73acbf93cdb49258d60a3be4b251a062e6cc
Sooamazing <honang515@gmail.com>
2024-02-07 15:11:32 +0900

    [#99] feat: 403 대신 400으로 변경

--- src/main/java/goorm/eagle7/stelligence/common/auth/filter/handler/CustomAccessDeniedHandler.java
+++ src/main/java/goorm/eagle7/stelligence/common/auth/filter/handler/CustomAccessDeniedHandler.java
@@ -8,7 +8,9 @@ import goorm.eagle7.stelligence.api.ResponseTemplate;
 import goorm.eagle7.stelligence.common.auth.filter.ResponseTemplateUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
@@ -20,9 +22,10 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 	public void handle(HttpServletRequest request, HttpServletResponse response,
 		AccessDeniedException accessDeniedException) {
 
-		// 필요한 권한이 없이 접근하려 할때 403 - Admin, user, 다른 사람 정보를 수정하려 할 때 등
+		// 필요한 권한이 없이 접근하려 할때 400으로 통일 - Admin, user, 다른 사람 정보를 수정하려 할 때 등
+		log.debug("CustomAccessDeniedHandler 진입");
 		ResponseTemplate<Void> responseTemplate = ResponseTemplate.fail(accessDeniedException.getMessage());
-		ResponseTemplateUtils.toErrorResponse(response, HttpServletResponse.SC_FORBIDDEN, responseTemplate);
+		ResponseTemplateUtils.toErrorResponse(response, HttpServletResponse.SC_BAD_REQUEST, responseTemplate);
 
 	}
 }
