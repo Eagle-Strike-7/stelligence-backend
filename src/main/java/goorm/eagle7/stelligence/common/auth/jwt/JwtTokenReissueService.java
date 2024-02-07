512bd349db913ed565673d88f0cf6f8b80a52c66
Sooamazing <honang515@gmail.com>
2024-02-08 06:38:59 +0900

    [#99] refactor: 주석, 메서드 이름 수정, log 추가 - 오류 시 로그 보고 파악 위해 debug 수준 로그 각 단계 별 추가 - TODO 트랜잭션 단위 작게 하는 법 고민하기. public에만 트랜잭션 가능, private을 그 안에서 쓰면 어떻게 되는지 확인 필요.

--- src/main/java/goorm/eagle7/stelligence/common/auth/jwt/JwtTokenReissueService.java
+++ src/main/java/goorm/eagle7/stelligence/common/auth/jwt/JwtTokenReissueService.java
@@ -13,77 +13,83 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * accessToken 만료인 경우, 이용하는 service
- *  - refreshToken의 유효성을 검증하고 재발급한다.
- * 1. refreshToken 만료 확인 -> 만료면 실패
- * 2. 쿠키의 refreshToken과 DB의 refreshToken의 동등성 확인. -> 다르면 실패
- * 3. 만료가 아니면 accessToken 재발급, 보안상 이유로 refreshToken도 재발급 및 저장
- *  -> 성공 시 accessToken 재발급
- *  -> 실패 시 로그인 필요, throw BadJwtException
+ * <h2>accessToken 만료인 경우, 이용하는 service</h2>
+ * <p>- 성공 시 accessToken 재발급, 실패 시 throws, 재로그인</p>
+ * <p>- refreshToken의 유효성을 검증하고 재발급한다.</p>
+ * <p>1. refreshToken 만료 확인 -> 만료면 실패</p>
+ * <p>2. 쿠키의 refreshToken과 DB의 refreshToken의 동등성 확인. -> 다르면 실패</p>
+ * <p>3. 만료가 아니면 accessToken 재발급, 보안상 이유로 refreshToken도 재발급 및 저장</p>
  *  TODO refreshToken 기한 확인해서 Update할지 정하는 게 좋은지
+ *  TODO Transactional 작게 적용하는 법
  */
 @Slf4j
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class JwtTokenReissueService {
 
-	private final JwtTokenProvider jwtTokenProvider;
-	private final JwtTokenService jwtTokenService;
-	private final MemberRepository memberRepository;
 	private final CookieUtils cookieUtils;
+	private final JwtTokenService jwtTokenService;
+	private final JwtTokenProvider jwtTokenProvider;
+	private final MemberRepository memberRepository;
 	private static final String ERROR_MESSAGE = "유효하지 않은 사용자입니다.";
 
 	/**
-	 * - accessToken 재발급, 불가 시 throw BadJwtException
-	 *
-	 * - refresh 토큰 만료 확인
-	 * 		   -> 유효하다면
-	 * 		 	   -> DB와 동일한 refreshToken인지 확인
-	 * 		       -> accessToken 재발급, 보안상 이유로 refreshToken 재발급 및 저장
-	 * 		   -> 만료라면
-	 * 		      throw BadJwtException
-	 * @param refreshToken refreshToken
-	 * @return accessToken
+	 * <h2>refreshToken 검증해 accessToken 재발급</h2>
+	 * <p>- refreshToken이 만료되었는지 확인</p>
+	 * <p>- refreshToken이 DB에 저장된 refreshToken과 일치하지 않는지 확인</p>
+	 * <p>- accessToken, refreshToken 재발급, 불가 시 throw BadJwtException</p>
+	 * @param refreshToken 검증할 refreshToken
+	 * @return accessToken 재발급한 accessToken
+	 * @throws UsernameNotFoundException memberId에 해당하는 회원이 없습니다.
+	 * @throws BadJwtException refreshToken이 만료되었습니다.
 	 */
+	@Transactional
 	public String reissueAccessToken(String refreshToken) {
 
-		// refreshToken 만료 확인, 만료라면 throw BadJwtException
-		jwtTokenService.validateTokenOrThrows(refreshToken);
+		log.debug("refreshToken 재발급 시작");
 
-		// memberId 추출
+		// refreshToken 검증 및 member 조회
 		Long memberId = jwtTokenService.getMemberId(refreshToken);
+		Member member = memberRepository
+			.findByIdAndActiveTrue(memberId)
+			.orElseThrow(() -> {
+				log.debug("memberId에 해당하는 회원이 없습니다.");
+				return new UsernameNotFoundException(ERROR_MESSAGE);
+			});
 
-		// DB에서 refreshToken 가져와 문자열 비교(동등성 확인), 다르면 throw BadJwtException
-		Member member = memberRepository.findById(memberId)
-			.orElseThrow(() -> new UsernameNotFoundException(ERROR_MESSAGE));
-		String refreshTokenFromDB = member.getRefreshToken();
-		validateTokenEquality(refreshToken, refreshTokenFromDB);
+		// DB에서 refreshToken 가져와 문자열 비교(동등성 확인), 다르면 throw ex (401)
+		validateTokenEquality(refreshToken, member.getRefreshToken());
 
 		// accessToken 재발급 및 쿠키에 추가 후 accessToken 반환
-		return reissueTokensAndSaveOnCookieAndDb(member);
+		return reissueTokens(member);
 
 	}
 
 	/**
-	 * refreshToken이 DB에 저장된 refreshToken과 일치하지 않는지 확인
+	 * <h2>refreshToken이 DB에 저장된 것과 동일한지 확인</h2>
 	 * @param refreshToken 쿠키의 refreshToken
 	 * @param refreshTokenFromDB DB에 저장된 refreshToken
 	 * @throws BadJwtException refreshToken이 DB에 저장된 refreshToken과 일치하지 않으면 throw
 	 */
 	private void validateTokenEquality(String refreshToken, String refreshTokenFromDB) {
+
 		if (!refreshToken.equals(refreshTokenFromDB)) {
 			log.debug("refreshToken이 DB에 저장된 refreshToken과 일치하지 않습니다.");
 			throw new BadJwtException(ERROR_MESSAGE);
 		}
+
 	}
 
 	/**
-	 * accessToken, refreshToken 재발급 후 쿠키와 DB에 저장
-	 * @param member 회원
-	 * @return accessToken 재발급
+	 * <h2>accessToken, refreshToken 재발급</h2>
+	 * <p>- accessToken, refreshToken 재발급 후 쿠키와 DB에 저장</p>
+	 * @param member token으로 검증한 회원
+	 * @return accessToken 재발급한 accessToken
 	 */
-	private String reissueTokensAndSaveOnCookieAndDb(Member member) {
+	private String reissueTokens(Member member) {
+
+		log.debug("accessToken, refreshToken 재발급 진행");
+
 		Long memberId = member.getId();
 
 		// Token 재발급
@@ -94,10 +100,11 @@ public class JwtTokenReissueService {
 		cookieUtils.addCookieBy(CookieType.ACCESS_TOKEN, newAccessToken);
 		cookieUtils.addCookieBy(CookieType.REFRESH_TOKEN, newRefreshToken);
 
-		// DB에 저장
+		// DB에 리프레시 토큰 업데이트
 		member.updateRefreshToken(newRefreshToken);
 
 		return newAccessToken;
+
 	}
 
 }
