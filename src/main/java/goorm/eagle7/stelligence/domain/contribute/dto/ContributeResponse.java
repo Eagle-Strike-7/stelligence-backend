f77772ce383f8857682bd38facd43f99dfa4317f
youngmin <dudals9901@naver.com>
2024-02-07 16:17:24 +0900

    [#203] refactor: 수정요청 요청 응답에 추가해야하는 필드 추가

--- src/main/java/goorm/eagle7/stelligence/domain/contribute/dto/ContributeResponse.java
+++ src/main/java/goorm/eagle7/stelligence/domain/contribute/dto/ContributeResponse.java
@@ -1,9 +1,13 @@
 package goorm.eagle7.stelligence.domain.contribute.dto;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 
 import goorm.eagle7.stelligence.domain.amendment.dto.AmendmentResponse;
 import goorm.eagle7.stelligence.domain.contribute.model.Contribute;
+import goorm.eagle7.stelligence.domain.contribute.model.ContributeStatus;
 import goorm.eagle7.stelligence.domain.member.dto.MemberDetailResponse;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -12,6 +16,10 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ContributeResponse {
+
+	@Value("${contribute.scheduler.vote-expiration-minutes:30}")
+	long voteExpirationMinutes;	// 투표 지속시간
+
 	private Long contributeId;
 	private String contributeTitle;
 	private String contributeDescription;
@@ -26,6 +34,14 @@ public class ContributeResponse {
 	private String beforeParentDocumentTitle;
 	private Long afterParentDocumentId;	//변경된 상위 문서
 	private String afterParentDocumentTitle;
+
+	// 추가된 필드
+	private ContributeStatus contributeStatus;	//현재 수정요청의 상태
+	private String documentTitle;	//수정요청과 관련 없이 현재 제목
+	private Long parentDocumentId;	//수정요청과 관련 없이 현재 상위 문서
+	private String parentDocumentTitle;
+	private Long relatedDebateId;	//연관된 토론
+	private LocalDateTime endAt;
 
 	private ContributeResponse(Contribute contribute) {
 		this.contributeId = contribute.getId();
@@ -49,6 +65,15 @@ public class ContributeResponse {
 		this.afterParentDocumentTitle = contribute.getAfterParentDocument() == null ?
 			null : contribute.getAfterParentDocument().getTitle();
 
+		// 추가된 필드
+		this.contributeStatus = contribute.getStatus();
+		this.documentTitle = contribute.getDocument().getTitle();
+		this.parentDocumentId = contribute.getDocument().getParentDocument() == null ?
+			null : contribute.getDocument().getParentDocument().getId();
+		this.parentDocumentTitle = contribute.getDocument().getParentDocument() == null ?
+			null : contribute.getDocument().getParentDocument().getTitle();
+		this.relatedDebateId = contribute.getRelatedDebate().getId();
+		this.endAt = contribute.getCreatedAt().plusMinutes(voteExpirationMinutes);
 	}
 
 	public static ContributeResponse of(Contribute contribute) {
