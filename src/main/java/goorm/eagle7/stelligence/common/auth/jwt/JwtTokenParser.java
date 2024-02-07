c5ebf73b5b813a86eb243e65b60bd4faa5a9118f
Sooamazing <honang515@gmail.com>
2024-02-08 07:47:47 +0900

    [#99] feat: token 값 추출 메서드 및 log 추가 - 마찬가지로 service 대신 component로 변경

--- src/main/java/goorm/eagle7/stelligence/common/auth/jwt/JwtTokenParser.java
+++ src/main/java/goorm/eagle7/stelligence/common/auth/jwt/JwtTokenParser.java
@@ -1,14 +1,19 @@
 package goorm.eagle7.stelligence.common.auth.jwt;
 
+import org.springframework.stereotype.Component;
+
+import goorm.eagle7.stelligence.domain.member.model.Role;
 import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 // TODO extends JwtDefaultParser
 @Slf4j
+@Component
 @RequiredArgsConstructor
 class JwtTokenParser {
 
+	private final JwtProperties jwtProperties;
 	private final JwtTokenValidator jwtTokenValidator;
 
 	/**
@@ -17,9 +22,22 @@ class JwtTokenParser {
 	 * @return memberId 현재 로그인한 사용자의 memberId
 	 */
 	public String getSubject(String token) {
+		log.debug("token에서 sub(memberId) 추출");
 		return jwtTokenValidator
 			.validateAndExtractClaims(token)
 			.getSubject();
+	}
+
+	/**
+	 * <h2>토큰에서 claims(role) 추출</h2>
+	 * @param token 토큰
+	 * @return Role claims(role)
+	 */
+	public Role getRole(String token) {
+		log.debug("토큰에서 claims(role) 추출");
+		return Role.fromValue(jwtTokenValidator
+			.validateAndExtractClaims(token)
+			.get(jwtProperties.getClaims().getValue(), String.class));
 	}
 
 	/**
