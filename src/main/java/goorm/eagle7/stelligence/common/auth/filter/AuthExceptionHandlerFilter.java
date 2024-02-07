006b87340605f8bc54b6d47ea8454a6012a8d0ea
Sooamazing <honang515@gmail.com>
2024-02-07 15:11:14 +0900

    [#99] feat: auth filter의 인증 관련 ex 401로 응답 보내는 filter 생성 - TODO security 자체 ex handler 사용법 찾기

---
+++ src/main/java/goorm/eagle7/stelligence/common/auth/filter/AuthExceptionHandlerFilter.java
@@ -0,0 +1,33 @@
+package goorm.eagle7.stelligence.common.auth.filter;
+
+import java.io.IOException;
+
+import javax.security.sasl.AuthenticationException;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import goorm.eagle7.stelligence.api.ResponseTemplate;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class AuthExceptionHandlerFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+		try {
+			filterChain.doFilter(request, response);
+		} catch (AuthenticationException e) {
+			log.debug("AuthenticationException catched in AuthExceptionHandlerFilter : {}", e.getMessage());
+			ResponseTemplateUtils.toErrorResponse(response, HttpServletResponse.SC_UNAUTHORIZED, ResponseTemplate.fail(e.getMessage()));
+		}
+	}
+
+}
