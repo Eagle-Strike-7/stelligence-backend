aff973074de38e05e0d95b05c565faaa98f8847d
Sooamazing <honang515@gmail.com>
2024-02-07 22:22:00 +0900

    [#165] feat: 각 도큐먼트에서 조회 후 추가하는 메서드 생성 - badge 내에서 조회 - badge 찾아지지 않는 경우 그대로 종료, 찾아지면 badge 추가 - 영속성 컨텍스트 문제로 member의 메서드로 구현.

--- src/main/java/goorm/eagle7/stelligence/domain/badge/BadgeService.java
+++ src/main/java/goorm/eagle7/stelligence/domain/badge/BadgeService.java
@@ -3,10 +3,10 @@ package goorm.eagle7.stelligence.domain.badge;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import goorm.eagle7.stelligence.domain.badge.model.Badge;
 import goorm.eagle7.stelligence.domain.badge.model.BadgeCategory;
 import goorm.eagle7.stelligence.domain.contribute.ContributeRepository;
 import goorm.eagle7.stelligence.domain.document.content.DocumentContentRepository;
-import goorm.eagle7.stelligence.domain.member.MemberRepository;
 import goorm.eagle7.stelligence.domain.member.model.Member;
 import lombok.RequiredArgsConstructor;
 
@@ -17,27 +17,56 @@ public class BadgeService {
 
 	private final DocumentContentRepository documentContentRepository;
 	private final ContributeRepository contributeRepository;
-	private final MemberRepository memberRepository;
 
 	public void getBadge(BadgeCategory badgeCategory, Member member) {
+
+		Badge newBadge = null;
 
 		// badgeCategory와 같은지 확인
 		switch (badgeCategory) {
 			case WRITING:
 				// 글 작성
+				newBadge = checkWritingConditionAndGetBadge(member, badgeCategory);
 				break;
 			case CONTRIBUTE:
 				// 수정 요청
+				newBadge = checkContributeConditionAndGetBadge(member, badgeCategory);
 				break;
 			case MEMBER_JOIN:
 				// 회원 가입
+				newBadge = checkMemberConditionAndGetBadge();
 				break;
 			case REPORT:
-				// 신고
+				// 신고 - 신고 테이블 따로 없어 미구현
 				break;
 		}
 
-		// 같은 badge를 찾고, count를 찾아 해당 count 이하인 badge를 member의 badges에 추가
+		if (newBadge != null) {
+			member.addBadge(newBadge);
+		}
 
 	}
+
+	private Badge checkWritingConditionAndGetBadge(Member member, BadgeCategory badgeCategory) {
+
+		long count = documentContentRepository.countDistinctByAuthor_Id(member.getId());
+		return Badge.findByEventCategoryAndCount(badgeCategory, count);
+
+	}
+
+	private Badge checkContributeConditionAndGetBadge(Member member, BadgeCategory badgeCategory) {
+
+		long count = contributeRepository.countDistinctByMemberId(member.getId());
+		return Badge.findByEventCategoryAndCount(badgeCategory, count);
+
+	}
+
+	// merge 추가 구현 필요
+
+
+	private Badge checkMemberConditionAndGetBadge() {
+		// 최초 회원 가입 검증 로직 따로 없어 미구현
+		 return Badge.SPROUT;
+	}
+
 }
