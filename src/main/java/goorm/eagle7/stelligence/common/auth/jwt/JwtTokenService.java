05a188c15bbde820e826e2b3abb017a9b0699402
Sooamazing <honang515@gmail.com>
2024-02-08 07:52:22 +0900

    [#99] refactor: util 제거 및 service 수정 - 주석 추가 및 정리 - parser, validator private-package로 관리 - TODO parser만 의존하게 변경하는 게 적절할지 고민

--- src/main/java/goorm/eagle7/stelligence/common/auth/jwt/JwtTokenService.java
+++ src/main/java/goorm/eagle7/stelligence/common/auth/jwt/JwtTokenService.java
@@ -1,50 +1,41 @@
 package goorm.eagle7.stelligence.common.auth.jwt;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.jwt.BadJwtException;
 import org.springframework.security.oauth2.jwt.JwtException;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import goorm.eagle7.stelligence.common.auth.memberinfo.MemberInfo;
 import goorm.eagle7.stelligence.domain.member.model.Role;
-import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * JwtTokenService
- * 역할: JWT 토큰의 파싱과 검증
- * 책임:
- * 	- 토큰의 유효성 검증
- *  - 토큰에서 사용자 정보 추출
- *    - HttpServletRequest에서 JWT 추출 (개발 시만)
- * log: 비즈니스 로직 상 error가 아니기 때문에 log.debug로 설정
+ * <h2>JWT 토큰의 파싱과 검증</h2>
+ * <p>- 토큰의 유효성 검증</p>
+ * <p>- 토큰에서 사용자 정보 추출</p>
+ * <p>log: 비즈니스 로직 상 error가 아니기 때문에 log.debug로 설정</p>
  *
  * getMemberId - token에서 sub(memberId) 추출
- * validateTokenOrThrows - void 반환(throw BadJwtException O)
- * isTokenValidated - boolean 반환(throw BadJwtException X)
- *  - validateExistsToken - token 자체가 null, empty, blank인지 확인
- *  - validateActiveToken - 만료 확인
- *  * getClaimsOrThrows - 코드 중복을 줄이기(private)
+ * isTokenValidated - boolean 반환, token 유효성 검사
  * makeAuthenticationFromToken - token 정보에서 Authentication 만들어 반환
  * getTokenFromCookie - 유효한 쿠키에서 토큰 추출(null 포함 반환)
+ * extractMemberInfo - token에서 MemberInfo로 조립
+ * private validateTokenOrThrows - token 유효성 검사, 유효하지 않다면 throws
+ * getMemberIdFromExpiredToken - 만료된 토큰에서 memberId 추출
  */
 @Slf4j
-@Service
+@Component
 @RequiredArgsConstructor
 public class JwtTokenService {
 
-	private final JwtTokenValidator jwtTokenValidator;
 	private final JwtTokenParser jwtTokenParser;
-
-	@Value("${jwt.claim.role}")
-	private String claimRole;
-	private static final String ERROR_MESSAGE = "유효하지 않은 사용자입니다.";
+	private final JwtTokenValidator jwtTokenValidator;
 
 	/**
 	 * <h2>token에서 sub(memberId) 추출</h2>
@@ -58,61 +49,78 @@ public class JwtTokenService {
 	}
 
 	/**
-	 * token의 유효성 검사, 유효하지 않다면 throw BadJwtException
+	 * <h2>token 유효성 검사</h2>
+	 * <p>- token이 null, empty, blank인지 확인</p>
+	 * <p>- 서명 검증 및 만료 검증</p>
 	 * @param token 검사할 token
-	 *              - null, empty, blank 검사
-	 *              - 서명 검증 및 만료 검증
-	 * @throws BadJwtException 유효하지 않은 사용자입니다.
-	 */
-	public void validateTokenOrThrows(String token) {
-		jwtTokenValidator.validateAndExtractClaims(token);
-	}
-
-	/**
-	 * token의 유효성 검사, 유효하지 않다면 false 반환
-	 * @param token 검사할 token
-	 *              - null, empty, blank 검사
-	 * 	 *          - 서명 검증 및 만료 검증
 	 * @return boolean 유효한 토큰이면 true, 아니면 false
-	 *
 	 */
 	public boolean isTokenValidated(String token) {
 		try {
-			jwtTokenValidator.validateAndExtractClaims(token);
+			validateTokenOrThrows(token);
 			return true;
-		} catch (JwtException e) {
+		} catch (JwtException | IllegalArgumentException e) {
 			return false;
 		}
 	}
 
 	/**
-	 * token 정보에서 Authentication 만들어 반환
-	 * @param token token
-	 * @return Authentication token 정보에서 만든 Authentication
-	 * @throws BadJwtException 유효하지 않은 사용자입니다.
+	 * <h2>token 정보에서 Authentication 만들어 반환</h2>
+	 * @param token 추출할 token
+	 * @return Authentication token 정보로 만든 Authentication
+	 * @throws JwtException 유효하지 않은 사용자입니다.
 	 */
 	public Authentication makeAuthenticationFromToken(String token) {
 
-		Claims claims = jwtTokenValidator.validateAndExtractClaims(token);
+		MemberInfo memberInfo = extractMemberInfo(token);
 
 		UserDetails user = User.builder()
-			.username(claims.getSubject())
+			.username(memberInfo.getId().toString())
 			.password("")
-			.authorities(Role.fromValue(claims.get(claimRole, String.class)).getValue())
+			.authorities(memberInfo.getRole().getValue())
 			.build();
 
 		return new UsernamePasswordAuthenticationToken(user, "", user.getAuthorities());
 	}
 
 	/**
-	 * 쿠키에서 토큰 추출(null 포함)
+	 * <h2>쿠키에서 토큰 추출(null 포함)</h2>
 	 * @param cookie 유효한 쿠키
 	 * @return token 쿠키에서 추출한 토큰, 없다면 null 반환
 	 */
 	public String getTokenFromCookie(Cookie cookie) {
 
+		if (cookie == null) {
+			return null;
+		}
 		return cookie.getValue();
 
+	}
+
+	/**
+	 * <h2>token에서 MemberInfo로 조립</h2>
+	 * @param token 추출할 token
+	 * @return MemberInfo memberId, role
+	 */
+	public MemberInfo extractMemberInfo(String token) {
+
+		String memberId = jwtTokenParser.getSubject(token);
+		Role role = jwtTokenParser.getRole(token);
+
+		return MemberInfo.of(
+			Long.parseLong(memberId),
+			role);
+
+	}
+
+	/**
+	 * <h2>token 유효성 검사, 유효하지 않다면 throws</h2>
+	 * @param token 검사할 token
+	 * @throws JwtException 유효하지 않은 사용자입니다.
+	 * @throws IllegalArgumentException 토큰 값이 없습니다.
+	 */
+	private void validateTokenOrThrows(String token) {
+		jwtTokenValidator.validateAndExtractClaims(token);
 	}
 
 	// 하기 메서드는 dev에서 사용
@@ -124,20 +132,6 @@ public class JwtTokenService {
 	 */
 	public String getMemberIdFromExpiredToken(String token) {
 		return jwtTokenParser.extractSubFromExpiredToken(token);
-	}
-
-	/**
-	 * <h2>token에서 MemberInfo로 조립</h2>
-	 * @param token 추출할 token
-	 * @return MemberInfo memberId, role
-	 */
-	public MemberInfo extractMemberInfo(String token) {
-
-		Claims claims = jwtTokenValidator.validateAndExtractClaims(token);
-		return MemberInfo.of(
-			Long.parseLong(claims.getSubject()),
-			Role.fromValue(claims.get(claimRole, String.class)));
-
 	}
 
 }
