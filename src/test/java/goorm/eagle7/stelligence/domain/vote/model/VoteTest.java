04aa41ed8058aa06ccc51d1cd46f12de2aa85fc2
eenzzi <purelej99@gmail.com>
2024-02-09 00:46:56 +0900

    [#187] test: updateAgree 테스트 작성

--- src/test/java/goorm/eagle7/stelligence/domain/vote/model/VoteTest.java
+++ src/test/java/goorm/eagle7/stelligence/domain/vote/model/VoteTest.java
@@ -28,4 +28,79 @@ class VoteTest {
 
 		assertThat(vote.getAgree()).isTrue();
 	}
+
+	@Test
+	@DisplayName("반대한 상태에서 찬성한다.")
+	void disagreeToAgree() {
+
+		Vote vote = Vote.createVote(null, null, false);
+
+		vote.updateAgree(true);
+
+		assertThat(vote.getAgree()).isTrue();
+	}
+
+	@Test
+	@DisplayName("반대를 취소한다.")
+	void disagreeToCancel() {
+
+		Vote vote = Vote.createVote(null, null, false);
+
+		vote.updateAgree(false);
+
+		assertThat(vote.getAgree()).isNull();
+	}
+
+	@Test
+	@DisplayName("취소한 상태에서 반대한다.")
+	void cancelToDisagree() {
+
+		Vote vote = Vote.createVote(null, null, null);
+
+		vote.updateAgree(false);
+
+		assertThat(vote.getAgree()).isFalse();
+	}
+
+	@Test
+	@DisplayName("찬성한 상태에서 반대한다.")
+	void agreeToDisagree() {
+
+		Vote vote = Vote.createVote(null, null, true);
+
+		vote.updateAgree(false);
+
+		assertThat(vote.getAgree()).isFalse();
+	}
+
+	@Test
+	@DisplayName("실패 - null로 updateAgree 호출")
+	void failUpdateAgree() {
+
+		Vote vote = Vote.createVote(null, null, null);
+
+		assertThatThrownBy(() -> vote.updateAgree(null))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	@DisplayName("실패 - null로 updateAgree 호출")
+	void failUpdateAgree2() {
+
+		Vote vote = Vote.createVote(null, null, true);
+
+		assertThatThrownBy(() -> vote.updateAgree(null))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	@DisplayName("실패 - null로 updateAgree 호출")
+	void failUpdateAgree3() {
+
+		Vote vote = Vote.createVote(null, null, false);
+
+		assertThatThrownBy(() -> vote.updateAgree(null))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
 }
