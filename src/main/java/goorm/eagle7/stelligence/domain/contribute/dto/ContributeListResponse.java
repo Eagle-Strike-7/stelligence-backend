d643ad382e076050097af019697a665883e2520a
eenzzi <purelej99@gmail.com>
2024-02-08 12:17:29 +0900

    [#187] refactor: ContributeListResponse애 voteSummary 추가

--- src/main/java/goorm/eagle7/stelligence/domain/contribute/dto/ContributeListResponse.java
+++ src/main/java/goorm/eagle7/stelligence/domain/contribute/dto/ContributeListResponse.java
@@ -2,6 +2,7 @@ package goorm.eagle7.stelligence.domain.contribute.dto;
 
 import goorm.eagle7.stelligence.domain.contribute.model.Contribute;
 import goorm.eagle7.stelligence.domain.contribute.model.ContributeStatus;
+import goorm.eagle7.stelligence.domain.vote.model.VoteSummary;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,8 +18,9 @@ public class ContributeListResponse {
 	private String documentTitle;
 	private Long contributorId;
 	private String contributorNickname;
+	private VoteSummary voteSummary;
 
-	private ContributeListResponse(Contribute contribute) {
+	private ContributeListResponse(Contribute contribute, VoteSummary voteSummary) {
 		this.contributeId = contribute.getId();
 		this.contributeTitle = contribute.getTitle();
 		this.contributeDescription = contribute.getDescription();
@@ -27,9 +29,10 @@ public class ContributeListResponse {
 		this.documentTitle = contribute.getDocument().getTitle();
 		this.contributorId = contribute.getMember().getId();
 		this.contributorNickname = contribute.getMember().getNickname();
+		this.voteSummary = voteSummary;
 	}
 
-	public static ContributeListResponse of(Contribute contribute) {
-		return new ContributeListResponse(contribute);
+	public static ContributeListResponse of(Contribute contribute, VoteSummary voteSummary) {
+		return new ContributeListResponse(contribute, voteSummary);
 	}
 }
