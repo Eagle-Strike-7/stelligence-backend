2237d85cdd7701f3b7104b6107275bec5bb5ec66
Sooamazing <honang515@gmail.com>
2024-02-08 15:06:52 +0900

    [#182] chore: 카테고리, 조건 별 순서 재정렬

--- src/main/java/goorm/eagle7/stelligence/domain/badge/model/Badge.java
+++ src/main/java/goorm/eagle7/stelligence/domain/badge/model/Badge.java
@@ -9,36 +9,32 @@ public enum Badge {
 
 	// enum type, value 필드, 생성자 순서가 일치해야 함.
 
+	/** 글 작성 Badge 목록 **/
 	ASTRONAUT("우주 여행자", WRITING, "첫 글 작성 완료", 1, "astronaut.png"),
-
 	MOON("달 탐사 완료", WRITING, "글 5개 작성 완료", 5, "moon.png"),
-
-	MERCURY("수성 탐사 완료", CONTRIBUTE, "수정 1번 요청 완료", 1, "mercury.png"),
-
-	VENUS("금성 탐사 완료", CONTRIBUTE, "수정 5번 요청 완료", 5, "venus.png"),
-
 	MARS("화성 탐사 완료", WRITING, "글 10개 작성 완료", 10, "mars.png"),
-
-	JUPITER("목성 탐사 완료", CONTRIBUTE, "수정 요청 1개 반영 완료", 1, "jupiter.png"),
-
-	SATURN("토성 탐사 완료", CONTRIBUTE, "수정 요청 5개 반영 완료", 5, "saturn.png"),
-
 	URANUS("천왕성 탐사 완료", WRITING, "글 20개 작성 완료", 20, "uranus.png"),
 
+	/** 수정 요청 Badge 목록 **/
+	MERCURY("수성 탐사 완료", CONTRIBUTE, "수정 1번 요청 완료", 1, "mercury.png"),
+	VENUS("금성 탐사 완료", CONTRIBUTE, "수정 5번 요청 완료", 5, "venus.png"),
 	NEPTUNE("해왕성 탐사 완료", CONTRIBUTE, "수정 10번 요청 완료", 10, "neptune.png"),
-
-	PLUTO("명왕성 탐사 완료", CONTRIBUTE, "수정 요청 10개 반영 완료", 10, "pluto.png"),
-
 	SUN("태양 탐사 완료", CONTRIBUTE, "수정 30번 요청 완료", 30, "sun.png"),
-
 	GALAXY("태양계", CONTRIBUTE, "수정 50번 요청 완료", 50, "galaxy.png"),
 
+	/** 수정 요청 반영 Badge 목록 **/
+	JUPITER("목성 탐사 완료", CONTRIBUTE, "수정 요청 1개 반영 완료", 1, "jupiter.png"),
+	SATURN("토성 탐사 완료", CONTRIBUTE, "수정 요청 5개 반영 완료", 5, "saturn.png"),
+	PLUTO("명왕성 탐사 완료", CONTRIBUTE, "수정 요청 10개 반영 완료", 10, "pluto.png"),
 	ANDROMEDA("안드로메다 정복", CONTRIBUTE, "수정 50번 반영 완료", 50, "andromeda.png"),
 
+	/** 반려 Badge 목록 **/
 	BLACKHOLE("블랙홀", CONTRIBUTE, "수정 n번 반려 완료", 100, "blackhole.png"),
 
+	/** 회원 가입 Badge 목록 **/
 	SPROUT("우주 새싹", MEMBER_JOIN, "회원 가입 완료", 1, "sprout.png"),
 
+	/** 신고 Badge 목록 **/
 	GUARD("우주 방범대", REPORT, "신고 10회 완료", 10, "guard.png");
 
 	private final String title; // 홈페이지에서 사용자가 보는 배지 이름
