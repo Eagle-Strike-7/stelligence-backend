a4ef58b12633226c03f4a4f42623317a53b82972
sbslc2000 <sbslc2000@gmail.com>
2024-02-09 18:51:34 +0900

    [#218] feat: ContributeEventAware 패키지 변경

--- src/main/java/goorm/eagle7/stelligence/domain/contribute/ContributeEventAware.java
+++ src/main/java/goorm/eagle7/stelligence/domain/contribute/ContributeEventAware.java
@@ -1,5 +1,6 @@
-package goorm.eagle7.stelligence.domain.notification;
+package goorm.eagle7.stelligence.domain.contribute;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.context.event.EventListener;
@@ -8,11 +9,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import goorm.eagle7.stelligence.common.event.contribute.ContributeMergedEvent;
-import goorm.eagle7.stelligence.domain.contribute.ContributeRepository;
 import goorm.eagle7.stelligence.domain.contribute.model.Contribute;
+import goorm.eagle7.stelligence.domain.notification.NotificationRequest;
+import goorm.eagle7.stelligence.domain.notification.NotificationSender;
 import goorm.eagle7.stelligence.domain.vote.VoteRepository;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * 수정요청과 관련된 이벤트를 처리합니다.
+ *
+ * <p>Async: 이벤트를 전달받아 이벤트 발생 쓰레드와는 별도의 쓰레드에서 처리합니다.
+ */
 @Async
 @Transactional
 @Component
@@ -32,21 +39,26 @@ public class ContributeEventAware {
 	 */
 	@EventListener(ContributeMergedEvent.class)
 	public void onContributeMerged(ContributeMergedEvent event) {
-		Set<Long> voters = voteRepository.findVoters(event.contributeId());
-
 		Contribute contribute = contributeRepository.findWithMember(event.contributeId())
 			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 수정요청입니다."));
 
+		//알림을 받을 대상을 찾는다.
+		Set<Long> targets = new HashSet<>();
+
+		targets.addAll(voteRepository.findVoters(event.contributeId()));
+		targets.add(contribute.getMember().getId());
+
 		//알림 요청 객체를 생성한다.
-		String slicedTitle = contribute.getTitle().length() > 10 ? contribute.getTitle().substring(0, 10) + "..." :
+		String slicedTitle = contribute.getTitle().length() > 20 ? contribute.getTitle().substring(0, 20) + "..." :
 			contribute.getTitle();
 
 		NotificationRequest request = NotificationRequest.of(
 			String.format(CONTRIBUTE_MERGED_MESSAGE, slicedTitle),
 			String.format(DOCUMENT_URI, contribute.getDocument().getId()),
-			voters
+			targets
 		);
 
+		//알림을 보낸다.
 		notificationSender.send(request);
 	}
 }
