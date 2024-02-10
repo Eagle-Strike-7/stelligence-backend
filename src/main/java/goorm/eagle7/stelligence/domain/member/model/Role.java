3687913cf6e8bc273e7a3e37c854c3d3966a9a7e
Sooamazing <honang515@gmail.com>
2024-02-10 16:09:55 +0900

    [#231] fix: Role 메서드 잘못 구현한 내용 정정 - fromValue는 value에서 role을 찾아야 함. - 그간 기본값이 USER라 알아채지 못함... - security에서 해당 value 필요해 추가 - 다른 브랜치에서도 변경했으나 재변경 필요해 test와 함께 수정.

--- src/main/java/goorm/eagle7/stelligence/domain/member/model/Role.java
+++ src/main/java/goorm/eagle7/stelligence/domain/member/model/Role.java
@@ -1,18 +1,30 @@
 package goorm.eagle7.stelligence.domain.member.model;
 
+import java.util.Arrays;
+
 /**
  * <h2>사용자 권한</h2>
  * <p>ADMIN: 관리자</p>
  * <p>USER: 일반 사용자</p>
  */
 public enum Role {
-	ADMIN, USER;
+	ADMIN("ROLE_ADMIN"), USER("ROLE_USER");
 
-	public static Role getRoleFromString(String roleStr) {
+	private final String value;
+
+	Role(String value) {
+		this.value = value;
+	}
+
+	public static Role fromValue(String value) {
 		try {
-			return Role.valueOf(roleStr.toUpperCase());
+			return Arrays.stream(values())
+				.filter(r -> r.value.equalsIgnoreCase(value))
+				.findAny()
+				.orElseThrow(IllegalArgumentException::new);
 		} catch (IllegalArgumentException e) {
 			return Role.USER;
 		}
 	}
+
 }
