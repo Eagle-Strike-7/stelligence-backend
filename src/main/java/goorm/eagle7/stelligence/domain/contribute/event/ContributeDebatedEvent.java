5ae533c794b9449b932d9e4d23194d6fc5340497
sbslc2000 <sbslc2000@gmail.com>
2024-02-11 22:33:48 +0900

    [#238] feat: ContributeDebated 발생시 알림 전송

--- src/main/java/goorm/eagle7/stelligence/domain/contribute/event/ContributeDebatedEvent.java
+++ src/main/java/goorm/eagle7/stelligence/domain/contribute/event/ContributeDebatedEvent.java
@@ -2,8 +2,8 @@ package goorm.eagle7.stelligence.domain.contribute.event;
 
 /**
  * Contribute에 관련된 토론이 진행될 때 발생하는 이벤트
- * @param contributeId Contribute id
+ * @param debateId 토론 ID
  */
-public record ContributeDebatedEvent(Long contributeId) {
+public record ContributeDebatedEvent(Long debateId) {
 
 }
