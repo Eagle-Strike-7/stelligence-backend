ded1ac95e6be9972a29a6bdb1212eb20f902a597
youngmin <dudals9901@naver.com>
2024-02-12 20:52:29 +0900

    [#242] docs: Swagger 추가

--- src/main/java/goorm/eagle7/stelligence/domain/report/ReportController.java
+++ src/main/java/goorm/eagle7/stelligence/domain/report/ReportController.java
@@ -10,6 +10,9 @@ import goorm.eagle7.stelligence.api.ResponseTemplate;
 import goorm.eagle7.stelligence.common.auth.memberinfo.Auth;
 import goorm.eagle7.stelligence.common.auth.memberinfo.MemberInfo;
 import goorm.eagle7.stelligence.domain.report.dto.ReportRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -19,9 +22,16 @@ public class ReportController {
 
 	private final ReportService reportService;
 
+	@Operation(summary = "문서 신고", description = "특정 문서를 신고합니다.")
+	@ApiResponse(
+		responseCode = "200",
+		description = "문서 신고 성공",
+		useReturnTypeSchema = true
+	)
 	@PostMapping("/documents/{documentId}")
 	public ResponseTemplate<Void> reportDocument(
 		@RequestBody ReportRequest reportRequest,
+		@Parameter(description = "신고할 문서의 ID", example = "1")
 		@PathVariable(name = "documentId") Long documentId,
 		@Auth MemberInfo memberInfo) {
 
@@ -29,9 +39,16 @@ public class ReportController {
 		return ResponseTemplate.ok();
 	}
 
+	@Operation(summary = "토론 댓글 신고", description = "토론 댓글을 신고합니다.")
+	@ApiResponse(
+		responseCode = "200",
+		description = "토론 댓글 신고 성공",
+		useReturnTypeSchema = true
+	)
 	@PostMapping("/comments/{commentId}")
 	public ResponseTemplate<Void> reportComment(
 		@RequestBody ReportRequest reportRequest,
+		@Parameter(description = "신고할 토론 댓글의 ID", example = "1")
 		@PathVariable(name = "commentId") Long commentId,
 		@Auth MemberInfo memberInfo) {
 
--- src/main/java/goorm/eagle7/stelligence/domain/report/dto/ReportRequest.java
+++ src/main/java/goorm/eagle7/stelligence/domain/report/dto/ReportRequest.java
@@ -1,5 +1,6 @@
 package goorm.eagle7.stelligence.domain.report.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,8 +9,10 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(staticName = "from")
+@Schema(description = "신고 대상에 대한 부가 정보")
 public class ReportRequest {
 
+	@Schema(description = "신고 사유", example = "욕설이 포함된 글입니다.")
 	private String description;
 
 }
