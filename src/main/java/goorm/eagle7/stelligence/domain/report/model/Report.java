4283f87bd84c8e2fa88a778e916296dd59b0eb65
youngmin <dudals9901@naver.com>
2024-02-12 17:55:17 +0900

    [#242] refactor: Report 엔티티에 Getter 추가

--- src/main/java/goorm/eagle7/stelligence/domain/report/model/Report.java
+++ src/main/java/goorm/eagle7/stelligence/domain/report/model/Report.java
@@ -12,9 +12,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "report_type")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
