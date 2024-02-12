5483ec082e0ebc4bfcb8098953e6258c1787dccf
youngmin <dudals9901@naver.com>
2024-02-12 18:26:01 +0900

    [#242] fix: ReportRequest의 abstract 제거하고 테스트를 위한 정적팩토리 메서드 추가

--- src/main/java/goorm/eagle7/stelligence/domain/report/dto/ReportRequest.java
+++ src/main/java/goorm/eagle7/stelligence/domain/report/dto/ReportRequest.java
@@ -1,12 +1,14 @@
 package goorm.eagle7.stelligence.domain.report.dto;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public abstract class ReportRequest {
+@AllArgsConstructor(staticName = "from")
+public class ReportRequest {
 
 	private String description;
 
