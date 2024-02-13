db751165a13f93d9d473129beedf1100fcffb35c
Sooamazing <honang515@gmail.com>
2024-02-13 22:59:39 +0900

    [#265] chore: 불필요한 import 삭제

--- src/main/java/goorm/eagle7/stelligence/config/WebConfig.java
+++ src/main/java/goorm/eagle7/stelligence/config/WebConfig.java
@@ -2,9 +2,7 @@ package goorm.eagle7.stelligence.config;
 
 import java.util.List;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
