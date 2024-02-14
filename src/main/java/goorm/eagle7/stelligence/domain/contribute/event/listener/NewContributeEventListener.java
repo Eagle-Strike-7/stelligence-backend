4bd3afd16e5beaac223d2d25d89c2a32f3f4724b
Sooamazing <honang515@gmail.com>
2024-02-15 07:26:13 +0900

    feat: contribute reject, merge 이벤트 리스터 구

---
+++ src/main/java/goorm/eagle7/stelligence/domain/contribute/event/listener/NewContributeEventListener.java
@@ -0,0 +1,36 @@
+package goorm.eagle7.stelligence.domain.contribute.event.listener;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import goorm.eagle7.stelligence.domain.badge.BadgeService;
+import goorm.eagle7.stelligence.domain.badge.model.BadgeCategory;
+import goorm.eagle7.stelligence.domain.contribute.ContributeRepository;
+import goorm.eagle7.stelligence.domain.contribute.event.NewContributeEvent;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class NewContributeEventListener {
+
+	private final BadgeService badgeService;
+	private final ContributeRepository contributeRepository;
+
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	@TransactionalEventListener(value = NewContributeEvent.class)
+	public void onContributeNew(NewContributeEvent event) {
+
+		if (contributeRepository.existsById(event.contributeId())) {
+
+			contributeRepository.findMemberById(event.contributeId())
+				.ifPresent(member ->
+					badgeService.checkAndAwardBadge(BadgeCategory.CONTRIBUTE_ALL, member)
+				);
+
+		}
+
+	}
+
+}
