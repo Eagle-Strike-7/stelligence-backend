824c35380703efbed7e3f4beec7ebbc4a47c4981
Sooamazing <honang515@gmail.com>
2024-02-15 08:30:33 +0900

    [#283] chore: 파라미터 변

--- src/main/java/goorm/eagle7/stelligence/domain/badge/strategy/ContributeAllBadgeAwardStrategy.java
+++ src/main/java/goorm/eagle7/stelligence/domain/badge/strategy/ContributeAllBadgeAwardStrategy.java
@@ -10,34 +10,32 @@ import org.springframework.stereotype.Component;
 import goorm.eagle7.stelligence.domain.badge.model.Badge;
 import goorm.eagle7.stelligence.domain.badge.model.BadgeCategory;
 import goorm.eagle7.stelligence.domain.contribute.ContributeRepository;
-import goorm.eagle7.stelligence.domain.member.model.Member;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class ContributeAllBadgeAwardStrategy implements BadgeAwardStrategy {
 
-    private final ContributeRepository contributeRepository;
+	private final ContributeRepository contributeRepository;
 
-    @Override
-    public boolean supports(BadgeCategory category) {
-        return BadgeCategory.CONTRIBUTE_ALL.equals(category);
-    }
+	@Override
+	public boolean supports(BadgeCategory category) {
+		return BadgeCategory.CONTRIBUTE_ALL.equals(category);
+	}
 
+	public long getCount(Long memberId) {
+		return contributeRepository.countByMemberId(memberId);
+	}
 
-    public long getCount(Member member) {
-        return contributeRepository.countByMemberId(member.getId());
-    }
-
-    @Override
-    public Map<Integer, Badge> getRequiredCounts() {
-        Map<Integer, Badge> requiredCounts = new HashMap<>();
-        requiredCounts.put(1, MERCURY);
-        requiredCounts.put(5, VENUS);
-        requiredCounts.put(10, NEPTUNE);
-        requiredCounts.put(30, SUN);
-        requiredCounts.put(50, GALAXY);
-        return requiredCounts;
-    }
+	@Override
+	public Map<Integer, Badge> getRequiredCounts() {
+		Map<Integer, Badge> requiredCounts = new HashMap<>();
+		requiredCounts.put(1, MERCURY);
+		requiredCounts.put(5, VENUS);
+		requiredCounts.put(10, NEPTUNE);
+		requiredCounts.put(30, SUN);
+		requiredCounts.put(50, GALAXY);
+		return requiredCounts;
+	}
 
 }
