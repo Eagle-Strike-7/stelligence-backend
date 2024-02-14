4686528b2ed31bc05045bc6b396b462b29221938
eenzzi <purelej99@gmail.com>
2024-02-14 14:56:44 +0900

    [#263] refactor: 수정요청 목록 조회 시 찬반 개수도 바로 받아 오도록 변경

--- src/main/java/goorm/eagle7/stelligence/domain/contribute/custom/CustomContributeRepository.java
+++ src/main/java/goorm/eagle7/stelligence/domain/contribute/custom/CustomContributeRepository.java
@@ -5,16 +5,18 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import goorm.eagle7.stelligence.domain.contribute.dto.ContributeSimpleResponse;
 import goorm.eagle7.stelligence.domain.contribute.model.Contribute;
 import goorm.eagle7.stelligence.domain.contribute.model.ContributeStatus;
 
 public interface CustomContributeRepository {
 
-	Page<Contribute> findByContributeStatus(ContributeStatus status, Pageable pageable);
+	Page<ContributeSimpleResponse> findByContributeStatus(ContributeStatus status, Pageable pageable);
 
-	Page<Contribute> findCompleteContributes(Pageable pageable);
+	Page<ContributeSimpleResponse> findCompleteContributes(Pageable pageable);
 
-	Page<Contribute> findByDocumentAndStatus(Long documentId, boolean merged, Pageable pageable);
+	Page<ContributeSimpleResponse> findByDocumentAndStatus(Long documentId, boolean merged, Pageable pageable);
 
 	Optional<Contribute> findLatestContributeByDocumentId(Long documentId);
+
 }
