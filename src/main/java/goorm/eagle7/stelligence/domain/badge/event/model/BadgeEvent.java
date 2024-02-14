0c523c6436ee67a3ef776f3ea973ba44f91f6f37
Sooamazing <honang515@gmail.com>
2024-02-15 07:43:35 +0900

    [#283] feat: record 타입이라 getter 따로 사용 X

--- src/main/java/goorm/eagle7/stelligence/domain/badge/event/model/BadgeEvent.java
+++ src/main/java/goorm/eagle7/stelligence/domain/badge/event/model/BadgeEvent.java
@@ -1,8 +1,6 @@
 package goorm.eagle7.stelligence.domain.badge.event.model;
 
 import goorm.eagle7.stelligence.domain.badge.model.BadgeCategory;
-import lombok.Getter;
 
-@Getter
 public record BadgeEvent (Long memberId, BadgeCategory badgeCategory) {
 }
