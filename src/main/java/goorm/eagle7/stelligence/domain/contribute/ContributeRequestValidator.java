973b766a258f55bbc33620342f5bfff6e2767be2
youngmin <dudals9901@naver.com>
2024-02-15 13:02:15 +0900

    [#232] feat: 수정요청 대기중인 문서에 대한 검증 로직 추가

--- src/main/java/goorm/eagle7/stelligence/domain/contribute/ContributeRequestValidator.java
+++ src/main/java/goorm/eagle7/stelligence/domain/contribute/ContributeRequestValidator.java
@@ -16,7 +16,6 @@ import goorm.eagle7.stelligence.domain.amendment.dto.AmendmentRequest;
 import goorm.eagle7.stelligence.domain.amendment.model.AmendmentType;
 import goorm.eagle7.stelligence.domain.contribute.dto.ContributeRequest;
 import goorm.eagle7.stelligence.domain.contribute.model.ContributeStatus;
-import goorm.eagle7.stelligence.domain.debate.model.DebateStatus;
 import goorm.eagle7.stelligence.domain.debate.repository.DebateRepository;
 import goorm.eagle7.stelligence.domain.document.content.DocumentContentRepository;
 import goorm.eagle7.stelligence.domain.document.content.model.Document;
@@ -41,17 +40,21 @@ public class ContributeRequestValidator {
 	 * <ul>
 	 *     <li>document가 존재하지 않을 경우</li>
 	 *     <li>해당 document에 대한 투표중인 수정요청이 존재하는 경우</li>
+	 *     <li>해당 document에 대한 토론이 진행중인 경우</li>
+	 *     <li>해당 document에 대한 토론이 종료되고 참여자를 대상으로 수정요청 대기중에 허용되지 않은 토론이나 참여자가 요청하는 경우</li>
 	 *     <li>수정하고자 하는 section들이 document에 존재하지 않을 경우</li>
 	 *     <li>수정하고자 하는 제목이 다른 문서와 중복되는 경우</li>
 	 *     <li>수정하고자 하는 제목에 대해 다른 수정요청이 해당 제목으로 변경을 요청중인 경우</li>
 	 *     <li>각각의 sectionId를 가진 amendmentRequest들이 creatingOrder가 중복되거나 순차적이지 않을 경우</li>
 	 * </ul>
 	 * </p>
+	 *
+	 * @param request       수정요청 DTO
+	 * @param loginMemberId
 	 * @throws BaseException 유효하지 않은 요청일 경우
-	 * @param request 수정요청 DTO
 	 */
 	@Transactional(readOnly = true)
-	public void validate(ContributeRequest request) {
+	public void validate(ContributeRequest request, Long loginMemberId) {
 
 		//document가 존재하는가
 		Document document = documentContentRepository.findById(request.getDocumentId())
@@ -62,10 +65,9 @@ public class ContributeRequestValidator {
 			throw new BaseException("이미 해당 문서에 대한 수정요청이 존재합니다. documentId=" + request.getDocumentId());
 		}
 
-		//해당 document에 대한 열린 토론이 존재하는가?
-		if (debateRepository.existsByContributeDocumentIdAndStatus(request.getDocumentId(), DebateStatus.OPEN)) {
-			throw new BaseException("해당 문서에 대한 토론이 진행중입니다. documentId=" + request.getDocumentId());
-		}
+		// 해당 document에 대한 토론이 진행중이거나, 수정요청 대기중인가?
+		checkDebate(request, loginMemberId);
+
 
 		//수정하고자 하는 section들이 document에 존재하는가
 		List<Long> sectionIds = sectionRepository.findSectionIdByVersion(document,
@@ -128,5 +130,24 @@ public class ContributeRequestValidator {
 			}
 		}
 		return true;
+	}
+
+	private void checkDebate(ContributeRequest request, Long loginMemberId) {
+		debateRepository.findLatestDebateByDocumentId(request.getDocumentId()).ifPresent(
+			debate -> {
+				// 토론이 진행중인가?
+				if (debate.isOnDebate()) {
+					throw new BaseException("해당 문서에 대한 토론이 진행중입니다. debateId=" + debate.getId());
+				}
+				// 토론이 종료된 후 토론 참여자를 위한 수정요청 대기중인가?
+				if (debate.isPendingForContribute()
+					// 다른 토론에서 파생된 수정요청을 보내는 경우
+					&& (!debate.getId().equals(request.getRelatedDebateId())
+					// 토론 참여자가 아닌 사람이 수정요청을 보내는 경우
+					|| !debate.hasPermissionToWriteDrivenContribute(loginMemberId))) {
+					throw new BaseException("최근 종료된 토론 참여자를 위한 수정요청 대기중입니다. debateId=" + debate.getId());
+				}
+			}
+		);
 	}
 }
