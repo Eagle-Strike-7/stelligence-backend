57392dc95639e306f74e1f9695965db2b35013dd
sbslc2000 <sbslc2000@gmail.com>
2024-02-15 21:22:54 +0900

    [#main] chore: 개발용 토론 연장을 위한 스케쥴러 도입

---
+++ src/main/java/goorm/eagle7/stelligence/DebateEndAtExtender.java
@@ -0,0 +1,24 @@
+package goorm.eagle7.stelligence;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 개발 테스트 용으로 특정 토론을 유지시키기 위한 스케쥴러입니다.
+ */
+@Component
+@RequiredArgsConstructor
+public class DebateEndAtExtender {
+
+	private final JdbcTemplate jdbcTemplate;
+
+	@Scheduled(fixedRate = 1000 * 60 * 3) // 3분마다 실행
+	public void extendDebateEndAt() {
+		//now + 15로 endat을 연장한다.
+		jdbcTemplate.update("UPDATE debate SET end_at = DATE_ADD(NOW(), INTERVAL 15 MINUTE) WHERE debate_id = 4");
+	}
+
+}
