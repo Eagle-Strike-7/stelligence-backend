a09f3ca90f6c17e824c45f5408198c229d58f197
Sooamazing <honang515@gmail.com>
2024-02-15 18:23:18 +0900

    [#283] refactor: Strategy 전략 패턴->템플릿메서드 패턴으로 변경

--- src/main/java/goorm/eagle7/stelligence/domain/badge/strategy/BadgeAwardStrategyTemplate.java
+++ src/main/java/goorm/eagle7/stelligence/domain/badge/strategy/BadgeAwardStrategyTemplate.java
@@ -2,32 +2,37 @@ package goorm.eagle7.stelligence.domain.badge.strategy;
 
 import java.util.Map;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import goorm.eagle7.stelligence.domain.badge.model.Badge;
 import goorm.eagle7.stelligence.domain.badge.model.BadgeCategory;
 import goorm.eagle7.stelligence.domain.member.model.Member;
 
-public interface BadgeAwardStrategy {
+public abstract class BadgeAwardStrategyTemplate {
 
-	boolean supports(BadgeCategory badgeCategory);
 
-	Map<Integer, Badge> getRequiredCounts();
+	protected abstract boolean supports(BadgeCategory badgeCategory);
 
-	long getCount(Long memberId);
+	protected abstract Map<Integer, Badge> getBadgeCriteria();
+
+	protected abstract long getCount(Long memberId);
 
 	/**
 	 * <h2>전략 상세 조건 확인 후 발급</h2>
 	 * <p>- 조건 숫자와 동일한 경우, 해당 배지를 찾아 배지 발급</p>
 	 * @param member
 	 */
-	default void checkAndAward(Member member) {
+	@Transactional
+	public void checkAndAward(Member member) {
 
 		long count = getCount(member.getId());
 
-		getRequiredCounts().entrySet().stream()
+		getBadgeCriteria().entrySet().stream()
 			.filter(entry -> count == entry.getKey())
 			.map(Map.Entry::getValue)
 			.findAny()
 			.ifPresent(member::addBadge);
 	}
+
 
 }
