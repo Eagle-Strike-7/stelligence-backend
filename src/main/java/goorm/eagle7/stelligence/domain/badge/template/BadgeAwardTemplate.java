61bd60d5d439441c1e94114884308e2adc024228
Sooamazing <honang515@gmail.com>
2024-02-16 12:47:42 +0900

    [#283] chore: (PR 반영) final 제외한 이유 주석 추가

--- src/main/java/goorm/eagle7/stelligence/domain/badge/template/BadgeAwardTemplate.java
+++ src/main/java/goorm/eagle7/stelligence/domain/badge/template/BadgeAwardTemplate.java
@@ -19,6 +19,8 @@ public abstract class BadgeAwardTemplate {
 	/**
 	 * <h2>전략 상세 조건 확인 후 발급</h2>
 	 * <p>- 조건 숫자와 동일한 경우, 해당 배지를 찾아 배지 발급</p>
+	 * <p>- 오버라이딩 방지를 위해 final 고려했으나, transactional과 함께 사용 불가해 선택 X</p>
+	 * <p>- transactional이 더 중요하다 판단</p>
 	 * @param member 해당 이벤트 확인할 member
 	 */
 	@Transactional
