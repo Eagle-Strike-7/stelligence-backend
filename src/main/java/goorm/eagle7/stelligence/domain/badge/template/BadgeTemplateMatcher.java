be10c903c5456ce83ac6a095c088fbf88da295a5
Sooamazing <honang515@gmail.com>
2024-02-16 14:01:18 +0900

    [#283] refactor: 전략, Strategy -> 템플릿으로 용어 변경

--- src/main/java/goorm/eagle7/stelligence/domain/badge/template/BadgeTemplateMatcher.java
+++ src/main/java/goorm/eagle7/stelligence/domain/badge/template/BadgeTemplateMatcher.java
@@ -12,31 +12,31 @@ import goorm.eagle7.stelligence.domain.badge.model.BadgeCategory;
 @Component
 public class BadgeTemplateMatcher {
 
-	private final Map<BadgeCategory, BadgeAwardTemplate> strategyMap = new EnumMap<>(BadgeCategory.class);
+	private final Map<BadgeCategory, BadgeAwardTemplate> templateMap = new EnumMap<>(BadgeCategory.class);
 
 	/**
 	 * <h2>BadgeCategory, BadgeAwardStrategy 매핑</h2>
 	 * <p>- BadgeAwardStrategy 구현체들을 주입받아서 BadgeCategory에 따라 매핑하여 Map으로 반환</p>
 	 * <p>- EnumMap: Enum을 key로 사용, 키의 순서를 Enum 내의 선언 순서대로 유지, null 허용 X</p>
 	 * <p>- EnumMap 내부적으로 Enum의 ordinal 값을 인덱스로 사용하여 요소를 저장해 HashMap보다 더 빠름</p>
-	 * @param strategies BadgeAwardStrategy 구현체들
+	 * @param templates BadgeAwardStrategy 구현체들
 	 */
 	public BadgeTemplateMatcher(
-		List<BadgeAwardTemplate> strategies) {
+		List<BadgeAwardTemplate> templates) {
 
 		// 각 BadgeAwardStrategy 구현체들을 BadgeCategory에 따라 매핑
-		strategies
+		templates
 			.forEach(strategy ->
 				Arrays.stream(BadgeCategory.values())
 					.filter(strategy::supports)
 					.forEach(category ->
-						strategyMap.put(category, strategy)
+						templateMap.put(category, strategy)
 					)
 			);
 	}
 
-	public BadgeAwardTemplate findStrategy(BadgeCategory badgeCategory) {
-		return strategyMap.get(badgeCategory);
+	public BadgeAwardTemplate findTemplate(BadgeCategory badgeCategory) {
+		return templateMap.get(badgeCategory);
 	}
 
 }
