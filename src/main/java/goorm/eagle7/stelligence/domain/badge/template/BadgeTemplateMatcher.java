1c4d250f6c6a877ebc56ca139c317f705d245f88
Sooamazing <honang515@gmail.com>
2024-02-16 11:47:58 +0900

    [#283] fix: 생성자로 초기화 진행 - @Bean등록해야 실행되던 문제 -> @RequiredArgsConstructor 때문, 제거 - 기존 Bean으로 등록, 메서드를 사용해 초기화하는 방식 -> 생성자 초기화 - 필드 주입으로 사용 및 private 시 사용되지 않는는 알림으로 protected로 설정

--- src/main/java/goorm/eagle7/stelligence/domain/badge/template/BadgeTemplateMatcher.java
+++ src/main/java/goorm/eagle7/stelligence/domain/badge/template/BadgeTemplateMatcher.java
@@ -1,21 +1,18 @@
-package goorm.eagle7.stelligence.domain.badge.strategytemplate;
+package goorm.eagle7.stelligence.domain.badge.template;
 
 import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import goorm.eagle7.stelligence.domain.badge.model.BadgeCategory;
-import lombok.RequiredArgsConstructor;
 
 @Component
-@RequiredArgsConstructor
 public class BadgeTemplateMatcher {
 
-	private final Map<BadgeCategory, BadgeAwardStrategyTemplate> strategyMap = new EnumMap<>(BadgeCategory.class);
+	private final Map<BadgeCategory, BadgeAwardTemplate> strategyMap = new EnumMap<>(BadgeCategory.class);
 
 	/**
 	 * <h2>BadgeCategory, BadgeAwardStrategy 매핑</h2>
@@ -23,11 +20,9 @@ public class BadgeTemplateMatcher {
 	 * <p>- EnumMap: Enum을 key로 사용, 키의 순서를 Enum 내의 선언 순서대로 유지, null 허용 X</p>
 	 * <p>- EnumMap 내부적으로 Enum의 ordinal 값을 인덱스로 사용하여 요소를 저장해 HashMap보다 더 빠름</p>
 	 * @param strategies BadgeAwardStrategy 구현체들
-	 * @return Map BadgeCategory에 따라 매핑된 BadgeAwardStrategy Map
 	 */
-	@Bean
-	private Map<BadgeCategory, BadgeAwardStrategyTemplate> findStrategy(
-		List<BadgeAwardStrategyTemplate> strategies) {
+	protected BadgeTemplateMatcher(
+		List<BadgeAwardTemplate> strategies) {
 
 		// 각 BadgeAwardStrategy 구현체들을 BadgeCategory에 따라 매핑
 		strategies
@@ -38,11 +33,9 @@ public class BadgeTemplateMatcher {
 						strategyMap.put(category, strategy)
 					)
 			);
-
-		return strategyMap;
 	}
 
-	public BadgeAwardStrategyTemplate findStrategy(BadgeCategory badgeCategory) {
+	public BadgeAwardTemplate findStrategy(BadgeCategory badgeCategory) {
 		return strategyMap.get(badgeCategory);
 	}
 
