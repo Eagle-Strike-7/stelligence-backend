af2df091c2a8b490d9abb022ffd5cc755df920f3
Sooamazing <honang515@gmail.com>
2024-02-16 17:19:02 +0900

    [#307] refactor: security 동작 확인 위해 그대로 403으로 반환

--- src/main/java/goorm/eagle7/stelligence/common/auth/filter/handler/CustomAccessDeniedHandler.java
+++ src/main/java/goorm/eagle7/stelligence/common/auth/filter/handler/CustomAccessDeniedHandler.java
@@ -22,10 +22,10 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 	public void handle(HttpServletRequest request, HttpServletResponse response,
 		AccessDeniedException accessDeniedException) {
 
-		// 필요한 권한이 없이 접근하려 할때 400으로 통일 - Admin, user, 다른 사람 정보를 수정하려 할 때 등
+		// 필요한 권한이 없이 접근하려 할때 403으로 통일 - Admin, user, 다른 사람 정보를 수정하려 할 때 등
 		log.debug("CustomAccessDeniedHandler 진입");
 		ResponseTemplate<Void> responseTemplate = ResponseTemplate.fail(accessDeniedException.getMessage());
-		ResponseTemplateUtils.toErrorResponse(HttpServletResponse.SC_BAD_REQUEST, responseTemplate);
+		ResponseTemplateUtils.toErrorResponse(HttpServletResponse.SC_FORBIDDEN, responseTemplate);
 
 	}
 }
