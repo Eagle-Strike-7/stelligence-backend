8b4b02f150a9676e2f10087dde5c3ecd5e5cc00c
sbslc2000 <sbslc2000@gmail.com>
2024-02-18 12:53:04 +0900

    [#321] test: 테스트를 위한 빌더 클래스 수정 및 생성

--- src/test/java/goorm/eagle7/stelligence/domain/notification/TestNotificationContent.java
+++ src/test/java/goorm/eagle7/stelligence/domain/notification/TestNotificationContent.java
@@ -4,66 +4,61 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
-import goorm.eagle7.stelligence.domain.notification.model.Notification;
 import goorm.eagle7.stelligence.domain.notification.model.NotificationContent;
 
-public class TestNotification {
-	// TestNotification의 Builder 클래스
+public class TestNotificationContent {
+
+	// TestNotificationContent의 Builder 클래스
 	public static class Builder {
-		private Notification notification;
+		private NotificationContent notificationContent;
 
 		public Builder() {
 			try {
-				Constructor<?> constructor = Notification.class.getDeclaredConstructor();
+				Constructor<?> constructor = NotificationContent.class.getDeclaredConstructor();
 				constructor.setAccessible(true);
-				notification = (Notification)constructor.newInstance();
+				notificationContent = (NotificationContent)constructor.newInstance();
 				id(1L);
-				content(null);
-				isRead(false);
-				memberId(null);
+				message("test message");
+				uri("/test uri");
 			} catch (NoSuchMethodException | InstantiationException | IllegalAccessException |
 					 InvocationTargetException e) {
 				throw new RuntimeException(e);
 			}
-
 		}
 
 		public Builder id(Long id) {
 			return setField("id", id);
 		}
 
-		public Builder content(NotificationContent notificationContent) {
-			return setField("content", notificationContent);
+		public Builder message(String message) {
+			return setField("message", message);
 		}
 
-		public Builder isRead(boolean isRead) {
-			return setField("isRead", isRead);
-		}
-
-		public Builder memberId(Long memberId) {
-			return setField("memberId", memberId);
+		public Builder uri(String uri) {
+			return setField("uri", uri);
 		}
 
 		// 다른 필드 설정 메소드...
 
 		private Builder setField(String fieldName, Object value) {
 			try {
-				Field field = Notification.class.getDeclaredField(fieldName);
+				Field field = NotificationContent.class.getDeclaredField(fieldName);
 				field.setAccessible(true);
-				field.set(notification, value);
+				field.set(notificationContent, value);
 			} catch (NoSuchFieldException | IllegalAccessException e) {
 				throw new RuntimeException(e);
 			}
 			return this;
 		}
 
-		public Notification build() {
-			return notification;
+		public NotificationContent build() {
+			return notificationContent;
 		}
+
 	}
 
-	// TestNotification에 대한 Builder 객체를 생성하는 정적 메소드
 	public static Builder builder() {
 		return new Builder();
 	}
+
 }
