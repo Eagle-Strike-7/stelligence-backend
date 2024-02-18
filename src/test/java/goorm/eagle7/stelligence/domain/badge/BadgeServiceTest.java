99c5248303a7cefb443ba68fe945813a0f387268
Sooamazing <honang515@gmail.com>
2024-02-19 06:45:24 +0900

    [#326] test: badge 발급 성공, 예외2 상황 테스트.

--- src/test/java/goorm/eagle7/stelligence/domain/badge/BadgeServiceTest.java
+++ src/test/java/goorm/eagle7/stelligence/domain/badge/BadgeServiceTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.*;
 
 import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationEventPublisher;
 
+import goorm.eagle7.stelligence.domain.badge.event.NewBadgeEvent;
 import goorm.eagle7.stelligence.domain.badge.model.Badge;
 import goorm.eagle7.stelligence.domain.badge.model.BadgeCategory;
 import goorm.eagle7.stelligence.domain.badge.template.BadgeMatchedCountTemplate;
@@ -34,7 +34,7 @@ class BadgeServiceTest {
 
 
 	@Test
-	@DisplayName("[성공] - 배지 추가, 글 작성 - WRITING, 1")
+	@DisplayName("[성공] - 배지 추가, 글 작성 - DOCUMENT - checkAndAwardBadge")
 	void getBadgeWriting1Success() {
 
 		// given
@@ -50,148 +50,56 @@ class BadgeServiceTest {
 		// when
 		badgeService.checkAndAwardBadge(BadgeCategory.DOCUMENT, member);
 
-		// then
+		// then - member에게 배지가 추가되고, 이벤트가 발행되었는지 확인
 		assertThat(member.getBadges())
 			.isNotEmpty()
 			.contains(Badge.ASTRONAUT)
-			.hasSize(1);
+			.hasSize(1); // TODO 왜 member는 mock 안 해도 되는 걸까?
+		verify(eventPublisher, times(1)).publishEvent(any(NewBadgeEvent.class));
 
 	}
-	//
-	// @Test
-	// @DisplayName("[예외] - 배지 추가, 글 작성 X (repository에 저장되지 않은 상황 가정) - WRITING, 0")
-	// void getBadgeWriting0() {
-	//
-	// 	// given
-	// 	BadgeCategory badgeCategory = BadgeCategory.DOCUMENT;
-	// 	when(documentContentRepository.countByAuthor_Id(member.getId())).thenReturn(0L);
-	//
-	// 	// when
-	// 	badgeService.checkAndAwardBadge(badgeCategory, member);
-	//
-	// 	// then
-	// 	assertThat(member.getBadges()).isEmpty();
-	// 	verify(documentContentRepository, times(1)).countByAuthor_Id(member.getId());
-	// 	verify(contributeRepository, never()).countByMemberId(member.getId());
-	// 	verify(contributeRepository, never()).countByMemberIdAndStatus(member.getId(), ContributeStatus.MERGED);
-	// 	verify(contributeRepository, never()).countByMemberIdAndStatus(member.getId(),
-	// 		ContributeStatus.REJECTED);
-	//
-	// }
-	//
-	// @Test
-	// @DisplayName("[성공] - 배지 추가, 모든 수정 요청 - CONTRIBUTE_ALL, 5")
-	// void getBadgeContributeAll5Success() {
-	//
-	// 	// given
-	// 	BadgeCategory badgeCategory = BadgeCategory.CONTRIBUTE_ALL;
-	// 	when(contributeRepository.countByMemberId(member.getId())).thenReturn(5L);
-	//
-	// 	// when
-	// 	badgeService.checkAndAwardBadge(badgeCategory, member);
-	//
-	// 	// then
-	// 	assertThat(member.getBadges())
-	// 		.isNotEmpty()
-	// 		.contains(Badge.VENUS)
-	// 		.hasSize(1);
-	// 	verify(contributeRepository, times(1)).countByMemberId(member.getId());
-	// 	verify(contributeRepository, never()).countByMemberIdAndStatus(member.getId(), ContributeStatus.MERGED);
-	// 	verify(contributeRepository, never()).countByMemberIdAndStatus(member.getId(),
-	// 		ContributeStatus.REJECTED);
-	// 	verify(documentContentRepository, never()).countByAuthor_Id(member.getId());
-	//
-	// }
-	//
-	// @Test
-	// @DisplayName("[예외] - 배지 추가, 모든 수정 요청 사잇값 - CONTRIBUTE_ALL, 8")
-	// void getBadgeContributeAll8() {
-	//
-	// 	// given
-	// 	BadgeCategory badgeCategory = BadgeCategory.CONTRIBUTE_ALL;
-	// 	when(contributeRepository.countByMemberId(member.getId())).thenReturn(8L);
-	//
-	// 	// when
-	// 	badgeService.checkAndAwardBadge(badgeCategory, member);
-	//
-	// 	// then
-	// 	assertThat(member.getBadges()).isEmpty();
-	// 	verify(contributeRepository, times(1)).countByMemberId(member.getId());
-	// 	verify(contributeRepository, never()).countByMemberIdAndStatus(member.getId(), ContributeStatus.MERGED);
-	// 	verify(contributeRepository, never()).countByMemberIdAndStatus(member.getId(),
-	// 		ContributeStatus.REJECTED);
-	// 	verify(documentContentRepository, never()).countByAuthor_Id(member.getId());
-	//
-	// }
-	//
-	// @Test
-	// @DisplayName("[성공] - 배지 추가, 반영된 수정 요청 - CONTRIBUTE_MERGED, 5")
-	// void getBadgeContributeMerged5Success() {
-	//
-	// 	// given
-	// 	BadgeCategory badgeCategory = BadgeCategory.CONTRIBUTE_MERGED;
-	// 	when(contributeRepository.countByMemberIdAndStatus(member.getId(), ContributeStatus.MERGED)).thenReturn(
-	// 		5L);
-	//
-	// 	// when
-	// 	badgeService.checkAndAwardBadge(badgeCategory, member);
-	//
-	// 	// then
-	// 	assertThat(member.getBadges())
-	// 		.isNotEmpty()
-	// 		.contains(Badge.SATURN)
-	// 		.hasSize(1);
-	// 	verify(contributeRepository, never()).countByMemberId(member.getId());
-	// 	verify(contributeRepository, times(1)).countByMemberIdAndStatus(member.getId(), ContributeStatus.MERGED);
-	// 	verify(contributeRepository, never()).countByMemberIdAndStatus(member.getId(),
-	// 		ContributeStatus.REJECTED);
-	// 	verify(documentContentRepository, never()).countByAuthor_Id(member.getId());
-	//
-	// }
-	//
-	// @Test
-	// @DisplayName("[성공] - 배지 추가, 반려된 수정 요청 - CONTRIBUTE_REJECTED, 100")
-	// void getBadgeContributeRejected100Success() {
-	//
-	// 	// given
-	// 	BadgeCategory badgeCategory = BadgeCategory.CONTRIBUTE_REJECTED;
-	// 	when(contributeRepository.countByMemberIdAndStatus(member.getId(),
-	// 		ContributeStatus.REJECTED)).thenReturn(100L);
-	//
-	// 	// when
-	// 	badgeService.checkAndAwardBadge(badgeCategory, member);
-	//
-	// 	// then
-	// 	assertThat(member.getBadges())
-	// 		.isNotEmpty()
-	// 		.contains(Badge.BLACKHOLE)
-	// 		.hasSize(1);
-	// 	verify(contributeRepository, never()).countByMemberId(member.getId());
-	// 	verify(contributeRepository, never()).countByMemberIdAndStatus(member.getId(), ContributeStatus.MERGED);
-	// 	verify(contributeRepository, times(1)).countByMemberIdAndStatus(member.getId(),
-	// 		ContributeStatus.REJECTED);
-	// 	verify(documentContentRepository, never()).countByAuthor_Id(member.getId());
-	//
-	// }
-	//
-	// @Test
-	// @DisplayName("[실패] - (현재 미구현) 배지 추가, 신고 - REPORT, 10")
-	// void getBadgeReport10Fail() {
-	//
-	// 	// given
-	// 	BadgeCategory badgeCategory = BadgeCategory.REPORT;
-	//
-	// 	// when
-	// 	badgeService.checkAndAwardBadge(badgeCategory, member);
-	//
-	// 	// then
-	// 	assertThat(member.getBadges()).isEmpty();
-	// 	verify(contributeRepository, never()).countByMemberId(member.getId());
-	// 	verify(contributeRepository, never()).countByMemberIdAndStatus(member.getId(), ContributeStatus.MERGED);
-	// 	verify(contributeRepository, never()).countByMemberIdAndStatus(member.getId(),
-	// 		ContributeStatus.REJECTED);
-	// 	verify(documentContentRepository, never()).countByAuthor_Id(member.getId());
-	//
-	// }
+
+
+	@Test
+	@DisplayName("[예외] - BadgeCategory에 해당하는 템플릿이 없을 경우, IllegalArgumentException - checkAndAwardBadge")
+	void getBadgeWriting1Fail() {
+
+		// given
+		// BadgeMatchedCountTemplate template = mock(BadgeMatchedCountTemplate.class);
+		Member member = member(1L, "nickname");
+		// badgeCategory에 해당하는 전략 category 찾기
+		when(badgeTemplateMatcher.findTemplate(BadgeCategory.CONTRIBUTE_MERGED)).thenReturn(null);
+
+		// when, then - 해당하는 badge category가 없을 경우, IllegalArgumentException
+		assertThatThrownBy(() ->
+			badgeService.checkAndAwardBadge(
+				BadgeCategory.CONTRIBUTE_MERGED,member))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("해당하는 badge category가 없습니다.: CONTRIBUTE_MERGED");
+		verify(eventPublisher, never()).publishEvent(any(NewBadgeEvent.class));
+
+	}
+
+
+	@Test
+	@DisplayName("[예외] - 현재 조건을 만족하는 배지가 없는 경우, empty - checkAndAwardBadge")
+	void getBadgeWriting0Fail() {
+
+		// given
+		Member member = member(1L, "nickname");
+		BadgeMatchedCountTemplate template = mock(BadgeMatchedCountTemplate.class);
+		// badgeCategory에 해당하는 전략 category 찾기
+		when(badgeTemplateMatcher.findTemplate(BadgeCategory.REPORT)).thenReturn(template);
+		// 배지 찾기
+		when(template.getBadgeWithCount(member)).thenReturn(Optional.empty());
+
+		// when
+		badgeService.checkAndAwardBadge(BadgeCategory.REPORT, member);
+
+		// then - member에게 배지가 추가되지 않았는지, eventPublisher 호출 X 확인
+		assertThat(member.getBadges()).isEmpty();
+		verify(eventPublisher, never()).publishEvent(any(NewBadgeEvent.class));
+
+	}
 
 }
